5a828485e054190ed724c994382b91ae85f627c5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-05-31 03:33:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.96%)

--- DSA/0206-reverse-linked-list/solution.java
+++ DSA/0206-reverse-linked-list/solution.java
@@ -1,25 +1,24 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode current=head;
-        ListNode nextNode=head;
-        ListNode prev=null;
-        while(nextNode!=null){
-            nextNode=nextNode.next;
-            current.next=prev;
-            prev=current;
-            current=nextNode;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = head;
+        while (nextNode != null) {
+            nextNode = nextNode.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
         }
-        head=prev;
-        return head;
+        return prev;    // return prev bcz nextNode node is null
     }
 }
