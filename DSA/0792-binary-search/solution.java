522f9f3cfe10dd51b48825886d7c8f8f9e825592
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-08 09:47:46 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 45.7 MB (99.03%)

---
+++ DSA/0792-binary-search/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int beg=0;
+        int end=nums.length-1;
+        int mid=0;
+        while(beg<=end){
+              mid=(beg+end)/2;
+            if(nums[mid]==target)
+                 return mid;
+            else if(target<nums[mid])
+                 end=mid-1;
+            else if(target>nums[mid])
+                 beg=mid+1;
+        }
+        return -1;
+    }
+}
