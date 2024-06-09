70a2292ec16f2c1311763f3e454b76083ab02b7b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-09 09:17:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.89%)

--- DSA/0033-search-in-rotated-sorted-array/solution.java
+++ DSA/0033-search-in-rotated-sorted-array/solution.java
@@ -1,31 +1,25 @@
-class Solution {
+public class Solution {
     public int search(int[] nums, int target) {
-        int pivot=findPivot(nums);
-        int ans1= binarySearch(nums,0,pivot-1,target); 
-        int ans2 = binarySearch(nums,pivot,nums.length-1,target);
-        if(ans1 ==-1 && ans2==-1)   return -1;
-        if(ans1 ==-1)   return ans2;
-        return ans1;    
-    }
-    private int findPivot(int[] nums){
-        int l=0;
-        int r=nums.length-1;
-        while(l<r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums[r]){
-                l=mid+1;
-            }else{
-                r=mid;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (nums[mid] == target)
+                return mid;
+        
+            if (nums[start] <= nums[mid]){
+                 if (target < nums[mid] && target >= nums[start]) 
+                    end = mid - 1;
+                 else
+                    start = mid + 1;
+            } 
+        
+            if (nums[mid] <= nums[end]){
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                 else
+                    end = mid - 1;
             }
-        }
-        return l;
-    }
-    private int binarySearch(int[] nums, int l, int r, int target){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target)   return mid;
-            if(target<nums[mid])    r=mid-1;
-            else l=mid+1;
         }
         return -1;
     }
