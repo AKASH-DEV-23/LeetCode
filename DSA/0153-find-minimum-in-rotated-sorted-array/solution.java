31785674c2c763c147bf8961a68a07818ce2de87
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-09 08:22:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.56%)

--- DSA/0153-find-minimum-in-rotated-sorted-array/solution.java
+++ DSA/0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public int findMin(int[] nums) {
-        int l=0;
-        int r=nums.length-1;
-        while(l<r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums[r])   l=mid+1;
-            else r=mid;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        return nums[l];
+        return nums[left];
     }
 }
