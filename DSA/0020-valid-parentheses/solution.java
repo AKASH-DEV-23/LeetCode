b2cdc4db1875deb109dda8ad02aeec270842c67a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-11 12:27:06 +0000

    [Commit] Runtime - 1 ms (99.72%), Memory - 41.3 MB (99.99%)

--- DSA/0020-valid-parentheses/solution.java
+++ DSA/0020-valid-parentheses/solution.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(ch=='(' || ch=='[' || ch== '{'){
-                st.push(ch);
-            }else{
-                if(st.isEmpty())    return false;
-                char opening = st.pop();
-                if(opening=='(' && ch == ')' || opening=='[' && ch==']' || opening=='{' && ch=='}'){
-                    continue;
-                }else{
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
                     return false;
+                } else {
+                    char top = stack.peek();
+                    if (top == '(' && c == ')' || top == '[' && c == ']'
+                            || top == '{' && c == '}') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 }
