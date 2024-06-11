79a81c622c33102f6845cdc97939ec20577c2230
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-11 14:14:57 +0000

    Sync LeetCode submission Runtime - 1 ms (99.80%), Memory - 43.2 MB (100.00%)

--- DSA/0224-basic-calculator/solution.java
+++ DSA/0224-basic-calculator/solution.java
@@ -1,32 +1,23 @@
 class Solution {
+    int idx; // this index traverse the string in one pass, between different level of recursion
     public int calculate(String s) {
-    Stack<Integer> stack = new Stack<>();
-    int sign = 1;
-    int result = 0;
-    int num = 0;
-    for (char c : s.toCharArray()) {
-        if (Character.isDigit(c)) {
-            num = num * 10 + c - '0';
-        } else if (c == '+') {
-            result += sign * num;
-            num = 0;
-            sign = 1;
-        } else if (c == '-') {
-            result += sign * num;
-            num = 0;
-            sign = -1;
-        } else if (c == '(') {
-            stack.push(result);
-            stack.push(sign);
-            sign = 1;
-            result = 0;
-        } else if (c == ')') {
-            result += sign * num;
-            num = 0;
-            result *= stack.pop();
-            result += stack.pop();
-        }
+        idx = 0; // Initialization should be here
+        return calc(s);
     }
-    return result + sign * num;
-}
+    
+    private int calc(String s) {
+        int res = 0, num = 0, sign = 1;
+        while (idx < s.length()) {
+            char c = s.charAt(idx++);
+            if (c >= '0' && c <= '9') num = num * 10 + c - '0';
+            else if (c == '(') num = calc(s); // ( is start of a new sub-problem, Let recursion solve the sub-problem
+            else if (c == ')') return res + sign * num;
+            else if (c == '+' || c == '-') { // only when we meet a new sign, we know a while number has been read
+                res += sign * num;
+                num = 0;
+                sign = c == '-' ? -1 : 1;
+            }
+        }
+        return res + sign * num; // last number is not processed yet
+    }
 }
