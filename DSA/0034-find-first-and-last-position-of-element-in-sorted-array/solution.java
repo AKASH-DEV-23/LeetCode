5e106cff60da1c27cbaf953cc39f278ccbdf5db5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-19 08:37:23 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 46.1 MB (99.97%)

--- DSA/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ DSA/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,23 +1,43 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int st=0;
-        int end=nums.length-1;
-        int mid=0;
-        boolean flag=true;
-        while(st<=end){
-            mid=(st+end)/2;
-            if(nums[mid]==target){
-                flag=false;
-                break;
+    private int leftSearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int leftMost = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                leftMost = mid;
+                high = mid - 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else if(target<nums[mid]){
-                end=mid-1;
-            }else   st=mid+1;
         }
-        if(flag)    return new int[]{-1,-1};
-        int i=mid;
-        while(i>=0 && nums[i]==target)  i--;
-        while(mid<nums.length && nums[mid]==target) mid++;
-        return new int[]{i+1,mid-1};
+        return leftMost;
+    }
+
+    private int rightSearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int rightMost = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                rightMost = mid;
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return rightMost;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int l = leftSearch(nums, target);
+        int r = rightSearch(nums, target);
+        return new int[] { l, r };
     }
 }
