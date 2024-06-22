2f8e04d5bdccbbb4215bc75e6787e66832a1970b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-22 04:39:00 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.99%)

--- DSA/0088-merge-sorted-array/solution.java
+++ DSA/0088-merge-sorted-array/solution.java
@@ -1,16 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    // Start from the end of nums1 and nums2
-    int i = m - 1, j = n - 1, k = m + n - 1;
-    
-    // Merge in reverse order
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k--] = nums1[i--];
-        } else {
-            nums1[k--] = nums2[j--];
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
         }
     }
-}
-
 }
