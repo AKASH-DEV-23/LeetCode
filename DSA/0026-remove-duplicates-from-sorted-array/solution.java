38ff6b94355035c07468651b0c8b37f822cac66c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-22 14:10:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.8 MB (99.68%)

--- DSA/0026-remove-duplicates-from-sorted-array/solution.java
+++ DSA/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
-        for(int j=1; j<nums.length; j++){
-            if(nums[i]!=nums[j]){
-                nums[++i]=nums[j];
+        int j = 0;
+        for (int n : nums) {
+            if (j < 1 || n != nums[j - 1]) {
+                nums[j] = n;
+                j++;
             }
         }
-        return i+1;
+        return j;
     }
 }
