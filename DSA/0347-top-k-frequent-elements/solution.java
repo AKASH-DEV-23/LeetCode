55bd3b80353b1e31f6155d2cc9215a0939deadb6
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-27 14:58:20 +0000

    [Commit] Runtime - 10 ms (96.98%), Memory - 49.8 MB (12.58%)

--- DSA/0347-top-k-frequent-elements/solution.java
+++ DSA/0347-top-k-frequent-elements/solution.java
@@ -1,25 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int num:nums){
-            mp.put(num,mp.getOrDefault(num,0)+1);
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
         }
-        List<Map.Entry<Integer,Integer>> list=new ArrayList<>(mp.entrySet());
-        Comparator<Map.Entry<Integer,Integer>> com=new Comparator<>(){
-            public int compare(Map.Entry<Integer,Integer> ent1,Map.Entry<Integer,Integer> ent2){
-                if(ent1.getValue()<ent2.getValue()) return 1;
-                else if(ent1.getValue()>ent2.getValue()) return -1;
-                else    return 0;
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
             }
-        };
-        Collections.sort(list,com);
-        int[] ans=new int[k];
-        int idx=0;
-        for(Map.Entry<Integer,Integer> ent:list){
-            ans[idx++]=ent.getKey();
-            k--;
-            if(k==0)    break;
         }
-        return ans;
+        return res;
     }
 }
+
