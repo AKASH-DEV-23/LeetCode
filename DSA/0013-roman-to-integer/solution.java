4151b65dc3bcde70d2185e2df488dd189d379d42
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-29 09:26:40 +0000

    [Commit] Runtime - 2 ms (99.80%), Memory - 44.3 MB (99.99%)

--- DSA/0013-roman-to-integer/solution.java
+++ DSA/0013-roman-to-integer/solution.java
@@ -1,21 +1,40 @@
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        int result = map.get(s.charAt(s.length() - 1));
-        for (int i = s.length() - 2; i >= 0; i--) {
-            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                result -= map.get(s.charAt(i));
+class Solution{
+
+    public static int romanToInt(String s) {
+        int n = s.length();
+        int sum = 0;
+        int prev = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = getValue(s.charAt(i));
+            if (curr < prev) {
+                sum -= curr;
             } else {
-                result += map.get(s.charAt(i));
+                sum += curr;
             }
+            prev = curr;
         }
-        return result;
+        return sum;
     }
+
+    private static int getValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            default:
+                return 1000;
+        }
+
+    }
+
 }
+
