c4131a8d06f11f17dca6a3aff5c92dad8a9e9b66
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-29 07:20:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57.4 MB (99.40%)

--- DSA/0189-rotate-array/solution.java
+++ DSA/0189-rotate-array/solution.java
@@ -1,5 +1,6 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
+    
+     public static void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
         reverse(nums, 0, n - 1);
@@ -7,7 +8,7 @@ class Solution {
         reverse(nums, k, n - 1);
     }
 
-    private void reverse(int[] nums, int start, int end) {
+    private static void reverse(int[] nums, int start, int end) {
         while (start <= end) {
             int temp = nums[start];
             nums[start] = nums[end];
