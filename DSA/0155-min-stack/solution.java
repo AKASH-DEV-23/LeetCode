fbb3e18ddf7188d21bf6110f52f5efe780f01ce9
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-29 12:40:31 +0000

    [commit] Runtime - 4 ms (99.68%), Memory - 44.5 MB (99.93%)

--- DSA/0155-min-stack/solution.java
+++ DSA/0155-min-stack/solution.java
@@ -1,49 +1,31 @@
-class MinStack {
-
-    private class Node {
-        private int val;
-        private int min;
-        private Node next;
-
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-    }
-
-    private Node head;
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
 
     public MinStack() {
-
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int val) {
-        if (head == null) {
-            head = new Node(val, val, null);
-        } else {
-            head = new Node(val, Math.min(val, head.min), head);
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
         }
     }
 
     public void pop() {
-        head = head.next;
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
     }
 
     public int top() {
-        return head.val;
+        return stack.peek();
     }
 
     public int getMin() {
-        return head.min;
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
