46b529cf320f1472232f4de0f1d021660e64ea7b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-30 15:25:30 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.7 MB (99.98%)

--- DSA/0086-partition-list/solution.java
+++ DSA/0086-partition-list/solution.java
@@ -9,23 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode partition(ListNode head, int x) {
+    
+        public ListNode partition(ListNode head, int x) {
         ListNode small = new ListNode(-1);
+        ListNode smallHead = small;
         ListNode large = new ListNode(-1);
-        ListNode smallP = small;
-        ListNode largeP = large;
+        ListNode largeHead = large;
         while (head != null) {
             if (head.val < x) {
-                smallP.next = head;
-                smallP = smallP.next;
+                small.next = head;
+                small = small.next;
             } else {
-                largeP.next = head;
-                largeP = largeP.next;
+                large.next = head;
+                large = large.next;
             }
             head = head.next;
         }
-        smallP.next = large.next;
-        largeP.next = null;
-        return small.next;
+        small.next = largeHead.next;
+        large.next = null;
+        return smallHead.next;
     }
 }
