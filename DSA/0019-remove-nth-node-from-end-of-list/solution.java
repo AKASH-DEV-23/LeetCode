d3089e109107d5785ae1e7c925c3dcfa8651a620
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-30 12:18:28 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.3 MB (99.92%)

--- DSA/0019-remove-nth-node-from-end-of-list/solution.java
+++ DSA/0019-remove-nth-node-from-end-of-list/solution.java
@@ -10,19 +10,18 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // if (head == null || head.next == null)  return null;
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode slow = dummy;
-        ListNode fast = dummy;
-        for (int i = 1; i <= n; i++) {
-            fast = fast.next;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i < n + 1; i++) {
+            first = first.next;
         }
-        while (fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
+        while (first != null) {
+            first = first.next;
+            second = second.next;
         }
-        slow.next = slow.next.next;
+        second.next = second.next.next;
         return dummy.next;
     }
 }
