1070d819f328ab9f55ffa28da317e6c1350747f4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-06-30 06:16:26 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.99%)

--- DSA/0033-search-in-rotated-sorted-array/solution.java
+++ DSA/0033-search-in-rotated-sorted-array/solution.java
@@ -1,24 +1,24 @@
 public class Solution {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        while (start <= end){
+        while (start <= end) {
             int mid = (start + end) / 2;
-            if (nums[mid] == target)
+            if (target == nums[mid]) {
                 return mid;
-        
-            if (nums[start] <= nums[mid]){
-                 if (target < nums[mid] && target >= nums[start]) 
+            }
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]) {
                     end = mid - 1;
-                 else
+                } else {
                     start = mid + 1;
-            } 
-        
-            if (nums[mid] <= nums[end]){
-                if (target > nums[mid] && target <= nums[end])
+                }
+            } else if (nums[mid] <= nums[end]) {
+                if (target >= nums[mid] && target <= nums[end]) {
                     start = mid + 1;
-                 else
+                } else {
                     end = mid - 1;
+                }
             }
         }
         return -1;
