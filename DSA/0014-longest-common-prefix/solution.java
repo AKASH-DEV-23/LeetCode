5b706f11f6a4a04cd5df5d97748f7f627fd2e285
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-03 17:19:07 +0000

    Sync LeetCode submission Runtime - 1 ms (64.07%), Memory - 41.1 MB (99.97%)

--- DSA/0014-longest-common-prefix/solution.java
+++ DSA/0014-longest-common-prefix/solution.java
@@ -1,21 +1,15 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String temp=strs[0];
-        String ans=strs[0];
-        for(int i=1;i<strs.length;i++){
-            temp=solve(temp,strs[i]);
-            ans=temp;
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+        for (int i = 0; i < first.length; i++) {
+            if(first[i] != last[i]){
+                break;
+            }
+            ans.append(first[i]);
         }
-        return ans;
-    }
-    private String solve(String str1, String str2){
-        StringBuilder sb=new StringBuilder();
-        int i=0;
-        while(i<str1.length() && i<str2.length()){
-            if(str1.charAt(i)!=str2.charAt(i))  return sb.toString();
-            sb.append(str1.charAt(i));
-            i++;
-        }
-        return sb.toString();
+        return ans.toString();
     }
 }
