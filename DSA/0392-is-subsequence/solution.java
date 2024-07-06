053f6a30694bae81e97068a3b79a86ba1f9727c2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-06 16:01:01 +0000

    [Commit] Runtime - 2 ms (66.84%), Memory - 41.5 MB (99.97%)

--- DSA/0392-is-subsequence/solution.java
+++ DSA/0392-is-subsequence/solution.java
@@ -1,11 +1,14 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i=0;
-        for(int j=0;j<t.length();j++){
-            if(i==s.length())   return true;
-            if(s.charAt(i)==t.charAt(j))    i++;
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
         }
-        if(i==s.length())   return true;
-        return false;
+        return i == s.length();
+
     }
 }
