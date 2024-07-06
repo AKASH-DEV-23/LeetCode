d6c940ab5903bf01783702937d806a3a590594bb
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-06 16:21:29 +0000

    [Commit] Runtime - 2 ms (96.32%), Memory - 47.7 MB (98.66%)

--- DSA/0167-two-sum-ii---input-array-is-sorted/solution.java
+++ DSA/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
-        while(left<right){
-            int sum=nums[left]+nums[right];
-            if(sum==target)  return new int[]{left+1,right+1};
-            else if(sum>target) right--;
-            else    left++;
+    public static int[] twoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[] { low + 1, high + 1 };
+            }
+            if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
         }
-        return new int[0];
+        return new int[] { -1, -1 };
     }
+
 }
