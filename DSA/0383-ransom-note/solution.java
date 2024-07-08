498adf9873cda584d721d5f3c2081123ff222fd2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-08 16:09:35 +0000

    [commit] Runtime - 1 ms (99.85%), Memory - 44.3 MB (99.98%)

---
+++ DSA/0383-ransom-note/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for (char ch : magazine.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        for (char ch : ransomNote.toCharArray()) {
+            count[ch - 'a']--;
+            if (count[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
