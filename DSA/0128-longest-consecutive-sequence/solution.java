4c233ad7d9be97e1294f4f8f6af2f192d59716cf
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-11 09:58:22 +0000

    [commit] Runtime - 50 ms (11.07%), Memory - 60.1 MB (99.13%)

---
+++ DSA/0128-longest-consecutive-sequence/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num,false);
+        }
+        int longestLength = 0;
+        for (int num : nums) {
+            int currentLength = 1;
+            int nextNum = num + 1;
+            while (map.containsKey(nextNum) && map.get(nextNum) == false) {
+                currentLength++;
+                map.put(nextNum, true);
+                nextNum++;
+            }
+            int prevNum = num - 1;
+            while (map.containsKey(prevNum) && map.get(prevNum) == false) {
+                currentLength++;
+                map.put(prevNum, true);
+                prevNum--;
+            }
+            longestLength = Math.max(longestLength, currentLength);
+        }
+        return longestLength;
+    }
+}
