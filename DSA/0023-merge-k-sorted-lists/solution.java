4fb7ad691df17e2c31d3885ac8309f94249947dd
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-18 17:23:05 +0000

    Sync LeetCode submission Runtime - 2 ms (88.28%), Memory - 44.2 MB (99.93%)

--- DSA/0023-merge-k-sorted-lists/solution.java
+++ DSA/0023-merge-k-sorted-lists/solution.java
@@ -1,40 +1,45 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    ListNode head;
-    public ListNode mergeKLists(ListNode[] lists) {
-        for(int i=0;i<lists.length;i++){
-            head=merge(lists[i],head);
+    private ListNode mergeTwoList(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoList(l2.next, l1);
+            return l2;
         }
-        return head;
     }
-    private ListNode merge(ListNode l1, ListNode l2){
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy;
-        while(l1!=null && l2 !=null){
-            if(l1.val<=l2.val){
-                curr.next=l1;
-                l1=l1.next;
-            }else{
-                curr.next=l2;
-                l2=l2.next;
-            }
-            curr=curr.next;
+
+    private ListNode partitionAndMerge(ListNode[] lists, int start, int end) {
+        // if(start> end){
+        //     return null;
+        // }
+        if (start == end) {
+            return lists[start];
         }
-        if(l1!=null){
-            curr.next=l1;
+        while(start< end){
+            int mid = (start + end) / 2;
+            ListNode L1 = partitionAndMerge(lists, start, mid);
+            ListNode L2 = partitionAndMerge(lists, mid + 1, end);
+            return mergeTwoList(L1, L2);
         }
-        if(l2!=null){
-            curr.next=l2;
-        }
-        return dummy.next;
+        return null;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partitionAndMerge(lists, 0, lists.length - 1);
     }
 }
