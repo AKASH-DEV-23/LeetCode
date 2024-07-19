6a0adec72ee01a06ac130f53a0e6919785011d4f
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-19 14:21:37 +0000

    [Commit] Runtime - 1077 ms (94.71%), Memory - 0.0B (100.00%)

--- DSA/1153-product-sales-analysis-i/solution.sql
+++ DSA/1153-product-sales-analysis-i/solution.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-select s2.product_name , s1.year ,s1.price
-from Sales s1  left join Product s2 
-on s1.product_id=s2.product_id;
+SELECT product_name, year, price from Sales cross join Product on Sales.product_id = Product.product_id
