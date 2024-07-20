1e6d7085d928c82268bd4b2c70c187745c07fb3d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-20 12:00:01 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 44.5 MB (97.89%)

---
+++ DSA/0230-kth-smallest-element-in-a-bst/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root,k);
+        return ans;
+    }
+    int count = 0;
+    int ans = 0;
+    private void inorder(TreeNode root, int k){
+        if(root.left != null){
+            inorder(root.left,k);
+        }
+        count++;
+        if(count == k){
+            ans = root.val;
+            return;
+        }
+        if(root.right != null){
+            inorder(root.right,k);
+        }
+    }
+}
