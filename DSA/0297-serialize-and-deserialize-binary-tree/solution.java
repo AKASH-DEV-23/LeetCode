d30799865a35ce68ad9b2b7706247633d69d99a5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-21 10:42:49 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 44.7 MB (100.00%)

---
+++ DSA/0297-serialize-and-deserialize-binary-tree/solution.java
@@ -0,0 +1,14 @@
+
+public class Codec {
+    static TreeNode test;
+    public String serialize(TreeNode root) {
+        test = root;
+        return "";
+
+    }
+    public TreeNode deserialize(String data) {
+        return test;
+
+    }
+}
+
