5e8e50494b48c85832c5e52651887d0747fd3ea5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-26 14:07:47 +0000

    [Commit] Runtime - 6 ms (94.30%), Memory - 43.2 MB (99.93%)

--- DSA/0179-largest-number/solution.java
+++ DSA/0179-largest-number/solution.java
@@ -1,24 +1,14 @@
 class Solution {
-    Comparator<Integer> com=new Comparator<Integer>(){
-        public int compare(Integer n1, Integer n2){
-            String s1=n1.toString();
-            String s2=n2.toString();
-            return (s2+s1).compareTo(s1+s2);
-        }
-    };
     public String largestNumber(int[] nums) {
-        Integer[] arr=new Integer[nums.length];
-        int i=0;
-        for(int num:nums){
-            arr[i++]=num;
+        String s[] = new String[nums.length];
+        for(int i=0;i<nums.length;i++){
+            s[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(arr,com);
-        System.out.print(Arrays.toString(arr));
-        String ans="";
-        for(Integer num:arr){
-            ans+=num;
+        Arrays.sort(s,(a,b) -> (b+a).compareTo(a+b));
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            result.append(s[i]);
         }
-        if(ans.charAt(0)=='0')  return "0";
-        return ans;
+        return result.charAt(0)=='0'?"0" : result.toString();
     }
 }
