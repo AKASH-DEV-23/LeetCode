59a7fffa585e53f6f1c0b05df91d5a6eeea1c5c7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-27 15:57:20 +0000

    Sync LeetCode submission Runtime - 321 ms (99.63%), Memory - 0.0B (100.00%)

--- DSA/0197-rising-temperature/solution.sql
+++ DSA/0197-rising-temperature/solution.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-SELECT w2.id
-FROM Weather AS w1 CROSS JOIN Weather AS w2
-WHERE DATEDIFF(w2.recordDate,w1.recordDate)=1 AND w1.temperature < w2.temperature;
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature > W2.temperature;
+
