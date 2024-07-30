031b7c80b036080f3e003649e6381d94e8fbc6ad
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-30 07:22:49 +0000

    [commit] Runtime - 7 ms (74.77%), Memory - 47.8 MB (98.79%)

--- DSA/0049-group-anagrams/solution.java
+++ DSA/0049-group-anagrams/solution.java
@@ -1,14 +1,14 @@
 class Solution {
- public static List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+    public static List<List<String>> groupAnagrams(String[] strs) {
+       Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
             }
-            map.get(sortedWord).add(word);
+            map.get(key).add(s);
         }
         return new ArrayList<>(map.values());
     }
