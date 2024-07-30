a7eb75b7470152952f4672fb8171608633afb978
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-30 13:51:30 +0000

    [commit] Runtime - 14 ms (7.21%), Memory - 44.9 MB (97.09%)

---
+++ DSA/0036-valid-sudoku/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    continue;
+                }
+                char c = board[row][col];
+                if (!set.add(c + "in row" + row) ||
+                        !set.add(c + "in col" + col) ||
+                        !set.add(c + "in box" + row / 3 + "_" + col / 3)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
