2446c671ccc5f2f3da81a8487c80978ae2e4b54e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-07-31 08:56:39 +0000

    [commit] Runtime - 14 ms (32.47%), Memory - 45.2 MB (40.65%)

--- DSA/0125-valid-palindrome/solution.java
+++ DSA/0125-valid-palindrome/solution.java
@@ -1,19 +1,16 @@
 class Solution {
-public boolean isPalindrome(String s) {
-    s = s.toLowerCase(); // convert to lowercase
-    int left = 0, right = s.length() - 1;
-    while (left <= right) {
-        if (!Character.isLetterOrDigit(s.charAt(left))) {
-            left++; // skip non-alphanumeric characters
-        } else if (!Character.isLetterOrDigit(s.charAt(right))) {
-            right--; // skip non-alphanumeric characters
-        } else if (s.charAt(left) != s.charAt(right)) {
-            return false; // mismatched characters
-        } else {
-            left++; right--; // move towards center
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int i = 0;
+        int j = s.length()-1;
+        while(i<=j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            } else{
+                return false;
+            }
         }
+        return true;
     }
-    return true; // palindrome
-}
-
 }
