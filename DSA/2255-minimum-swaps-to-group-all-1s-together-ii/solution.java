cf9698b20c0cbed394d67b67d1cff919b6a8a88d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-08-02 16:59:27 +0000

    [commit] Runtime - 11 ms (29.79%), Memory - 59.1 MB (99.76%)

---
+++ DSA/2255-minimum-swaps-to-group-all-1s-together-ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSwaps(int[] nums) {
+        int countOnes = 0;
+        for (int n : nums) {
+            if (n == 1) {
+                countOnes++;
+            }
+        }
+        int i = 0;
+        int j = 0;
+        int currCount = 0;
+        int maxCount = 0;
+        int n = nums.length;
+        while (j < 2 * n) {
+            if (nums[j % n] == 1) {
+                currCount++;
+            }
+            if (j - i + 1 > countOnes) {
+                currCount -= nums[i % n];
+                i++;
+            }
+            maxCount = Math.max(currCount, maxCount);
+            j++;
+        }
+        return countOnes - maxCount;
+    }
+}
