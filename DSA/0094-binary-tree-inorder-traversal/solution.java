d2e18ae1ccee1e00adfe1a2fa23a84031a98bc41
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-08-06 17:02:00 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.97%)

--- DSA/0094-binary-tree-inorder-traversal/solution.java
+++ DSA/0094-binary-tree-inorder-traversal/solution.java
@@ -1,31 +1,32 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    List<Integer> list;
+    List<Integer> list = new ArrayList<>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        list = new ArrayList<>();
-        if(root==null)  return list;
-        helper(root);
+        inorder(root);
         return list;
     }
-    private void helper(TreeNode root){
-        if(root==null)  return;
-        helper(root.left);
-        list.add(root.val);
-        helper(root.right);
 
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
     }
 }
