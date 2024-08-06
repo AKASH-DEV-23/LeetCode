3d76dc158cece5b6ead1803514f7e8582b977514
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-08-06 04:10:31 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.9 MB (96.98%)

---
+++ DSA/3275-minimum-number-of-pushes-to-type-word-i/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+     public int minimumPushes(String word) {
+        int n = word.length();
+        int ans = 0;
+        while(n > 0) {
+            ans += n;
+            n -= 8;
+        }
+        return ans;
+    }
+}
