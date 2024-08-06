330d3817f175b86b34c3d727771bd8be5206c3a6
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-08-06 17:39:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.7 MB (99.83%)

--- DSA/0083-remove-duplicates-from-sorted-list/solution.java
+++ DSA/0083-remove-duplicates-from-sorted-list/solution.java
@@ -1,24 +1,29 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy=new ListNode(-111);
-        ListNode prev=dummy;
-        while(head!=null){
-            if(prev.val!=head.val){
-                prev.next=new ListNode(head.val);
-                prev=prev.next;
+        ListNode dummy = new ListNode(-111);
+        dummy.next = head;
+        ListNode temp = dummy;
+        while (temp != null) {
+            if(temp.next == null){
+                break;
             }
-            head=head.next;
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
         }
         return dummy.next;
+
     }
 }
