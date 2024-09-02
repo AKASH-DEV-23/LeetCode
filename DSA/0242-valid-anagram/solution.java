6c41439df9d9fd6af60bea3c124a9ff19ecd8ec1
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-09-02 17:45:56 +0000

    [commit] Runtime - 2 ms (99.45%), Memory - 43.2 MB (99.00%)

--- DSA/0242-valid-anagram/solution.java
+++ DSA/0242-valid-anagram/solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         int[] count = new int[26];
-        for(char c : s.toCharArray()){
+        for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
-        for(char c : t.toCharArray()){
+        for (char c : t.toCharArray()) {
             count[c - 'a']--;
         }
-        for(int c : count){
-            if(c != 0){
+        for (int n : count) {
+            if (n != 0) {
                 return false;
             }
         }
