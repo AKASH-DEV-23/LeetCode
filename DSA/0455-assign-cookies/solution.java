7cafe5376a6c94187786c1bd0a1b41ca22895c8e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-15 17:08:06 +0000

    [Commit] Runtime - 12 ms (99.41%), Memory - 48.8 MB (99.97%)

--- DSA/0455-assign-cookies/solution.java
+++ DSA/0455-assign-cookies/solution.java
@@ -2,18 +2,17 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int i=0;
-        int j=0;
-        int n1=g.length;
-        int n2=s.length;
         int cnt=0;
-        while(i<n1 && j<n2){
-            if(g[i]<=s[j]){
+         int i=0;
+         int j=0;
+         while(i<g.length && j<s.length){
+            if(g[i]<=s[j]) {
                 cnt++;
                 i++;
-            }                  
-            j++;
-        }
+                j++;
+            }else if(s[j]<g[i]) j++;
+         }
         return cnt;
+
     }
 }
