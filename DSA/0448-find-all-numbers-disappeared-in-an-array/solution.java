41a06ef97f50b74ffeb8d6116a3703ef25445b23
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-16 04:10:39 +0000

    [Commit] Runtime - 6 ms (77.91%), Memory - 53.2 MB (100.00%)

--- DSA/0448-find-all-numbers-disappeared-in-an-array/solution.java
+++ DSA/0448-find-all-numbers-disappeared-in-an-array/solution.java
@@ -1,13 +1,17 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = Math.abs(nums[i]);
+            if (nums[n - 1] > 0) {
+                nums[n - 1] = -1 * (nums[n - 1]);
+            }
         }
-        List<Integer> ans=new ArrayList<>();
-        for(int i=1;i<=nums.length;i++){
-            if(!set.contains(i))  ans.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
         }
-        return ans;
+        return list;
     }
 }
