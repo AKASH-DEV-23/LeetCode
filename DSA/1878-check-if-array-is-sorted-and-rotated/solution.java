d18c6a98676918d54c5ed34b783b1d3ca3f665ef
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-17 15:54:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.3 MB (99.61%)

--- DSA/1878-check-if-array-is-sorted-and-rotated/solution.java
+++ DSA/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -1,15 +1,11 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int n=nums.length;
-        int cnt=0;
-
-        for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                cnt++;
+    public static boolean check(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
             }
         }
-        if(nums[0]<nums[n-1])   cnt++;
-
-        return cnt>1?false:true;
+        return (count > 1) ? false : true;
     }
 }
