7846130bdd2fa080501b487bbb861d4ae80c5702
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-17 17:11:18 +0000

    Sync LeetCode submission Runtime - 1 ms (99.79%), Memory - 45.5 MB (99.96%)

--- DSA/0283-move-zeroes/solution.java
+++ DSA/0283-move-zeroes/solution.java
@@ -1,17 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                swap(nums,i,j);
-                i++;
+        int count = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[count] = num;
+                count++;
             }
         }
-
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+        while (count < nums.length) {
+            nums[count++] = 0;
+        }
     }
 }
