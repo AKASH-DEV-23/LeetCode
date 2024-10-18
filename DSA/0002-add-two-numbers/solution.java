d7ea5db522b8c20c9a665e9c2c4a42415a5fad66
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-18 19:12:12 +0000

    [Commit] Runtime - 1 ms (99.86%), Memory - 44 MB (99.92%)

--- DSA/0002-add-two-numbers/solution.java
+++ DSA/0002-add-two-numbers/solution.java
@@ -10,25 +10,23 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+        ListNode dummy = new ListNode(-111);
+        ListNode curr = dummy;
         int carry = 0;
-        int sum = 0;
         while (l1 != null || l2 != null || carry != 0) {
-            sum = carry;
             if (l1 != null) {
-                sum += l1.val;
+                carry += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
-                sum += l2.val;
+                carry += l2.val;
                 l2 = l2.next;
             }
-            carry = sum / 10;
-            sum = sum % 10;
-            temp.next = new ListNode(sum);
-            temp = temp.next;
+            curr.next = new ListNode(carry % 10);
+            carry /= 10;
+            curr = curr.next;
         }
         return dummy.next;
+
     }
 }
