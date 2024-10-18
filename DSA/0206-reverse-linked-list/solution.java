77eed97df21d2903f31be16d8f5b7125c6c3b179
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-18 17:25:20 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 42.4 MB (99.48%)

--- DSA/0206-reverse-linked-list/solution.java
+++ DSA/0206-reverse-linked-list/solution.java
@@ -1,23 +1,23 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode previous = null;
+        ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
             ListNode nextNode = curr.next;
-            curr.next = previous;
-            previous = curr;
+            curr.next = prev;
+            prev = curr;
             curr = nextNode;
         }
-        return previous;
+        return prev;
     }
 }
