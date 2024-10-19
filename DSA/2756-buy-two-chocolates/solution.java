c0ce8383ab990d78918dcb0210b7b55eeac11d7d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-19 12:10:00 +0000

    [Commit] Runtime - 2 ms (64.13%), Memory - 44.6 MB (100.00%)

--- DSA/2756-buy-two-chocolates/solution.java
+++ DSA/2756-buy-two-chocolates/solution.java
@@ -1,8 +1,13 @@
 class Solution {
-    public int buyChoco(int[] prices, int money) {
+   public static int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
-        int cost=prices[0]+prices[1];
-        if(cost<=money) return money-cost;
-        return money;
+        int sum = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (i < 2) {
+                sum += prices[i];
+            }
+        }
+        int result = money - sum;
+        return (result >= 0) ? result : money;
     }
 }
