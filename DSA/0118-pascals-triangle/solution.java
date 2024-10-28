82a8ce477c3677c59e5102fb21da294bcd301936
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-10-28 04:02:16 +0000

    [Commit] Runtime - 1 ms (94.57%), Memory - 41.8 MB (99.99%)

--- DSA/0118-pascals-triangle/solution.java
+++ DSA/0118-pascals-triangle/solution.java
@@ -1,24 +1,16 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> t1=new ArrayList<>();
-        t1.add(1);
-        ans.add(t1);
-        if(numRows==1)  return ans;
-        List<Integer> t2=new ArrayList<>();
-        t2.add(1);
-        t2.add(1);
-        ans.add(t2);
-        if(numRows==2)  return ans;
-        for(int i=2;i<numRows;i++){
-            List<Integer> temp=new ArrayList<>();
-            temp.add(1);
+        List<List<Integer>> trg=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            row.add(1);
             for(int j=1;j<i;j++){
-                temp.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+                int val=trg.get(i-1).get(j-1)+trg.get(i-1).get(j);
+                row.add(val);
             }
-            temp.add(1);
-            ans.add(temp);
+            if(i>0) row.add(1);
+            trg.add(row);
         }
-        return ans;
+        return trg;
     }
 }
