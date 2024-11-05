7a69ececcc4ab1d576de16169501a81067b65814
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-05 10:03:47 +0000

    Sync LeetCode submission Runtime - 2 ms (97.11%), Memory - 41.7 MB (99.90%)

--- DSA/0020-valid-parentheses/solution.java
+++ DSA/0020-valid-parentheses/solution.java
@@ -1,22 +1,22 @@
 class Solution {
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c=='(' || c == '[' || c == '{'){
-                stack.push(c);
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
             }
-            else {
-                if(stack.isEmpty()){
+            else{
+                if(st.isEmpty()){
                     return false;
                 }
-                char top = stack.peek();
-                if(top == '(' && c == ')' || top == '[' && c == ']' || top == '{' && c == '}'){
-                    stack.pop();
+                char top = st.peek();
+                if(top=='(' && c==')' || top=='[' && c==']' || top=='{' && c=='}'){
+                    st.pop();
                 } else{
-                    return false;
+                    return  false;
                 }
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
