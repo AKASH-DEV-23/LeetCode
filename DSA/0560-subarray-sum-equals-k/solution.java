68866906ec7df62b25cc5b895cb6c533f2c808ef
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-11 18:48:03 +0000

    [Commit] Runtime - 1096 ms (24.38%), Memory - 46.4 MB (99.96%)

--- DSA/0560-subarray-sum-equals-k/solution.java
+++ DSA/0560-subarray-sum-equals-k/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int size=nums.length;
-        int cnt=0;
-        for(int i=0;i<size;i++){
+        int count =0;
+        for(int i=0;i<nums.length;i++){
             int sum=0;
-            for(int j=i;j<size;j++){
+            for(int j=i;j<nums.length;j++){
                 sum+=nums[j];
                 if(sum==k){
-                    cnt++;
-                }  
+                    count++;
+                }
             }
         }
-        return cnt;
+        return count;
     }
 }
