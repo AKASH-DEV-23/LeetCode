f9109d7def3038a6f945ae1c25f27733de69b853
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-11 18:12:16 +0000

    Sync LeetCode submission Runtime - 2 ms (97.65%), Memory - 43.4 MB (99.44%)

--- DSA/0349-intersection-of-two-arrays/solution.java
+++ DSA/0349-intersection-of-two-arrays/solution.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums1)  set.add(num);
-        List<Integer> list=new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int num:nums1){
+            set.add(num);
+        }
         for(int num:nums2){
-            if(set.contains(num)){ 
+            if(set.contains(num)){
                 list.add(num);
                 set.remove(num);
             }
         }
-        int[] ans=new int[list.size()];
-        int i=0;
-        for(int num:list){
-            ans[i++]=num;
+        int[] arr=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
         }
-        return ans;
+        return arr;
     }
 }
