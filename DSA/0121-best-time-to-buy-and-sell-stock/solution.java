732f806f858c3c47fea70b1f77cc1b090a2fdbd9
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-12 11:23:49 +0000

    [commit] Runtime - 1 ms (99.82%), Memory - 61.6 MB (98.97%)

--- DSA/0121-best-time-to-buy-and-sell-stock/solution.java
+++ DSA/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for (int n : prices) {
-            if (n < buyPrice) {
-                buyPrice = n;
-            } else {
-                // int profit = n - buyPrice;
-                maxProfit = Math.max(maxProfit, n - buyPrice);
+        int buy = Integer.MAX_VALUE;
+        for (int num : prices) {
+            if (num < buy) {
+                buy = num;
+            } else{
+               int profit=num-buy;
+                maxProfit=Math.max(profit,maxProfit);
             }
         }
         return maxProfit;
