8a3f1ddd19e3aa82d0f67dfc251869ba577dc925
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-21 17:55:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57.6 MB (98.69%)

--- DSA/0189-rotate-array/solution.java
+++ DSA/0189-rotate-array/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        int len=nums.length;
-        swap(nums,0,len-1);
-        swap(nums,0,k-1);
-        swap(nums,k,len-1);
+        k = k % nums.length;
+        int len = nums.length;
+        swap(nums, 0, len - 1);
+        swap(nums, 0, k - 1);
+        swap(nums, k, len - 1);
     }
-    private void swap(int[] nums, int i, int j){
-        while(i<j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-        i++;
-        j--;
+
+    private void swap(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
