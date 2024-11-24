e06d94e9a253ab1fc4c608b0b52d41f503451965
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-24 15:34:07 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 40.6 MB (99.99%)

--- DSA/0908-middle-of-the-linked-list/solution.java
+++ DSA/0908-middle-of-the-linked-list/solution.java
@@ -13,9 +13,10 @@ class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
+            slow=slow.next;
         }
         return slow;
+
     }
 }
