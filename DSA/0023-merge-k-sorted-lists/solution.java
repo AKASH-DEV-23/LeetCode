feb3ee52d6f5dc168bad4bd504f079e446259754
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-25 15:50:50 +0000

    [Commit] Runtime - 83 ms (12.50%), Memory - 44.2 MB (99.99%)

--- DSA/0023-merge-k-sorted-lists/solution.java
+++ DSA/0023-merge-k-sorted-lists/solution.java
@@ -1,45 +1,43 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    private ListNode mergeTwoList(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        if (l1.val <= l2.val) {
-            l1.next = mergeTwoList(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoList(l2.next, l1);
-            return l2;
-        }
-    }
-
-    private ListNode partitionAndMerge(ListNode[] lists, int start, int end) {
-        // if(start> end){
-        //     return null;
-        // }
-        if (start == end) {
-            return lists[start];
-        }
-        while(start< end){
-            int mid = (start + end) / 2;
-            ListNode L1 = partitionAndMerge(lists, start, mid);
-            ListNode L2 = partitionAndMerge(lists, mid + 1, end);
-            return mergeTwoList(L1, L2);
-        }
-        return null;
-    }
-
     public ListNode mergeKLists(ListNode[] lists) {
-        return partitionAndMerge(lists, 0, lists.length - 1);
+        if(lists.length ==0){
+            return null;
+        }
+        ListNode head=lists[0];
+        for(int i=1;i<lists.length;i++){
+            head= merge(head,lists[i]);
+        }
+        return head;
+    }
+    private static ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        while(l1 !=null && l2 !=null){
+            if(l1.val <= l2.val){
+                temp.next=l1;
+                l1=l1.next;
+            } else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next;
+        }
+        if(l1 !=null){
+            temp.next=l1;
+        }
+        if(l2 !=null){
+            temp.next=l2;
+        }
+        return dummy.next;
     }
 }
