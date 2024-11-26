5e3cd4b74e73eef3c41d3f33b19fdcd1af44ce36
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-26 17:03:20 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 45.5 MB (97.82%)

--- DSA/0268-missing-number/solution.java
+++ DSA/0268-missing-number/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int max = 0;
-        max = n * (n + 1) / 2;
-        for (int num : nums) {
-            max -= num;
+        int n=nums.length;
+        int sum=n*(n+1)/2;
+        for(int num:nums){
+            sum-=num;
         }
-        return max;
+        return sum;
     }
 }
