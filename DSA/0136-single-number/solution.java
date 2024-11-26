f65f2c4b8503e597d5df90844e34ffb975303772
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-26 17:13:01 +0000

    Sync LeetCode submission Runtime - 1 ms (99.88%), Memory - 46.1 MB (98.69%)

--- DSA/0136-single-number/solution.java
+++ DSA/0136-single-number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=nums[0];
-        for(int i=1;i<nums.length;i++){
-            ans^=nums[i];
+        int sum=0;
+        for(int num:nums){
+            sum^=num;
         }
-        return ans;
+        return sum;
     }
 }
