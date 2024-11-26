4f8b1c4f9bfe8a6b16345d765446eb177f6c4984
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-26 07:57:01 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 42 MB (99.95%)

--- DSA/0086-partition-list/solution.java
+++ DSA/0086-partition-list/solution.java
@@ -1,32 +1,31 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    
-        public ListNode partition(ListNode head, int x) {
-        ListNode small = new ListNode(-1);
-        ListNode smallHead = small;
-        ListNode large = new ListNode(-1);
-        ListNode largeHead = large;
-        while (head != null) {
-            if (head.val < x) {
-                small.next = head;
-                small = small.next;
-            } else {
-                large.next = head;
-                large = large.next;
+    public ListNode partition(ListNode head, int x) {
+        ListNode small=new ListNode(0);
+        ListNode big=new ListNode(1);
+        ListNode temp1=small;
+        ListNode temp2=big;
+        ListNode curr=head;
+        while(curr!=null){
+            if(curr.val<x){
+                temp1.next=new ListNode(curr.val);
+                temp1=temp1.next;
+            }else{
+                temp2.next=new ListNode(curr.val);
+                temp2=temp2.next;
             }
-            head = head.next;
+            curr=curr.next;
         }
-        small.next = largeHead.next;
-        large.next = null;
-        return smallHead.next;
+        temp1.next=big.next;
+        return small.next;
     }
 }
