e78909a81f79b9a43c57edf16f7e39b3ae293439
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-27 16:54:32 +0000

    [Commit] Runtime - 17 ms (37.73%), Memory - 56.2 MB (99.93%)

--- DSA/0287-find-the-duplicate-number/solution.java
+++ DSA/0287-find-the-duplicate-number/solution.java
@@ -1,22 +1,10 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Start a fast and slow pointer
-        // until they meet
-        int slow = 0;
-        int fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-        // As soon as they meet, move both
-        // pointer at same speed until they
-        // meet again
-        slow = 0;
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(!set.add(num))
+                return num;
         }
-        return slow;
+        return 0;
     }
-
 }
