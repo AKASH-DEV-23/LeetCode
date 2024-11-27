ef34df3f5f7a971d1e001bce47db476e718632fe
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-27 17:02:07 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 42 MB (99.94%)

--- DSA/0019-remove-nth-node-from-end-of-list/solution.java
+++ DSA/0019-remove-nth-node-from-end-of-list/solution.java
@@ -1,27 +1,27 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-        for (int i = 0; i < n + 1; i++) {
-            first = first.next;
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode fast=dummy;
+        ListNode slow=dummy;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
         }
-        while (first != null) {
-            first = first.next;
-            second = second.next;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
         }
-        second.next = second.next.next;
+        slow.next=slow.next.next;
         return dummy.next;
     }
 }
