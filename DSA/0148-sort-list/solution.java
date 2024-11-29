feaae2817c59072ff69d10093fc19de0ae1a040e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-29 16:45:26 +0000

    [Commit] Runtime - 14 ms (22.39%), Memory - 55.4 MB (99.96%)

--- DSA/0148-sort-list/solution.java
+++ DSA/0148-sort-list/solution.java
@@ -1,29 +1,28 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while (temp != null) {
+        List<Integer> list=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null){
             list.add(temp.val);
-            temp = temp.next;
+            temp=temp.next;
         }
-        
         Collections.sort(list);
-        ListNode dummy = new ListNode(1000000);
-        dummy.next=head;
-        ListNode temp1=head;
-        for (int i = 0; i < list.size(); i++) {
-            temp1.val = list.get(i);
-            temp1 = temp1.next;
+        ListNode dummy=new ListNode(-1111);
+        temp=dummy;
+        for(int i=0;i<list.size();i++){
+            temp.next=new ListNode(list.get(i));
+            temp=temp.next;
+            
         }
         return dummy.next;
     }
