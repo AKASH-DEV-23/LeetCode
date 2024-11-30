3042140295dbe8c3a2c1b40be2c03641a9a96dd9
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-11-30 13:16:53 +0000

    Sync LeetCode submission Runtime - 2 ms (90.34%), Memory - 49 MB (84.50%)

--- DSA/0143-reorder-list/solution.java
+++ DSA/0143-reorder-list/solution.java
@@ -1,49 +1,42 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public void reorderList(ListNode head) {
-        // if LL has 1 or 2 node simply return
-        if (head == null || head.next == null || head.next.next == null) {
-            return;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null &&  fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        // find middle of the LL
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode curr=slow.next;
+        slow.next=null;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode nextNode =curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextNode;
         }
-        // Break the LL and reverse the second half
-        ListNode newHead = slow.next;
-        slow.next = null;
-        ListNode prev = null;
-        ListNode curr = newHead;
-        ListNode nextnode = newHead;
-        while (nextnode != null) {
-            nextnode = nextnode.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextnode;
-        }
-        // merge both LL
-        ListNode t1 = head;
-        ListNode t2 = prev;
-        while (t2 != null) {
-            ListNode m1 = t1.next;
-            ListNode m2 = t2.next;
-            t1.next = t2;
-            t2.next = m1;
-            t1 = m1;
-            t2 = m2;
+        ListNode left = head;
+        ListNode right = prev;
+        ListNode nextl , nextr;
 
+         while(left != null && right != null){
+            nextl = left.next;
+            left.next = right;
+            nextr = right.next;
+            right.next = nextl;
+
+            left = nextl;
+            right = nextr;
         }
     }
 }
