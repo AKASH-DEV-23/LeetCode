0bd0f52a4eb378ce120bfaedbec8b5ade5b72606
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-02 15:17:03 +0000

    [Commit] Runtime - 1 ms (59.21%), Memory - 45.7 MB (99.96%)

--- DSA/0073-set-matrix-zeroes/solution.java
+++ DSA/0073-set-matrix-zeroes/solution.java
@@ -2,35 +2,40 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int[] row=new int[m];
-        int[] col=new int[n];
+        int col0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            if(row[i]==1)
-                rowZero(matrix,i);
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
         }
-        for(int i=0;i<n;i++){
-            if(col[i]==1)
-                colZero(matrix,i);
+        if(matrix[0][0]==0){
+            for(int i=0; i<n; i++){
+                matrix[0][i]=0;
+            }
         }
-    }
-    private void rowZero(int[][] mat, int idx){
-        int n=mat[0].length;
-        for(int i=0;i<n;i++){
-            mat[idx][i]=0;
+
+        if(col0==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
         }
-    }
-    private void colZero(int[][] mat, int idx){
-        int m=mat.length;
-        for(int i=0;i<m;i++){
-            mat[i][idx]=0;
-        }
+
     }
 }
