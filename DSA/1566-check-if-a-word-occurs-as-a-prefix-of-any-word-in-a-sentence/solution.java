b1f2592c0d4b10d72ea507082574e9a68ee7c276
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-02 15:38:51 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.6 MB (97.63%)

---
+++ DSA/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] s=sentence.split(" ");
+        for(int i=0; i < s.length;i++){
+            String str=s[i];
+            if(str.startsWith(searchWord)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
