271eb73a975ebccea1c44576b02ef8f5f7eb0636
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-03 05:03:14 +0000

    [commit] Runtime - 25 ms (23.09%), Memory - 80.8 MB (98.09%)

---
+++ DSA/2232-adding-spaces-to-a-string/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int idx=0;
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(idx<spaces.length && i==spaces[idx]){
+                str.append(" ");
+                idx++;
+            }
+            str.append(s.charAt(i));
+        }
+        return str.toString();
+    }
+}
