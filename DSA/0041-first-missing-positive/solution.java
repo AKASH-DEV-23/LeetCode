85f75fec6bca21280b9e0534d838b16429bfefa8
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-03 06:30:52 +0000

    [commit] Runtime - 13 ms (32.76%), Memory - 60.6 MB (96.59%)

---
+++ DSA/0041-first-missing-positive/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+
+
+        for(int i=1;i<nums.length+2;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+
+       return -1;
+    }
+}
