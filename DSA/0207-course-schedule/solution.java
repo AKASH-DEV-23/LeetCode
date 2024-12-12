ffd3b31753fbe6dfc48467a8917cebc7275b83a6
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-12 14:08:27 +0000

    Sync LeetCode submission Runtime - 4 ms (92.92%), Memory - 45 MB (99.69%)

--- DSA/0207-course-schedule/solution.java
+++ DSA/0207-course-schedule/solution.java
@@ -1,26 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer,List<Integer>> adj=new HashMap<>();
-        int[] inDegree=new int[numCourses]; 
-        for(int[] edge:prerequisites){
-            int u=edge[1];
-            int v=edge[0];
-            if(!adj.containsKey(u))    adj.put(u,new ArrayList<>());
-            adj.get(u).add(v);
-            inDegree[v]++;
-        }
-        Queue<Integer> myq=new LinkedList<>();
+        List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0)  myq.offer(i);
+            adj.add(new ArrayList<>());
         }
-        while(!myq.isEmpty()){
-            int u=myq.poll();
-            for(int neigh:adj.getOrDefault(u,new ArrayList<>())){
-                inDegree[neigh]--;
-                if(inDegree[neigh]==0)  myq.offer(neigh);
+        for(int[] arr: prerequisites){
+            adj.get(arr[0]).add(arr[1]);
+        }
+
+        int inStack[]=new int[numCourses];
+        int visited[] =new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(dfs(adj,inStack,visited,i)){
+                return false;
             }
-        } 
-        for(int num:inDegree)   if(num>0)   return false;
+        }
         return true;
+    }
+
+    private static boolean dfs(List<List<Integer>> adj, int[] inStack, int[] visited, int i){
+        if(inStack[i]==1){
+            return true;
+        }
+        if(visited[i]==1){
+            return false;
+        }
+
+        inStack[i]=1;
+        visited[i]=1;
+
+        for(int n:adj.get(i)){
+            if(dfs(adj,inStack,visited,n)){
+                return true;
+            }
+        }
+
+        inStack[i]=0;
+        return false;
     }
 }
