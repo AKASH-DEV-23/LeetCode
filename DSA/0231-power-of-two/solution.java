13a9d7b92a49e482279b78b0dd8a34acb8c54bfc
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-13 04:08:43 +0000

    Sync LeetCode submission Runtime - 1 ms (94.78%), Memory - 41 MB (99.09%)

--- DSA/0231-power-of-two/solution.java
+++ DSA/0231-power-of-two/solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0)    return false;
-        return (n&(n-1))==0;
+        if(n<1){
+            return false;
+        }else if(n==1){
+            return true;
+        }
+
+        while(n%2==0){
+            n=n/2;
+        }
+
+        if(n==1)    return true;
+        return false;
     }
 }
