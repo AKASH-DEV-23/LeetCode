a882fc4b4851502289e982e94b3c9f12fded8c77
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-13 04:24:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (97.83%)

--- DSA/1013-fibonacci-number/solution.java
+++ DSA/1013-fibonacci-number/solution.java
@@ -1,11 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1)    return n;
-        int[] dp=new int[n+1];
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n==0){
+            return 0;
         }
-        return dp[n];
+        if(n==1){
+            return 1;
+        }
+
+        int ft=0;
+        int st=1;
+        for(int i=1;i<=n;i++){
+            int tt=ft+st;
+            ft=st;
+            st=tt;
+        }
+        return ft;
     }
 }
