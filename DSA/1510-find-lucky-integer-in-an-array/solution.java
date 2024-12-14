24cf73d4335b70a35b5880122d5bcbe3872f898d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-14 06:38:25 +0000

    [Commit] Runtime - 1 ms (99.80%), Memory - 43.3 MB (100.00%)

--- DSA/1510-find-lucky-integer-in-an-array/solution.java
+++ DSA/1510-find-lucky-integer-in-an-array/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int[] nums=new int[501];
 
         for(int num:arr){
-           nums[num]+=1;
+           nums[num]++;
         }
         for(int i=500;i>0;i--){
             if(nums[i]==i){
