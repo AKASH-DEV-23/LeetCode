fdc66d490dad7beaba0bb9e8fc96a86fac482ed3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-14 05:40:28 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.7 MB (98.86%)

---
+++ DSA/0748-largest-number-at-least-twice-of-others/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max=nums[0];
+        int maxIdx=0;
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+                maxIdx=i;
+            }
+        }
+        nums[maxIdx]=0;
+        for(int num:nums){
+            if(num*2>max){
+                return -1;
+            }
+        }
+        return maxIdx;
+    }
+}
