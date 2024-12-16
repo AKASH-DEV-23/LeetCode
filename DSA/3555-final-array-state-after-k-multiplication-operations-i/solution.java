5a8e7b903d31bb7e142613844889e9b372ef7aaf
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-16 12:38:27 +0000

    [commit] Runtime - 1 ms (100.00%), Memory - 45.4 MB (96.21%)

---
+++ DSA/3555-final-array-state-after-k-multiplication-operations-i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for(int i=0;i<k;i++){
+            int min=nums[0];
+            int idx=0;
+            for(int j=0;j<nums.length;j++){
+                if(min>nums[j]){
+                    min=nums[j];
+                    idx=j;
+                }
+            }
+            nums[idx]=nums[idx]*multiplier;
+        }
+        return nums;
+    }
+}
