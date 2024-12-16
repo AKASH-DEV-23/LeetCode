78a11ff6e82ac6e9e3ccfc5488f401e8b8083f4f
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-16 17:30:00 +0000

    [Commit] Runtime - 1 ms (44.03%), Memory - 45 MB (99.86%)

--- DSA/0941-sort-array-by-parity/solution.java
+++ DSA/0941-sort-array-by-parity/solution.java
@@ -1,13 +1,22 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int n=nums.length;
-        int[] ans = new int[n];
-        int i=0;
-        int j=n-1;
-        for(int num:nums){
-            if(num%2==0)    ans[i++]=num;
-            else    ans[j--]=num;   
+        int[] even=new int[n];
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                even[k]=nums[i];
+                k++;
+                nums[i]=-1;
+            }
         }
-        return ans;
+        for(int num:nums){
+            if(num>-1){
+                even[k]=num;
+                k++;
+            }
+        }
+        return even;
+
     }
 }
