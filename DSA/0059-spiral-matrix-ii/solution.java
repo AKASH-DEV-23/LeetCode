331aa2352c4c339a590e0207df743c4b96ce7cab
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-17 15:45:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.5 MB (99.53%)

--- DSA/0059-spiral-matrix-ii/solution.java
+++ DSA/0059-spiral-matrix-ii/solution.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[][] generateMatrix(int n) {
+        int rowStart=0;
+        int rowEnd=n-1;
+        int colStart=0;
+        int colEnd=n-1;
         int[][] ans=new int[n][n];
-        int num=1;
-        int left=0;
-        int right=n-1;
-        int top=0;
-        int bottom=n-1;
-        while(num<=n*n){
-            for(int i=left;i<=right;i++){
-                ans[top][i]=num++;
-            }
-            top++;
-            for(int i=top;i<=bottom;i++){
-                ans[i][right]=num++;
-            }
-            right--;
-            for(int i=right;i>=left;i--){
-                ans[bottom][i]=num++;
-            }
-            bottom--;
-            for(int i=bottom;i>=top;i--){
-                ans[i][left]=num++;
-            }
-            left++;
+        int cnt=1;
+        while(cnt<=n*n){
+           for(int i=rowStart;i<=colEnd;i++){
+            ans[rowStart][i]=cnt;
+            cnt++;
+           }
+            rowStart++;
+           for(int i=rowStart;i<=rowEnd;i++){
+            ans[i][colEnd]=cnt;
+            cnt++;
+           }
+           colEnd--;
+           if(rowStart<=rowEnd){
+           for(int i=colEnd;i>=colStart;i--){
+            ans[rowEnd][i]=cnt;
+            cnt++;
+           }
+           rowEnd--;
+           }
+           if(colStart<=colEnd){
+           for(int i=rowEnd;i>=rowStart;i--){
+            ans[i][colStart]=cnt;
+            cnt++;
+           }
+           colStart++;
+           }
         }
         return ans;
     }
