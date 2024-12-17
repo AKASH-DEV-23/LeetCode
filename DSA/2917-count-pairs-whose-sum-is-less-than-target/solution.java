bc10b7e7077e4d5c77708e46cab2a635dd34f24d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-17 16:16:25 +0000

    Sync LeetCode submission Runtime - 2 ms (95.50%), Memory - 42.2 MB (99.96%)

--- DSA/2917-count-pairs-whose-sum-is-less-than-target/solution.java
+++ DSA/2917-count-pairs-whose-sum-is-less-than-target/solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
+        // Collections.sort(nums);
         int cnt=0;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
