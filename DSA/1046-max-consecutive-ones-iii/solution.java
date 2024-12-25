a0144026a2cf167b5ff06e0820083453d0561513
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-25 16:52:33 +0000

    [commit] Runtime - 3 ms (97.78%), Memory - 46.6 MB (100.00%)

---
+++ DSA/1046-max-consecutive-ones-iii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int start=0;
+        int zero=0;
+        int maxOne=0;
+
+        for(int end=0;end<nums.length;end++){
+            if(nums[end]==0){
+                zero++;
+            }
+            while(zero>k){
+                if(nums[start]==0){
+                    zero--;
+                }
+                start++;
+            }
+            maxOne=Math.max(maxOne,end-start+1);
+        }
+
+        return maxOne;
+    }
+}
