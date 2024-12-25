864bb3ffb0a8c7c6f6775834e0acd5f3bff1d72a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-25 17:05:25 +0000

    Sync LeetCode submission Runtime - 3 ms (85.18%), Memory - 57.2 MB (97.68%)

--- DSA/1586-longest-subarray-of-1s-after-deleting-one-element/solution.java
+++ DSA/1586-longest-subarray-of-1s-after-deleting-one-element/solution.java
@@ -1,18 +1,23 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int i=0;
+        int k=1;
+        int start=0;
+        int maxOne=0;
         int zero=0;
-        int ans=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]==0)  zero++;
-            while(zero>1){
-                if(nums[i]==0)  zero--;
-                i++;
+
+        for(int end=0; end<nums.length;end++){
+            if(nums[end]==0){
+                zero++;
             }
-            if(zero==0) ans=Math.max(ans,j-i+1);
-            if(zero==1) ans=Math.max(ans,j-i);
+            while(zero>k){
+                if(nums[start]==0){
+                    zero--;
+                }
+                start++;
+            }
+
+            maxOne=Math.max(maxOne, end-start+1);
         }
-        if(ans==nums.length)    return ans-1;
-        return ans;
+        return maxOne-1;
     }
 }
