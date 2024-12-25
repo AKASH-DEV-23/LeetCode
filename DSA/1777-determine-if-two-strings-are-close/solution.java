d05c32fc06babe40faac12233b0bb21621860427
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-25 17:19:10 +0000

    [commit] Runtime - 10 ms (97.81%), Memory - 45.7 MB (98.51%)

---
+++ DSA/1777-determine-if-two-strings-are-close/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] ch1 = new int[26];
+        int ch2[] = new int[26];
+
+        for(char ch: word1.toCharArray()){
+            ch1[ch-'a']++;
+        }
+
+        for(char ch: word2.toCharArray()){
+            ch2[ch-'a']++;
+        }
+
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i] == 0 && ch2[i] !=0){
+                return false;
+            }
+            if(ch2[i] == 0 && ch1[i] !=0){
+                return false;
+            }
+        }
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i] != ch2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
