7d52e7e63663519ac340c08e4ba028f2b9f25314
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-26 18:32:22 +0000

    Sync LeetCode submission Runtime - 7 ms (98.46%), Memory - 46.4 MB (99.87%)

--- DSA/0056-merge-intervals/solution.java
+++ DSA/0056-merge-intervals/solution.java
@@ -1,36 +1,17 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Comparator<int[]> com=new Comparator<>(){
-            public int compare(int[] a, int[] b){
-                if(a[0]<b[0]){
-                    return -1;
-                }else if(a[0]>b[0]){
-                    return 1;
-                }else{
-                    return 0;
-                }
-            }
-        };
-        Arrays.sort(intervals,com);
-        int st=intervals[0][0];
-        int end=intervals[0][1];
+    public int[][] merge(int[][] nums) {
         List<int[]> list=new ArrayList<>();
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i][0]<=end){
-                end=Math.max(end,intervals[i][1]);
+        Arrays.sort(nums, (a,b)->Integer.compare(a[0],b[0]));
+        int[] interval=nums[0];
+        list.add(interval);
+        for(int newInterval[]:nums ){
+            if(interval[1]>=newInterval[0]){
+                interval[1]=Math.max(interval[1],newInterval[1]);
             }else{
-                list.add(new int[]{st,end});
-                st=intervals[i][0];
-                end=intervals[i][1];
+                interval=newInterval;
+                list.add(interval);
             }
         }
-        list.add(new int[]{st,end});
-        int idx=0;
-        int[][] ans=new int[list.size()][2];
-        for(int[] temp:list){
-            ans[idx][0]=temp[0];
-            ans[idx++][1]=temp[1];
-        }
-        return ans;
+        return list.toArray(new int[list.size()][]);
     }
 }
