e7e971c6f9269535ac156b9ac3774df984e59c21
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-27 15:30:42 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.7 MB (89.45%)

---
+++ DSA/1252-break-a-palindrome/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length()<=1){
+            return "";
+        }
+        StringBuilder sb=new StringBuilder("");
+        sb.append(palindrome);
+
+        for(int i=0;i<(palindrome.length())/2;i++){
+            if(palindrome.charAt(i)!='a'){
+                sb.setCharAt(i,'a');
+                return sb.toString();
+            }
+        }
+        sb.setCharAt(sb.length()-1,'b');
+        return sb.toString();
+    }
+}
