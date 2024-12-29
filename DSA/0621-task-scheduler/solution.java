6b27f20c8d563e567c7880bfee002e5acada3c74
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2024-12-29 15:22:06 +0000

    Sync LeetCode submission Runtime - 2 ms (99.15%), Memory - 45.8 MB (99.27%)

--- DSA/0621-task-scheduler/solution.java
+++ DSA/0621-task-scheduler/solution.java
@@ -5,9 +5,13 @@ class Solution {
         for(char ch:tasks){
             freq[ch-'A']++;
         }
+
         Arrays.sort(freq);
         int i=25;
         while(i>=0 && freq[i]==freq[25]) i--;
+
         return Math.max(tasks.length, (freq[25]-1)*(n+1)+25-i);
+        
+
     }
 }
