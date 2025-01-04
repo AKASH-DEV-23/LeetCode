d5b3e1319b5808534b711672d32f803b68f4f2e4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-04 08:51:28 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 57.7 MB (99.80%)

--- DSA/0189-rotate-array/solution.java
+++ DSA/0189-rotate-array/solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        int len = nums.length;
-        swap(nums, 0, len - 1);
-        swap(nums, 0, k - 1);
-        swap(nums, k, len - 1);
+        int len=nums.length;
+        k=k%len;
+        reverse(nums,0,len-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,len-1);
     }
 
-    private void swap(int[] nums, int i, int j) {
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+    private void reverse(int[] nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
             i++;
             j--;
         }
     }
 }
+
+
