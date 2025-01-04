db36c84d1150fca0d5f5628c65f3557cc0373c8c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-04 09:40:52 +0000

    [Commit] Runtime - 2 ms (89.13%), Memory - 46.2 MB (99.91%)

--- DSA/0283-move-zeroes/solution.java
+++ DSA/0283-move-zeroes/solution.java
@@ -1,21 +1,12 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
+    public void moveZeroes(int[] nums) {
         int i=0;
-        int j=0;
-        int len=arr.length;
-        while(j<len){
-            if(arr[i]!=0){
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
                 i++;
-                j++;
-            } else{
-                while( j < len && arr[j]==0){
-                    j++;
-                }
-                if(j<len){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                }
             }
         }
     }
