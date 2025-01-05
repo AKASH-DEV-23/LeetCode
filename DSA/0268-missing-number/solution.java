94d1b62d95b193cbda031b9f6792f0e7de614764
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-05 02:59:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.5 MB (98.83%)

--- DSA/0268-missing-number/solution.java
+++ DSA/0268-missing-number/solution.java
@@ -1,10 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int xor1=0;
+        int xor2=0;
         int n=nums.length;
-        int sum=n*(n+1)/2;
-        for(int num:nums){
-            sum-=num;
+
+        for(int i=0;i<n;i++){
+            xor1^=i;
+            xor2^=nums[i];
         }
-        return sum;
+        xor1^=n;
+        return xor1^xor2;
     }
 }
