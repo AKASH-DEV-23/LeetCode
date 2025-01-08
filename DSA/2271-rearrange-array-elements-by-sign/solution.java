42202262863b17acb32c69870e142ce0647763e4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-08 14:23:26 +0000

    [commit] Runtime - 3 ms (99.68%), Memory - 76.9 MB (99.17%)

--- DSA/2271-rearrange-array-elements-by-sign/solution.java
+++ DSA/2271-rearrange-array-elements-by-sign/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] res=new int[nums.length];
+        int[] arr=new int[nums.length];
         int pos=0;
-        int nev=1;
+        int neq=1;
         for(int num:nums){
-            if(num>0){
-                res[pos]=num;
+            if(num<0){
+                arr[neq]=num;
+                neq+=2;
+            }else{
+                arr[pos]=num;
                 pos+=2;
-            } else{
-                res[nev]=num;
-                nev+=2;
             }
         }
-        return res;
+        return arr;
     }
 }
