962ab8ec304a2f9ae86105389a63b3fee7c4a44d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-10 13:20:14 +0000

    [Commit] Runtime - 2 ms (99.12%), Memory - 45.1 MB (99.82%)

--- DSA/0001-two-sum/solution.java
+++ DSA/0001-two-sum/solution.java
@@ -5,9 +5,10 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int diff=target-nums[i];
             if(map.containsKey(diff)){
-                return new int[]{map.get(diff),i};
+                return new int[]{i,map.get(diff)};
+            }else{
+                map.put(nums[i],i);
             }
-            map.put(nums[i],i);
         }
         return new int[]{0,0};
     }
