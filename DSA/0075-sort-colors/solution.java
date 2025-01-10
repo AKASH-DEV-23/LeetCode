37243adce29ee1c6456c35599925e3a278b4c6c2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-10 13:27:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (99.95%)

--- DSA/0075-sort-colors/solution.java
+++ DSA/0075-sort-colors/solution.java
@@ -3,6 +3,7 @@ class Solution {
         int low=0;
         int mid=0;
         int high=nums.length-1;
+
         while(mid<=high){
             if(nums[mid]==0){
                 swap(nums,low,mid);
@@ -10,12 +11,13 @@ class Solution {
                 mid++;
             }else if(nums[mid]==1){
                 mid++;
-            }else if(nums[mid]==2){
+            }else{
                 swap(nums,mid,high);
                 high--;
             }
         }
     }
+
     private void swap(int[] nums, int i, int j){
         int temp=nums[i];
         nums[i]=nums[j];
