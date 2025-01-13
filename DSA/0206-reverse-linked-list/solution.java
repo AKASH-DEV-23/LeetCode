795032507471b6a1173560ff7ce84a150af9f700
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-13 15:17:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (98.76%)

--- DSA/0206-reverse-linked-list/solution.java
+++ DSA/0206-reverse-linked-list/solution.java
@@ -14,7 +14,7 @@ class Solution {
             return head;
         }
 
-        ListNode newHead = reverseList(head.next);
+        ListNode newHead=reverseList(head.next);
         ListNode front=head.next;
         front.next=head;
         head.next=null;
