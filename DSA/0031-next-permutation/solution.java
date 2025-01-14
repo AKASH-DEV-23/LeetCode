5dc187409163a14318422244d7905e552d2f352a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-14 14:54:07 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 43.3 MB (99.91%)

--- DSA/0031-next-permutation/solution.java
+++ DSA/0031-next-permutation/solution.java
@@ -1,27 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i>=0 && nums[i] >= nums[i + 1]){
-            i--;
-        }
-        if (i != -1) {
-            int j= nums.length-1;
-            while (j>=0 && nums[i] >= nums[j]) {
-                j--;
+        int n=nums.length;
+        int idx=-1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
             }
-            swap(nums, i, j);
         }
-        int start = i + 1;
-        int end = nums.length - 1;
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+        if(idx==-1){
+            reverse(nums,idx+1,n-1);
+            return;
+        }
+        for(int i=n-1;i>=idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums,i,idx);
+                break;
+            }
+        }
+
+        reverse(nums,idx+1,n-1);
+    }
+
+    private void reverse(int[] nums, int i, int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
     }
-    public static void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    private void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
