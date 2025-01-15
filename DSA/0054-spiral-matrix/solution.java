be783008ef30fad07a93e06833544f75f0aed63a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-15 19:43:16 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.44%)

--- DSA/0054-spiral-matrix/solution.java
+++ DSA/0054-spiral-matrix/solution.java
@@ -1,35 +1,34 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> list=new ArrayList<>();
+    public List<Integer> spiralOrder(int[][] mat) {
         int rowBegin=0;
-        int rowEnd=matrix.length-1;
         int colBegin=0;
-        int colEnd=matrix[0].length-1;
+        int rowEnd=mat.length-1;
+        int colEnd=mat[0].length-1;
+
+        List<Integer> list=new ArrayList<>();
 
         while(rowBegin<=rowEnd && colBegin<=colEnd){
             for(int i=colBegin;i<=colEnd;i++){
-                list.add(matrix[rowBegin][i]);
+                list.add(mat[rowBegin][i]);
             }
             rowBegin++;
 
             for(int i=rowBegin;i<=rowEnd;i++){
-                list.add(matrix[i][colEnd]);
+                list.add(mat[i][colEnd]);
             }
             colEnd--;
 
             if(rowBegin<=rowEnd){
-                for(int i=colEnd;i>=colBegin;i--){
-                    list.add(matrix[rowEnd][i]);
-                }
-                rowEnd--;
+            for(int i=colEnd;i>=colBegin;i--){
+                list.add(mat[rowEnd][i]);
             }
+            rowEnd--;}
 
             if(colBegin<=colEnd){
-                for(int i=rowEnd;i>=rowBegin;i--){
-                    list.add(matrix[i][colBegin]);
-                }
-                colBegin++;
+            for(int i=rowEnd;i>=rowBegin;i--){
+                list.add(mat[i][colBegin]);
             }
+            colBegin++;}
         }
         return list;
     }
