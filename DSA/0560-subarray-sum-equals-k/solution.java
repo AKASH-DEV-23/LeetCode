23ec93403e410780b74627a763a521e9793c2310
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-16 16:21:49 +0000

    [Commit] Runtime - 23 ms (92.85%), Memory - 46.5 MB (99.95%)

--- DSA/0560-subarray-sum-equals-k/solution.java
+++ DSA/0560-subarray-sum-equals-k/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count =0;
+        Map<Integer, Integer> map=new HashMap<>();
+        map.put(0,1);
+        int prefixSum=0;
+        int count=0;
+
         for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
+            prefixSum+=nums[i];
+            int prevSum=prefixSum-k;
+            if(map.containsKey(prevSum))    count+=map.get(prevSum);
+            map.put(prefixSum, map.getOrDefault(prefixSum,0)+1);
         }
         return count;
     }
