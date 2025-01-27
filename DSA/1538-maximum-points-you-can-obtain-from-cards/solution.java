e68caf7a0e4b00d9df8f18d932a9db4194b04bac
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-27 12:18:04 +0000

    [commit] Runtime - 2 ms (99.48%), Memory - 55 MB (99.03%)

---
+++ DSA/1538-maximum-points-you-can-obtain-from-cards/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxScore(int[] nums, int k) {
+        int lsum=0;
+        int rsum=0;
+        int maxSum=0;
+        for(int i=0;i<k;i++){
+            lsum+=nums[i];
+        }
+        maxSum=lsum;
+        int ridx=nums.length-1;
+        for(int i=k-1;i>=0;i--){
+            lsum-=nums[i];
+            rsum+=nums[ridx];
+            ridx--;
+            maxSum=Math.max(maxSum,lsum+rsum);
+        }
+        return maxSum;
+        
+    }
+}
