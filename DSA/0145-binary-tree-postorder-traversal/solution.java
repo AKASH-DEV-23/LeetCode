68097b7fda7937e778587c76737064da3548ce69
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-31 16:28:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (99.32%)

--- DSA/0145-binary-tree-postorder-traversal/solution.java
+++ DSA/0145-binary-tree-postorder-traversal/solution.java
@@ -1,32 +1,28 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
-
     public List<Integer> postorderTraversal(TreeNode root) {
-        postorder(root);
+        List<Integer> list = new ArrayList<>();
+        postorder(root,list);
         return list;
     }
-
-    private void postorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        postorder(root.left);
-        postorder(root.right);
+    private void postorder(TreeNode root, List<Integer> list){
+        if(root==null)  return;
+        postorder(root.left,list);
+        postorder(root.right,list);
         list.add(root.val);
     }
 }
