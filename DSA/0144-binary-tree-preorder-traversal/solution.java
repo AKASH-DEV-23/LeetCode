8d1b096d4946b473cbc6836e27ed11a2ff6bb403
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-01-31 15:55:49 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.5 MB (99.98%)

--- DSA/0144-binary-tree-preorder-traversal/solution.java
+++ DSA/0144-binary-tree-preorder-traversal/solution.java
@@ -1,33 +1,28 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;+
+ *     }
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
-
     public List<Integer> preorderTraversal(TreeNode root) {
-        preorder(root);
+        List<Integer> list = new ArrayList<>();
+        preorder(root,list);
         return list;
-
     }
-
-    private void preorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+    private void preorder(TreeNode root, List<Integer> list){
+        if(root==null)  return;
         list.add(root.val);
-        preorder(root.left);
-        preorder(root.right);
+        preorder(root.left,list);
+        preorder(root.right,list);
     }
 }
