c85b795ae4590498adba3568da49e67391afa0d3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-07 13:00:02 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 42.2 MB (97.05%)

--- DSA/0101-symmetric-tree/solution.java
+++ DSA/0101-symmetric-tree/solution.java
@@ -15,21 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root==null){
-            return true;
-        }
-        return helper(root.left,root.right);
+        if(root==null)  return true;
+        return isSame(root.left,root.right);
     }
-
-    private static boolean helper(TreeNode root1,TreeNode root2){
-        if(root1==null || root2==null){
-            return root1==root2;
-        }
-
-        if(root1.val !=root2.val){
-            return false;
-        }
-
-        return helper(root1.left,root2.right) && helper(root1.right,root2.left);
+    private boolean isSame(TreeNode root1, TreeNode root2){
+        if(root1==null && root2==null)  return true;
+        if(root1==null || root2==null)  return false;
+        if(root1.val != root2.val)    return false;
+        return isSame(root1.left,root2.right) && isSame(root1.right,root2.left);
     }
 }
