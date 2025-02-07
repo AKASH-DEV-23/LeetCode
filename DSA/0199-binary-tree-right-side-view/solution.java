b1059842d7556782605c6ef47bb64bbfce5819a4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-07 13:01:07 +0000

    Sync LeetCode submission Runtime - 1 ms (73.24%), Memory - 42.2 MB (98.87%)

--- DSA/0199-binary-tree-right-side-view/solution.java
+++ DSA/0199-binary-tree-right-side-view/solution.java
@@ -14,19 +14,18 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
         if(root==null)  return list;
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
-
         while(!q.isEmpty()){
-            int n = q.size();
+            int n=q.size();
             for(int i=0;i<n;i++){
-                TreeNode  curr=q.poll();
+                TreeNode curr=q.poll();
                 if(i==0)    list.add(curr.val);
                 if(curr.right!=null)    q.offer(curr.right);
-                if(curr.left!=null) q.offer(curr.left);
+                if(curr.left!=null)    q.offer(curr.left);
             }
         }
         return list;
