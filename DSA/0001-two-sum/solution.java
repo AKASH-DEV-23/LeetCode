1d239a66fd09d63e080b53a91dde0bab810bbe5d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-10 17:59:23 +0000

    [Commit] Runtime - 2 ms (99.12%), Memory - 45.1 MB (99.85%)

--- DSA/0001-two-sum/solution.java
+++ DSA/0001-two-sum/solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map=new HashMap<>();
-
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int diff=target-nums[i];
             if(map.containsKey(diff)){
                 return new int[]{i,map.get(diff)};
-            }else{
-                map.put(nums[i],i);
             }
+            map.put(nums[i],i);
         }
-        return new int[]{0,0};
+        return new int[0];
     }
 }
