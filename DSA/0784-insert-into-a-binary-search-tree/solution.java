3ecaf2772936ba2fae12ca1389410023a9aa1b3c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-10 19:33:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.3 MB (99.13%)

--- DSA/0784-insert-into-a-binary-search-tree/solution.java
+++ DSA/0784-insert-into-a-binary-search-tree/solution.java
@@ -16,13 +16,12 @@
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root==null){
-            return root=new TreeNode(val);
+            return new TreeNode(val);
         }
         if(val<root.val){
-            root.left=insertIntoBST(root.left,val);
-        }
-        else{
-            root.right=insertIntoBST(root.right,val);
+            root.left = insertIntoBST(root.left,val);
+        }else{
+            root.right =  insertIntoBST(root.right,val);
         }
         return root;
     }
