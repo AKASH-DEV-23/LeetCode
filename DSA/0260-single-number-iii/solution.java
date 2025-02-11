3582a4054c97b861605b3058752a6c87bb473bbc
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-11 12:21:47 +0000

    [Commit] Runtime - 4 ms (43.17%), Memory - 45.4 MB (99.85%)

--- DSA/0260-single-number-iii/solution.java
+++ DSA/0260-single-number-iii/solution.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[2];
-        int index = 0;
-
-        for (int i = 0; i < n; i++) {
-            boolean found = false;
-            for (int j = 0; j < n; j++) {
-                if (i != j && nums[i] == nums[j]) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                result[index++] = nums[i];
-                if (index == 2) {
-                    break;
-                }
+        int[] ans = new int[2];
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            if(!set.contains(num)){
+                set.add(num);
+            }else{
+                set.remove(num);
             }
         }
-
-        return result;
+        int idx=0;
+        for(int num:set){
+            ans[idx++]=num;
+        }
+        return ans;
     }
 }
