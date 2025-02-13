d8e86c1df620bd7dd69f4d16e62a56750ed06ddb
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-02-13 16:24:09 +0000

    Sync LeetCode submission Runtime - 1 ms (35.69%), Memory - 41.8 MB (99.17%)

--- DSA/0058-length-of-last-word/solution.java
+++ DSA/0058-length-of-last-word/solution.java
@@ -1,15 +1,8 @@
 class Solution {
-public static int lengthOfLastWord(String s) {
-        String str = s.trim();
-        int count = 0;
-        for(int i = str.length()-1; i>=0;i--){
-            if(str.charAt(i) != ' '){
-                count++;
-            } else {
-                break;
-            }
-        }
-        return count;
+    public int lengthOfLastWord(String s) {
+        s.trim();
+        int j=s.length()-1;
+        String[] str=s.split(" ");
+        return str[str.length-1].length();
     }
-
 }
