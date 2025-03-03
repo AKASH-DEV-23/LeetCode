017fb9372889f8b6dd9244e7ce48e1db7d13e080
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-03 12:24:51 +0530

    Time: 3 ms (46.03%) | Memory: 45.5 MB (31.18%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,60 +1,18 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
+        Map<Integer,Integer> map=new TreeMap<>();
+        for(int[] num:nums1){
+            map.put(num[0],num[1]);
+        }
+        for(int[] num:nums2){
+            map.put(num[0],map.getOrDefault(num[0],0)+num[1]);
+        }
+        int[][] ans = new int[map.size()][2];
         int i=0;
-        int j=0;
-        List<List<Integer>> ans = new ArrayList<>();
-        while(i<n1 && j<n2){
-            int id1=nums1[i][0];
-            int id2=nums2[j][0];
-            int val1=nums1[i][1];
-            int val2=nums2[j][1];
-            List<Integer> list=new ArrayList<>();
-            if(id1==id2){
-                list.add(id1);
-                list.add(val1+val2);
-                i++;
-                j++;
-            }else if(id1<id2){
-                list.add(id1);
-                list.add(val1);
-                i++;
-            }else{
-                list.add(id2);
-                list.add(val2);
-                j++;
-            }
-            ans.add(list);
+        for(int key:map.keySet()){
+            ans[i][0]=key;
+            ans[i++][1]=map.get(key);
         }
-
-        while(i<n1){
-            List<Integer> list = new ArrayList<>();
-            list.add(nums1[i][0]);
-            list.add(nums1[i][1]);
-            i++;
-            ans.add(list);
-        }
-        while(j<n2){
-            List<Integer> list = new ArrayList<>();
-            list.add(nums2[j][0]);
-            list.add(nums2[j][1]);
-            j++;
-            ans.add(list);
-        }
-
-        int[][] res=new int[ans.size()][2];
-
-        for( i=0;i<ans.size();i++){
-            int k=0;
-            for(int num:ans.get(i)){
-                if(k==0)
-                    res[i][0]=num;
-                else
-                    res[i][1]=num;
-                k=1;
-            }
-        }
-        return res;
+        return ans;
     }
 }
