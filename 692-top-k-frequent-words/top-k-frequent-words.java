43204c2aa30e09b63de435df7785f625ef89d78b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-05 01:36:19 +0530

    Time: 9 ms (15.99%) | Memory: 44.5 MB (94.19%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> map = new HashMap<>();
+        List<String> ans = new ArrayList<>();
+        for(String str : words){
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        List<Map.Entry<String,Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
+            .thenComparing(Map.Entry.comparingByKey()));
+
+        int i=0;
+        while(k>0){
+            Map.Entry<String, Integer> sorted = list.get(i++);
+            ans.add(sorted.getKey());
+            k--;
+        }
+        // System.out.println(list);
+        return ans; 
+    }
+}
