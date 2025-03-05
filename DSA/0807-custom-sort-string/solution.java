126aac04d8462928da8b77575b3724b138764895
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-05 03:42:26 +0000

    Sync LeetCode submission Runtime - 2 ms (61.28%), Memory - 41.7 MB (98.88%)

--- DSA/0807-custom-sort-string/solution.java
+++ DSA/0807-custom-sort-string/solution.java
@@ -1,25 +1,28 @@
 class Solution {
     public String customSortString(String order, String s) {
-        int[] freq=new int[26];
-        for(char ch:s.toCharArray()){
-            freq[ch-'a']++;
+        Map<Character, Integer>  map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        StringBuilder ans= new StringBuilder();
-        for(int i=order.length()-1;i>=0;i--){
-            char ch=order.charAt(i);
-            while(freq[ch-'a']>0){
-                ans.append(ch+"");
-                freq[ch-'a']--;
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<order.length();i++){
+            char ch = order.charAt(i);
+            if(map.containsKey(ch)){
+                int cnt = map.get(ch);
+                while(cnt>0){
+                    ans.append(ch);
+                    cnt--;
+                }
+                map.remove(ch);
             }
         }
-        for(int i=0;i<26;i++){
-            int cnt=freq[i];
-            char ch=(char)('a'+i);
+        for(char key:map.keySet()){
+            int cnt=map.get(key);
             while(cnt>0){
-                ans.append(ch);
+                ans.append(key);
                 cnt--;
             }
         }
-        return ans.reverse().toString();
+        return ans.toString();
     }
 }
