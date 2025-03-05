bee90e36f6a13fd4fb0d81de3346def619adf783
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-05 10:42:47 +0000

    [commit] Runtime - 97 ms (10.00%), Memory - 45.4 MB (98.00%)

---
+++ DSA/2320-find-all-k-distant-indices-in-an-array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==key) set.add(i);
+        }
+        int n=nums.length;
+        Set<Integer> ans = new HashSet<>();
+        for(Integer idx:set){
+            int i=0;
+            while(i<n){
+                if(Math.abs(i-idx)<=k)  ans.add(i);
+                i++;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int num:ans){
+            list.add(num);
+        }
+    //  Collections.sort(list);
+        list.sort(null);
+        return list;
+    }
+}
