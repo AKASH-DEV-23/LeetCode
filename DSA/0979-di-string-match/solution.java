f24a88b6cff7e41ad8ae1b8502a5eb3d4cf77892
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-09 03:34:21 +0000

    Sync LeetCode submission Runtime - 2 ms (97.18%), Memory - 45.2 MB (99.42%)

--- DSA/0979-di-string-match/solution.java
+++ DSA/0979-di-string-match/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] diStringMatch(String s) {
-        int low=0;
-        int high=s.length();
-        int[] ans=new int[high+1];
-        int idx=0;
+        int[] ans = new int[s.length()+1];
+        int maxi=s.length();
+        int mini=0;
+        int i=0;
         for(char ch:s.toCharArray()){
-            if(ch=='I')    ans[idx++]=low++;
-            else    ans[idx++]=high--;
+            if(ch=='D') ans[i++]=maxi--;
+            else    ans[i++]=mini++;
         }
-        ans[idx]=s.charAt(s.length()-1)=='I' ? low : high;
+        ans[ans.length-1]=(s.length()-1)=='D' ? maxi : mini;
         return ans;
     }
 }
