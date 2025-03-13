314a6766d951647d2f84808cff78498581681a14
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-13 12:06:56 +0530

    Time: 2 ms (99.11%) | Memory: 44.5 MB (42.95%) - LeetSync

--- 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.java
+++ 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.java
@@ -1,20 +1,18 @@
 class Solution {
-    public boolean canTransform(String start, String target) {
+    public boolean canTransform(String start, String result) {
         int i=0;
         int j=0;
-        char[] st = start.toCharArray();
-        char[] tar = target.toCharArray();
-        while(i<start.length() || j<target.length()){
-            while(i<st.length && st[i]=='X')   i++;
-            while(j<tar.length && tar[j]=='X')   j++;
-            if(i==st.length || j==tar.length)   return i==st.length && j==tar.length;
-            if(st[i] != tar[j]) return false;
-            if(st[i]=='L' && i<j){
-                return false;
-            }
-            if(st[i]=='R' && i>j){
-                return false;
-            }
+        int n=start.length();
+        int m=n;
+        char[] st=start.toCharArray();
+        char[] res=result.toCharArray();
+        while(i<n || j<m){
+            while(i<n && st[i]=='X')    i++;
+            while(j<m && res[j]=='X')    j++;
+            if(i==n || j==m)    return i==n && j==m;
+            if(st[i] != res[j])   return false;
+            if(st[i]=='L' && i<j)   return false;
+            if(st[i]=='R' && i>j)   return false;
             i++;
             j++;
         }
