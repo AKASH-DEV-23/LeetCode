07c4f5ee05873accdaccd078d27433d25ea3313d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-14 18:42:21 +0000

    [Commit] Runtime - 1 ms (99.91%), Memory - 42.7 MB (99.91%)

--- DSA/0415-add-strings/solution.java
+++ DSA/0415-add-strings/solution.java
@@ -30,6 +30,7 @@ class Solution {
             j--;
         }
         if(carry!=0)    str.append(carry);
+        // System.out.println(str.toString());
         return str.reverse().toString();
     }
 }
