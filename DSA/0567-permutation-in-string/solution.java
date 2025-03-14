2ac167c36e8b74f5fd8832af4e0dd823f53099fb
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-14 18:26:37 +0000

    [Commit] Runtime - 4 ms (99.62%), Memory - 42.5 MB (99.96%)

--- DSA/0567-permutation-in-string/solution.java
+++ DSA/0567-permutation-in-string/solution.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] count1=new int[26];
-        int[] count2=new int[26];
-        int len1=s1.length();
-        int len2=s2.length();
-        if(len1>len2)   return false;
-        for(int i=0;i<len1;i++){
-            count1[s1.charAt(i)-'a']++;
-            count2[s2.charAt(i)-'a']++;
+        int[] arr=new int[26];
+        for(char ch:s1.toCharArray())   arr[ch-'a']++;
+        int j=0;
+        int n=s1.length();
+        for(int i=0;i<s2.length();i++){
+            arr[s2.charAt(i)-'a']--;
+            if(i-j+1==n){
+                if(checkZeros(arr)) return true;
+                arr[s2.charAt(j)-'a']++;
+                j++;
+            }
         }
-        for(int i=len1;i<len2;i++){
-            if(Arrays.equals(count1,count2))     return true;
-            count2[s2.charAt(i)-'a']++;
-            count2[s2.charAt(i-len1)-'a']--;
-        }
-        if(Arrays.equals(count1,count2))    return true;
         return false;
+    }
+    private boolean checkZeros(int[] arr){
+        for(int num:arr){
+            if(num!=0)  return false;
+        }
+        return true;
     }
 }
