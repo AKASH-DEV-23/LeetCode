375c6d2c16af7f4fa0d60f60d3ebb408e4e7d6cf
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-23 08:01:04 +0000

    [commit] Runtime - 1 ms (80.98%), Memory - 40.5 MB (99.03%)

---
+++ DSA/1448-maximum-69-number/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximum69Number (int num) {
+       String str=num+"";
+       char ch[]=str.toCharArray();
+       for(int i=0;i<ch.length;i++){
+            if(ch[i]=='6'){
+                ch[i]='9';
+                break;
+            }
+       }
+       int ans=0;
+       for(char che:ch){
+        ans=ans*10+(che-'0');
+       }
+
+       return ans;
+    }
+}
