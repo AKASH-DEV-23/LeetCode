2eb5712324a62518d6befc251f2d397346662323
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-03-24 11:52:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (72.55%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.java
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.java
@@ -1,16 +1,9 @@
 class Solution {
     public int minTimeToType(String word) {
-        Map<Character,Integer> map=new HashMap<>();
-        char che='a';
-        map.put('a',1);
-        for(int i=2;i<=26;i++){
-            che++;
-            map.put(che,i);
-        }
         char curr='a';
         int sum=0;
         for(char ch:word.toCharArray()){
-            int val=Math.abs(map.get(curr)-map.get(ch));
+            int val=Math.abs(curr-ch);
             int mini=Math.min(val,26-val);
             sum+=mini;
             curr=ch;
