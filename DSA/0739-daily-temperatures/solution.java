ee2c91684aac42ecc14074246fc5c6735526ca6e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-01 06:43:30 +0000

    Sync LeetCode submission Runtime - 81 ms (9.42%), Memory - 60.1 MB (99.38%)

--- DSA/0739-daily-temperatures/solution.java
+++ DSA/0739-daily-temperatures/solution.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int size=temperatures.length;
-        int[] ans=new int[size];
-        size-=1;
+        int n=temperatures.length;
+        int answer[]=new int[n];
+        answer[n-1]=0;
         Stack<Integer> st=new Stack<>();
-        while(size>=0){
-            while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[size]) st.pop();
-            if(st.isEmpty())    ans[size]=0;
-            else    ans[size]=st.peek()-size;
-            st.push(size--);
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && temperatures[st.peek()] <= temperatures[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                answer[i]=0;
+            }else{
+                answer[i]=st.peek()-i;
+            }
+            st.push(i);
         }
-        return ans;
+        return answer;
     }
 }
