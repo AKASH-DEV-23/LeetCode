8c8f2fdf39f04654da82420da7ceffbc792316a2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-04 14:09:13 +0000

    [commit] Runtime - 2 ms (99.16%), Memory - 45.9 MB (98.56%)

--- DSA/0621-task-scheduler/solution.java
+++ DSA/0621-task-scheduler/solution.java
@@ -5,13 +5,9 @@ class Solution {
         for(char ch:tasks){
             freq[ch-'A']++;
         }
-
         Arrays.sort(freq);
         int i=25;
         while(i>=0 && freq[i]==freq[25]) i--;
-
         return Math.max(tasks.length, (freq[25]-1)*(n+1)+25-i);
-        
-
     }
 }
