81f3d86f7fca6981fa0612f19b01d9c64541f534
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-09 05:39:01 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 40.1 MB (100.00%)

--- DSA/0070-climbing-stairs/solution.java
+++ DSA/0070-climbing-stairs/solution.java
@@ -1,13 +1,16 @@
 class Solution {
     int[] dp;
     public int climbStairs(int n) {
-        dp=new int[n+1];
-       Arrays.fill(dp,-1);
-       return solve(n);
+        dp=new int[45+1];
+        Arrays.fill(dp,-1);
+        return helper(n);
     }
-    private int solve(int n){
-        if(n==1 || n==2)    return n;    
-        if(dp[n] != -1)   return dp[n];
-        return dp[n]= (solve(n-1)+solve(n-2));
+    private int helper(int n){
+        if(n==0)    return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int one=helper(n-1);
+        int two=helper(n-2);
+        return dp[n] = one+two;
     }
 }
