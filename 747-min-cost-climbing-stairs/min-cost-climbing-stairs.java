1c7b6f681febb4d537300290961b4f56360fd6ab
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-09 19:06:27 +0530

    Time: 1 ms (38.07%) | Memory: 42.6 MB (98.98%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -6,7 +6,8 @@ class Solution {
         Arrays.fill(dp,-1);
         int min1 = helper(cost, 0);
         int min2 = helper(cost, 1);
-        return Math.min(min1, min2);
+        // return Math.min(min1, min2);
+        return bottomUp(cost);
     }
 
     private int helper(int[] nums, int idx) {
@@ -16,5 +17,11 @@ class Solution {
         int mini1 = nums[idx]+helper(nums, idx + 1);
         int mini2 = nums[idx]+helper(nums, idx + 2);
         return dp[idx] = Math.min(mini1, mini2);
+    }
+    private int bottomUp(int[] nums){
+        for(int i=2;i<nums.length;i++){
+            nums[i]=nums[i]+Math.min(nums[i-1],nums[i-2]);
+        }
+        return Math.min(nums[nums.length-1],nums[nums.length-2]);
     }
 }
