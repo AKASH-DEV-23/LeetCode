2128334ccf3a0243ea8418f5cf1d1c0a107470b4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-10 19:24:46 +0000

    [Commit] Runtime - 2 ms (81.80%), Memory - 45.9 MB (99.98%)

--- DSA/0130-surrounded-regions/solution.java
+++ DSA/0130-surrounded-regions/solution.java
@@ -1,51 +1,29 @@
 class Solution {
     public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int i=0;i<n;i++){
-            if(board[0][i]=='O'){
-                DFS(board,0,i);
-            }
-            if(board[m-1][i]=='O'){
-                DFS(board,m-1,i);
-            }
+        int row=board.length;
+        int col=board[0].length;
+        for(int i=0;i<col;i++){
+            if(board[0][i]=='O')    dfs(board,0,i);
+            if(board[row-1][i]=='O')    dfs(board,row-1,i);
         }
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O'){
-                DFS(board,i,0);
-            }
-            if(board[i][n-1]=='O'){
-                DFS(board,i,n-1);
-            }
+        for(int i=0;i<row;i++){
+            if(board[i][0]=='O')    dfs(board,i,0);
+            if(board[i][col-1]=='O')    dfs(board,i,col-1);
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O'){
-                    DFS1(board,i,j);
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(board));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='Y')    board[i][j]='O';
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j]=='O')    board[i][j]='X';
+                if(board[i][j]=='T')    board[i][j]='O';
             }
         }
     }
-    private void DFS(char[][] grid, int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='O')    return;
-        grid[i][j]='Y';
-        DFS(grid,i-1,j);
-        DFS(grid,i+1,j);
-        DFS(grid,i,j-1);
-        DFS(grid,i,j+1);
-    }
-    private void DFS1(char[][] grid, int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='O')    return;
-        grid[i][j]='X';
-        DFS1(grid,i-1,j);
-        DFS1(grid,i+1,j);
-        DFS1(grid,i,j-1);
-        DFS1(grid,i,j+1);
+    private void dfs(char[][] board, int row, int col){
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length ||
+            board[row][col]!='O')         return;
+        board[row][col]='T';
+        dfs(board,row+1,col);
+        dfs(board,row-1,col);
+        dfs(board,row,col+1);
+        dfs(board,row,col-1);
     }
 }
