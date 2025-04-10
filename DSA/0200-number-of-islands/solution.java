6a99ba2c2cdd7aec28c3221794a5ac7194729b97
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-10 06:18:16 +0000

    [Commit] Runtime - 2 ms (99.82%), Memory - 49.9 MB (99.99%)

--- DSA/0200-number-of-islands/solution.java
+++ DSA/0200-number-of-islands/solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int islands=0;
+        int row=grid.length;
+        int col=grid[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]=='1'){
-                    DFS(grid,i,j);
-                    cnt++;
+                    islands++;
+                    dfs(grid,i,j);
                 }
             }
         }
-        return cnt;
+        return islands;
     }
-    private void DFS(char[][] grid, int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1')  return;
+    private void dfs(char[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0') return;
         grid[i][j]='0';
-        DFS(grid,i-1,j);
-        DFS(grid,i+1,j);
-        DFS(grid,i,j-1);
-        DFS(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
 }
