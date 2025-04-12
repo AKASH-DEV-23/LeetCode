721844a048a023e4bda1d47c0f49c44e2f134585
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-12 04:36:34 +0000

    [Commit] Runtime - 8 ms (5.83%), Memory - 47.4 MB (33.63%)

--- DSA/0547-number-of-provinces/solution.java
+++ DSA/0547-number-of-provinces/solution.java
@@ -1,31 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int V=isConnected.length;
-        Map<Integer,List<Integer>> adj=new HashMap<>();
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if( i!=j && isConnected[i][j]==1){
-                    if(!adj.containsKey(i)) adj.put(i,new ArrayList<>());
+        int n=isConnected.length;
+        Map<Integer, ArrayList<Integer>> adj=new HashMap<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    if(!adj.containsKey(i))   adj.put(i,new ArrayList<Integer>());
+                    if(!adj.containsKey(j)) adj.put(j,new ArrayList<>());
                     adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-        boolean[] visited=new boolean[V];
         int cnt=0;
-        for(int i=0;i<V;i++){
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<n;i++){
             if(!visited[i]){
-                DFS(adj,visited,i);
+                dfs(adj,i,visited);
                 cnt++;
             }
-        } 
+        }
         return cnt;
     }
-    private void DFS(Map<Integer,List<Integer>> adj, boolean[] visited, int u){
-        visited[u]=true;
-        for(int v:adj.getOrDefault(u,new ArrayList<>())){
-            if(!visited[v]){
-                DFS(adj,visited,v);
-            }
+
+    private void dfs(Map<Integer, ArrayList<Integer>> adj, int vertex, boolean[] visited){
+        visited[vertex]=true;
+        for(int node:adj.get(vertex)){
+            if(!visited[node])  dfs(adj,node,visited);
         }
     }
 }
