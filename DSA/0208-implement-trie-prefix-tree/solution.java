4b4533b6674eabfcc3d1f48d10d20ff749a2094e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-16 06:31:11 +0000

    [Commit] Runtime - 41 ms (25.98%), Memory - 56.2 MB (97.83%)

--- DSA/0208-implement-trie-prefix-tree/solution.java
+++ DSA/0208-implement-trie-prefix-tree/solution.java
@@ -1,41 +1,47 @@
-class TrieNode{
+class TrieNode {
+    char val;
+    boolean isWord;
     TrieNode[] children=new TrieNode[26];
-    boolean isEnd=false;
+    TrieNode(){}
+    TrieNode(char c){
+        TrieNode node = new TrieNode();
+        node.val=c;
+    }
 }
 class Trie {
     TrieNode root;
-    public Trie() {
+    Trie(){
         root=new TrieNode();
+        root.val=' ';
     }
-    
     public void insert(String word) {
-        TrieNode node=root;
-        for(char ch:word.toCharArray()){
-            int idx=ch-'a';
-            if(node.children[idx]==null){
-                node.children[idx]=new TrieNode();
+        TrieNode ws=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(ws.children[c-'a']==null){
+                ws.children[c-'a']=new TrieNode(c);
             }
-            node=node.children[idx];
+            ws=ws.children[c-'a'];
         }
-        node.isEnd=true;
+        ws.isWord=true;
     }
-    
-    public boolean search(String word) {
-        TrieNode node=root;
-        for(char ch:word.toCharArray()){
-            int idx=ch-'a';
-            if(node.children[idx]==null)    return false;
-            node=node.children[idx];
+
+    public boolean search(String word){
+        TrieNode ws=root;
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(ws.children[c-'a']==null)    return false;
+            ws=ws.children[c-'a'];
         }
-        return node.isEnd;
+        return ws.isWord;
     }
     
     public boolean startsWith(String prefix) {
-        TrieNode node=root;
-        for(char ch:prefix.toCharArray()){
-            int idx=ch-'a';
-            if(node.children[idx]==null)    return false;   
-            node=node.children[idx];
+        TrieNode ws=root;
+        for(int i=0;i<prefix.length();i++){
+            char ch=prefix.charAt(i);
+            if(ws.children[ch-'a']==null)    return false;
+            ws=ws.children[ch-'a'];
         }
         return true;
     }
