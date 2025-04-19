bfedddb3893436a9e567327840c071eaa4bb4b9a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-04-19 13:44:58 +0000

    [Commit] Runtime - 7 ms (98.43%), Memory - 46.8 MB (99.99%)

--- DSA/0056-merge-intervals/solution.java
+++ DSA/0056-merge-intervals/solution.java
@@ -1,7 +1,14 @@
 class Solution {
     public int[][] merge(int[][] nums) {
         List<int[]> list=new ArrayList<>();
-        Arrays.sort(nums, (a,b)->Integer.compare(a[0],b[0]));
+        Comparator<int[]> com=new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                if(a[0]<b[0])   return -1;
+                else if(a[0]>b[0])  return 1;
+                else    return 0;
+            }
+        };
+        Arrays.sort(nums,com);
         int[] interval=nums[0];
         list.add(interval);
         for(int newInterval[]:nums ){
