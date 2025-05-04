19330eab7aae6cdf27cebe5f7c26b3495a497c18
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-04 18:44:15 +0530

    Time: 2149 ms (6.11%) | Memory: 52.7 MB (82.70%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
@@ -1,17 +1,5 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
-        Comparator<int[]> com=new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                if(a[0]<b[0])   return -1;
-                else if(a[0]>b[0])  return 1;
-                else{
-                    if(a[1]<b[1])   return -1;
-                    else if(a[1]>b[1])  return 1;
-                    else    return 0;
-                }
-            }
-        };
-        // Arrays.sort(dominoes,com);
         for(int[] a:dominoes){
             Arrays.sort(a);
         }
@@ -23,7 +11,6 @@ class Solution {
                 }
             }
         }
-        System.out.println(Arrays.deepToString(dominoes));
         return cnt;
     }
 }
