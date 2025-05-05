1b54d1ec30f08648016e375f5891db7e5e2e39f4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-05 06:20:48 +0000

    [commit] Runtime - 1 ms (4.11%), Memory - 41.9 MB (98.91%)

--- DSA/0027-remove-element/solution.java
+++ DSA/0027-remove-element/solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int i=0;
+        int n=nums.length;
+        List<Integer> list=new ArrayList<>();
         for(int num:nums){
-            if(num!=val){
-                nums[i++]=num;
-            }
+            if(num!=val)    list.add(num);
+        }
+        for(int num:list){
+            nums[i++]=num;
         }
         return i;
     }
