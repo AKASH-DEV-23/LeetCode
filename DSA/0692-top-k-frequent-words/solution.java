fd83801a8a8bc1e51a5bf7d3ed3348b1f9d93d15
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-11 08:12:50 +0000

    [Commit] Runtime - 6 ms (99.52%), Memory - 44.8 MB (99.97%)

--- DSA/0692-top-k-frequent-words/solution.java
+++ DSA/0692-top-k-frequent-words/solution.java
@@ -1,19 +1,24 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String,Integer> map = new HashMap<>();
-        List<String> ans = new ArrayList<>();
-        for(String str : words){
-            map.put(str,map.getOrDefault(str,0)+1);
+        Map<String,Integer> map=new HashMap<>();
+        for(String s:words){
+            map.put(s,map.getOrDefault(s,0)+1);
         }
-        List<Map.Entry<String,Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
-            .thenComparing(Map.Entry.comparingByKey()));
-        int i=0;
-        while(k>0){
-            Map.Entry<String, Integer> sorted = list.get(i++);
-            ans.add(sorted.getKey());
-            k--;
+        List<Map.Entry<String,Integer>> list=new ArrayList<>(map.entrySet());
+        Comparator<Map.Entry<String,Integer>> com=new Comparator<>(){
+            public int compare(Map.Entry<String,Integer> a, Map.Entry<String,Integer> b){
+                if(a.getValue()>b.getValue())   return -1;
+                else if(a.getValue()<b.getValue())  return 1;
+                else{
+                    return a.getKey().compareTo(b.getKey());
+                }
+            }
+        };
+        Collections.sort(list,com);
+        List<String> ans=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            ans.add(list.get(i).getKey());
         }
-        return ans; 
+        return ans;
     }
 }
