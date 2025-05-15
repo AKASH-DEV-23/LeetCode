2d8e75780c8096e907a0cf97d871950fdfc08b01
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-15 09:22:10 +0000

    [Commit] Runtime - 3 ms (64.35%), Memory - 44 MB (92.28%)

--- DSA/0125-valid-palindrome/solution.java
+++ DSA/0125-valid-palindrome/solution.java
@@ -1,16 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
-        int i = 0;
-        int j = s.length()-1;
-        while(i<=j){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            } else{
-                return false;
+        String str=s.toLowerCase();
+        // System.out.println(str);
+        StringBuilder sb=new StringBuilder();
+        for(char ch:str.toCharArray()){
+            if(ch>='a' && ch<='z' || ch>='0' && ch<='9'){
+                sb.append(ch);
             }
         }
+        int i=0;
+        int j=sb.length()-1;
+        while(i<=j){
+            if(sb.charAt(i) != sb.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        // System.out.println(sb);
         return true;
     }
 }
