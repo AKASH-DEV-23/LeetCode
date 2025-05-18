d67cf8266717c099edcfad790fda64bcaad85934
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-18 11:07:18 +0000

    [Commit] Runtime - 10 ms (12.61%), Memory - 46.2 MB (99.07%)

--- DSA/0210-course-schedule-ii/solution.java
+++ DSA/0210-course-schedule-ii/solution.java
@@ -1,33 +1,51 @@
 class Solution {
+    boolean[] visited;
+    List<Integer> list;
+    int[] inDegree;
+    int cnt=0;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer,List<Integer>> adj=new HashMap<>();
-        int[] inDegree=new int[numCourses];
-        for(int[] pre:prerequisites){
-            int u=pre[1];
-            int v=pre[0];
-            if(!adj.containsKey(u))    adj.put(u,new ArrayList<>());
-            adj.get(u).add(v);
-            inDegree[v]++;
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int[] edge:prerequisites){
+            int u=edge[1];
+            int v=edge[0];
+            if(!map.containsKey(u)) map.put(u,new ArrayList<>());
+            map.get(u).add(v);
         }
-        Queue<Integer> myq=new LinkedList<>();
-        int[] ans=new int[numCourses];
-        int idx=0;
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0){
-                myq.offer(i);
+        visited=new boolean[numCourses];
+        inDegree=new int[numCourses];
+        for(int key:map.keySet()){
+            for(int v:map.get(key)){
+                inDegree[v]++;
             }
         }
-        while(!myq.isEmpty()){
-            int u=myq.poll();
-            ans[idx++]=u;
-            for(int v:adj.getOrDefault(u,new ArrayList<>())){
+        list=new ArrayList<>();
+        BFS(map);
+        if(cnt!=numCourses) return new int[]{};
+        int[] ans=new int[list.size()];
+        int i=0;
+        for(int num:list){
+            ans[i++]=num;
+        }
+        return ans;
+    }
+    private void BFS(Map<Integer,List<Integer>> map){
+       Queue<Integer> q=new LinkedList<>();
+       for(int i=0;i<inDegree.length;i++){
+            if(inDegree[i]==0){
+                q.offer(i);
+                cnt++;
+            }
+       }
+       while(!q.isEmpty()){
+            int u=q.poll();
+            list.add(u);
+            for(int v:map.getOrDefault(u,new ArrayList<>())){
                 inDegree[v]--;
                 if(inDegree[v]==0){
-                    myq.offer(v);
+                    q.offer(v);
+                    cnt++;
                 }
             }
-        }   
-        for(int num:inDegree)   if(num>0)   return new int[0];
-        return ans;
+        }
     }
 }
