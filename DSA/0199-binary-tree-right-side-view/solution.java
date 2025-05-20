80afe5a4624af10a3aff7b56d4fd195a6139ac43
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-20 01:39:20 +0000

    [Commit] Runtime - 1 ms (73.21%), Memory - 42.4 MB (99.92%)

--- DSA/0199-binary-tree-right-side-view/solution.java
+++ DSA/0199-binary-tree-right-side-view/solution.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        if(root==null)  return list;
+        List<Integer> ans=new ArrayList<>();
+        if(root==null)  return ans;
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
             int n=q.size();
             for(int i=0;i<n;i++){
                 TreeNode curr=q.poll();
-                if(i==0)    list.add(curr.val);
+                if(i==0)    ans.add(curr.val);
                 if(curr.right!=null)    q.offer(curr.right);
-                if(curr.left!=null)    q.offer(curr.left);
+                if(curr.left!=null)     q.offer(curr.left);
             }
         }
-        return list;
+        return ans;
     }
 }
