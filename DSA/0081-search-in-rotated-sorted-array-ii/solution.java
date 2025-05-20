206c9b4be693ffe04ba321424dbb1d8e9c91f744
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-20 02:36:51 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 43.6 MB (96.96%)

--- DSA/0081-search-in-rotated-sorted-array-ii/solution.java
+++ DSA/0081-search-in-rotated-sorted-array-ii/solution.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Set<Integer> set = new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-        if(set.contains(target)){
-            return true;
+        int st=0;
+        int end=nums.length-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target)   return true;
+            if(nums[st]==nums[mid] && nums[end]==nums[mid]){
+                st++;
+                end--;
+            }
+            else if(nums[st]<=nums[mid]){
+                if(nums[st]<=target && target<nums[mid])    end=mid-1;
+                else    st=mid+1;
+            }else{
+                if(nums[mid]<target && target<=nums[end])   st=mid+1;
+                else    end=mid-1;
+            }
         }
         return false;
     }
