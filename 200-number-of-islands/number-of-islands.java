2dc458102328826f646a9cce454083aa4c7cf3e1
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-24 00:18:47 +0530

    Time: 5 ms (33.46%) | Memory: 52.4 MB (15.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -13,7 +13,26 @@ class Solution {
         }
         return cnt;
     }
+
     private void BFS(char[][] grid, int i, int j){
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{i,j});
+        grid[i][j]='0';
+        int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        while(!q.isEmpty()){
+            int[] current=q.poll();
+            for(int[] dir:directions){
+                int newX=current[0]+dir[0];
+                int newY=current[1]+dir[1];
+                if(isSafe(grid,newX,newY)){
+                    q.offer(new int[]{newX,newY});
+                    grid[newX][newY]='0';
+                }
+            }
+        }
+    }
+
+    private void DFS(char[][] grid, int i, int j){
         if(!isSafe(grid,i,j))   return;
         grid[i][j]='0';
         BFS(grid,i-1,j);
@@ -21,6 +40,7 @@ class Solution {
         BFS(grid,i,j-1);
         BFS(grid,i,j+1);
     }
+    
     private boolean isSafe(char[][] grid, int i, int j){
         return i>=0 && i<grid.length && j>=0 && j<grid[0].length && grid[i][j]=='1';
     }
