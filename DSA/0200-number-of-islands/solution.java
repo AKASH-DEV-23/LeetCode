49650d7b390efd7ef13a35d352c304e5b1331e0c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-23 18:48:37 +0000

    [commit] Runtime - 5 ms (32.69%), Memory - 52.4 MB (30.06%)

--- DSA/0200-number-of-islands/solution.java
+++ DSA/0200-number-of-islands/solution.java
@@ -1,24 +1,47 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int islands=0;
-        int row=grid.length;
-        int col=grid[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        int m=grid.length;
+        int n=grid[0].length;
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    islands++;
-                    dfs(grid,i,j);
+                    BFS(grid,i,j);
+                    cnt++;
                 }
             }
         }
-        return islands;
+        return cnt;
     }
-    private void dfs(char[][] grid, int i, int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0') return;
+
+    private void BFS(char[][] grid, int i, int j){
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{i,j});
         grid[i][j]='0';
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
+        int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        while(!q.isEmpty()){
+            int[] current=q.poll();
+            for(int[] dir:directions){
+                int newX=current[0]+dir[0];
+                int newY=current[1]+dir[1];
+                if(isSafe(grid,newX,newY)){
+                    q.offer(new int[]{newX,newY});
+                    grid[newX][newY]='0';
+                }
+            }
+        }
+    }
+
+    private void DFS(char[][] grid, int i, int j){
+        if(!isSafe(grid,i,j))   return;
+        grid[i][j]='0';
+        BFS(grid,i-1,j);
+        BFS(grid,i+1,j);
+        BFS(grid,i,j-1);
+        BFS(grid,i,j+1);
+    }
+    
+    private boolean isSafe(char[][] grid, int i, int j){
+        return i>=0 && i<grid.length && j>=0 && j<grid[0].length && grid[i][j]=='1';
     }
 }
