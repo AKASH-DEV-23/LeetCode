fc53e1cfc4b80a13ad80b68ab0fea00b8a545712
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-24 10:52:38 +0000

    [Commit] Runtime - 2 ms (90.00%), Memory - 42.6 MB (99.94%)

--- DSA/1036-rotting-oranges/solution.java
+++ DSA/1036-rotting-oranges/solution.java
@@ -1,33 +1,57 @@
 class Solution {
-    int m;
-    int n;
-    int time=0;
+    int cnt=0;
     public int orangesRotting(int[][] grid) {
-        m=grid.length;
-        n=grid[0].length;
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean flag=true;
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    DFS(grid,i,j,2);
+                    q.offer(new int[]{i,j});
                 }
             }
         }
+        if(q.isEmpty()){
+            int fresh=0;
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(grid[i][j]==1){
+                        fresh++;
+                    }
+                }
+            }
+            return fresh>0?-1:0;
+        }
+        BFS(grid,q);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1)   return -1;
-                time=Math.max(time,grid[i][j]);
             }
         }
-        if(time==0) return 0;
-        return time==2 ? 0 : time-2;
+        return cnt-1;
     }
-    private void DFS(int[][] grid, int i, int j, int currTime){
-        if(i<0 || j<0 || i>=m || j>=n)  return;
-        if(grid[i][j]!=1 && grid[i][j]<currTime)    return;
-        grid[i][j]=currTime;
-        DFS(grid,i-1,j,currTime+1);
-        DFS(grid,i+1,j,currTime+1);
-        DFS(grid,i,j-1,currTime+1);
-        DFS(grid,i,j+1,currTime+1);
+    private void BFS(int[][] grid,Queue<int[]> q){
+        int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        while(!q.isEmpty()){
+            int n=q.size();
+            cnt++;
+            for(int idx=0;idx<n;idx++){
+                int[] curr=q.poll();
+                for(int[] dir:directions){
+                    int newX=curr[0]+dir[0];
+                    int newY=curr[1]+dir[1];
+                    if(isSafe(grid,newX,newY)){
+                        q.offer(new int[]{newX,newY});
+                        grid[newX][newY]=2;
+                    }
+                }
+            }
+        }
+    }
+    private boolean isSafe(int[][] grid, int i, int j){
+        int m=grid.length;
+        int n=grid[0].length;
+        return i>=0 && i<m && j>=0 && j<n && grid[i][j]==1;
     }
 }
