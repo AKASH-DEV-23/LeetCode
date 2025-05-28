41cf59b41c5b243ab11a41db1fbc42b2a7ca46a2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-28 08:43:24 +0000

    Sync LeetCode submission Runtime - 686 ms (19.00%), Memory - 0.0B (100.00%)

--- DSA/1292-immediate-food-delivery-ii/solution.sql
+++ DSA/1292-immediate-food-delivery-ii/solution.sql
@@ -1,4 +1,3 @@
--- Write your PostgreSQL query statement below
 SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1
                 ELSE 0 END)*100, 2) AS immediate_percentage
 FROM Delivery
