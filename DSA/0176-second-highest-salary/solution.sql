36ceeb9111f110957e2e84ba60910c3a962c96a3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-29 07:26:34 +0000

    Sync LeetCode submission Runtime - 284 ms (21.65%), Memory - 0.0B (100.00%)

--- DSA/0176-second-highest-salary/solution.sql
+++ DSA/0176-second-highest-salary/solution.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-select
-(select distinct Salary 
-from Employee order by salary desc 
-limit 1 offset 1) 
-as SecondHighestSalary;
+SELECT 
+IFNULL( (SELECT DISTINCT salary AS SecondHighestSalary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1,1),
+NULL)
+AS SecondHighestSalary
+
