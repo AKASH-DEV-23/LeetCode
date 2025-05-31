30b9e2df8a3d741bf57f3b3a851bd31d15ea9e26
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-31 12:06:09 +0000

    Sync LeetCode submission Runtime - 2 ms (12.92%), Memory - 45.4 MB (98.45%)

--- DSA/0733-flood-fill/solution.java
+++ DSA/0733-flood-fill/solution.java
@@ -1,39 +1,32 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]==color) return image;
-        // BFS(image,sr,sc,color);
-        DFS(image,sr,sc,image[sr][sc],color);
-        return image;
-    }
-    private void BFS(int[][] image, int i, int j, int color){
+        if(image[sr][sc]==color)    return image;
         Queue<int[]> q=new LinkedList<>();
-        int orgPxl=image[i][j];
-        q.offer(new int[]{i,j});
-        image[i][j]=-1;
-        image[i][j]=color;
+        int mark=image[sr][sc];
+        q.offer(new int[]{sr,sc});
+        image[sr][sc]=color;
         int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
         while(!q.isEmpty()){
-            int[] curr=q.poll();
-            for(int[] dir:directions){
-                int newX=curr[0]+dir[0];
-                int newY=curr[1]+dir[1];
-                if(isSafe(image,newX,newY,orgPxl)){
-                    q.offer(new int[]{newX,newY});
-                    image[newX][newY]=-1;
-                    image[newX][newY]=color;
+            int k=q.size();
+            for(int i=0;i<k;i++){
+                int[] cordi=q.poll();
+                int x=cordi[0];
+                int y=cordi[1];
+                for(int[] dir:directions){
+                    int newX=x+dir[0];
+                    int newY=y+dir[1];
+                    if(isSafe(image,newX,newY) && image[newX][newY]==mark){
+                        q.offer(new int[]{newX,newY});
+                        image[newX][newY]=color;
+                    }
                 }
             }
         }
+        return image;
     }
-    private boolean isSafe(int[][] image, int i, int j, int orgPxl){
-        return i>=0 && i<image.length && j>=0 && j<image[0].length && image[i][j]==orgPxl;
-    }
-    private void DFS(int[][] image, int i, int j, int orgPxl, int color){
-        if(!isSafe(image, i, j, orgPxl))  return;
-        image[i][j]=color;
-        DFS(image,i-1,j,orgPxl,color);
-        DFS(image,i+1,j,orgPxl,color);
-        DFS(image,i,j-1,orgPxl,color);
-        DFS(image,i,j+1,orgPxl,color);
+    private boolean isSafe(int[][] image, int i, int j){
+        int m=image.length;
+        int n=image[0].length;
+        return i>=0 && i<m && j>=0 && j<n;
     }
 }
