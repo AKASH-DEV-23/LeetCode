39cf7ece64d6365d332ccdef77ff7b20888cfa7c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-01 01:27:37 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 43.3 MB (97.15%)

--- DSA/0031-next-permutation/solution.java
+++ DSA/0031-next-permutation/solution.java
@@ -1,39 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
+        int n=nums.length-1;
         int idx=-1;
-        for(int i=n-2; i>=0; i--){
+        for(int i=n-1;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 idx=i;
                 break;
             }
         }
         if(idx==-1){
-            reverse(nums,idx+1,n-1);
+            reverse(nums,0,n);
             return;
         }
-        for(int i=n-1;i>=idx;i--){
+        for(int i=n;i>idx;i--){
             if(nums[i]>nums[idx]){
                 swap(nums,i,idx);
                 break;
             }
         }
-
-        reverse(nums,idx+1,n-1);
+        reverse(nums,idx+1,n);
     }
-
+    private void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
     private void reverse(int[] nums, int i, int j){
-        while(i<j){
+        while(i<=j){
             int temp=nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
             i++;
             j--;
         }
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
