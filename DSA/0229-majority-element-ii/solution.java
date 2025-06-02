82f5905a66bf884ddc917b5ab7e615ea14e55596
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-02 07:54:56 +0000

    [commit] Runtime - 13 ms (46.11%), Memory - 50.6 MB (96.84%)

--- DSA/0229-majority-element-ii/solution.java
+++ DSA/0229-majority-element-ii/solution.java
@@ -1,38 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        int cnt1=0;
-        int cnt2=0;
-        int cnd1=Integer.MIN_VALUE;
-        int cnd2=Integer.MIN_VALUE;
-
+        Map<Integer,Integer> map=new HashMap<>();
         for(int num:nums){
-            if(cnt1==0 && num!=cnd2){
-                cnt1++;
-                cnd1=num;
-            } else if(cnt2==0 && num !=cnd1){
-                cnt2++;
-                cnd2=num;
-            }else if(num==cnd1){
-                cnt1++;
-            }else if(num==cnd2){
-                cnt2++;
-            }else{
-                cnt1--;
-                cnt2--;
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int n=nums.length;
+        List<Integer> ans=new ArrayList<>();
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            int val=entry.getValue();
+            if(val>n/3){
+                ans.add(entry.getKey());
             }
         }
-        cnt1=0;
-        cnt2=0;
-        for(int num:nums){
-            if(num==cnd1)   cnt1++;
-            if(num==cnd2)   cnt2++;
-        }
-
-        int max=nums.length/3;
-        if(cnt1>max)    list.add(cnd1);
-        if(cnt2>max)    list.add(cnd2);
-        
-        return list;
+        return ans;
     }
 }
