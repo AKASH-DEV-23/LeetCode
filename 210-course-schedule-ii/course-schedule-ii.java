559eac7a334640a86898143ee9ecd7b68403ca5b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-05 14:08:17 +0530

    Time: 9 ms (14.72%) | Memory: 45.8 MB (73.04%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,51 +1,47 @@
 class Solution {
-    boolean[] visited;
-    List<Integer> list;
-    int[] inDegree;
-    int cnt=0;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<numCourses;i++){
+            map.put(i,new ArrayList<>());
+        }
         for(int[] edge:prerequisites){
             int u=edge[1];
             int v=edge[0];
-            if(!map.containsKey(u)) map.put(u,new ArrayList<>());
             map.get(u).add(v);
         }
-        visited=new boolean[numCourses];
-        inDegree=new int[numCourses];
-        for(int key:map.keySet()){
-            for(int v:map.get(key)){
+        int[] inDegree=new int[numCourses];
+        for(Map.Entry<Integer,List<Integer>> entry:map.entrySet()){
+            for(int v:entry.getValue()){
                 inDegree[v]++;
             }
-        }
-        list=new ArrayList<>();
-        BFS(map);
-        if(cnt!=numCourses) return new int[]{};
-        int[] ans=new int[list.size()];
-        int i=0;
-        for(int num:list){
-            ans[i++]=num;
-        }
-        return ans;
-    }
-    private void BFS(Map<Integer,List<Integer>> map){
-       Queue<Integer> q=new LinkedList<>();
-       for(int i=0;i<inDegree.length;i++){
+        } 
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] visited=new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
             if(inDegree[i]==0){
                 q.offer(i);
-                cnt++;
-            }
-       }
-       while(!q.isEmpty()){
-            int u=q.poll();
-            list.add(u);
-            for(int v:map.getOrDefault(u,new ArrayList<>())){
-                inDegree[v]--;
-                if(inDegree[v]==0){
-                    q.offer(v);
-                    cnt++;
-                }
+                visited[i]=true;
             }
         }
+        int[] ans=new int[numCourses];
+        int idx=0;
+        while(!q.isEmpty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                int u=q.poll();
+                for(int v:map.get(u)){
+                    inDegree[v]--;
+                    if(inDegree[v]==0){
+                        q.offer(v);
+                        visited[v]=true;
+                    }
+                }
+                ans[idx++]=u;
+            }
+        }
+        for(boolean flag:visited){
+            if(!flag)   return new int[]{};
+        }
+        return ans;
     }
 }
