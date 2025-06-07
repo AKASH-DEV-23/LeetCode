4eccdd80200663ddfc4957ea63b024adfc25f33a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-07 13:17:36 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 45.2 MB (99.61%)

--- DSA/0784-insert-into-a-binary-search-tree/solution.java
+++ DSA/0784-insert-into-a-binary-search-tree/solution.java
@@ -15,20 +15,13 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        return search(root,val);
-    }
-    private TreeNode search(TreeNode root, int val){
-        if(root==null)  return new TreeNode(val);
+        if(root==null){
+            return new TreeNode(val);
+        }
         if(val<root.val){
-            TreeNode l=search(root.left,val);
-            root.left=l;
-            return root;
-        }
-        else if(val>root.val){
-            TreeNode r= search(root.right,val);
-            root.right=r;
-            return root;
-        }
+            root.left=insertIntoBST(root.left,val);
+        }else
+            root.right=insertIntoBST(root.right,val);
         return root;
     }
 }
