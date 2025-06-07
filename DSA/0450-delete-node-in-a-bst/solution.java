85d914f13ba4a895130de54ee3100d36e6b84696
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-07 14:07:31 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 46.4 MB (99.74%)

--- DSA/0450-delete-node-in-a-bst/solution.java
+++ DSA/0450-delete-node-in-a-bst/solution.java
@@ -1,55 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-
-        if (root == null) {
-            return null;
-        }
-        if (root.val < key) {
-            root.right = deleteNode(root.right, key);
-        } else if (root.val > key) {
-            root.left = deleteNode(root.left, key);
-        } else {
-
-            if (root.left == null && root.right == null) {
-                return null;
+        if(root==null)  return root;
+        if(key<root.val){
+            root.left=deleteNode(root.left,key);
+        }else if(key>root.val){
+            root.right=deleteNode(root.right,key);
+        }else{
+            if(root.left==null && root.right==null)     return null;
+            else if(root.left==null)    return root.right;
+            else if(root.right==null)   return root.left;
+            else{
+                TreeNode successor=inorderSuccessor(root.left);
+                root.val=successor.val;
+                root.left=deleteNode(root.left,root.val);
             }
-
-            if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
-            }
-
-            TreeNode IS = findInOrderSuccessor(root.right);
-            root.val = IS.val;
-
-            root.right = deleteNode(root.right, IS.val);
         }
         return root;
-
     }
-
-    private TreeNode findInOrderSuccessor(TreeNode root) {
-        while (root.left != null) {
-            root = root.left;
+    private TreeNode inorderSuccessor(TreeNode root){
+        while(root.right!=null){
+            root=root.right;
         }
         return root;
     }
 }
-
