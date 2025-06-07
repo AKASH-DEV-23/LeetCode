a77ef337d1ca4b7d394b594a8fb422ff19f7875e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-07 11:25:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.5 MB (98.98%)

--- DSA/0783-search-in-a-binary-search-tree/solution.java
+++ DSA/0783-search-in-a-binary-search-tree/solution.java
@@ -16,8 +16,12 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null)  return root;
-        if(val==root.val)   return root;
-        if(val<root.val)  return  searchBST(root.left,val);
-        else  return  searchBST(root.right,val);
+        if(root.val==val){
+            return root;
+        }
+        if(val<root.val){
+           return searchBST(root.left,val);
+        }else
+            return searchBST(root.right,val);
     }
 }
