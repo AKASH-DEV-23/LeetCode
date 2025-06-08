8f8851cc8a215bfa8f96ce4af111fb852cc005d7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-08 20:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (88.46%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    int[] dp;
+    public int rob(int[] nums) {
+        dp=new int[101];
+        Arrays.fill(dp,-1);
+        return solve(nums,0);
+    }
+    private int solve(int[] nums, int idx){
+        if(idx>=nums.length)    return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int steal=nums[idx]+solve(nums,idx+2);
+        int skip=solve(nums,idx+1);
+        return dp[idx]=Math.max(steal,skip);
+    }
+}
