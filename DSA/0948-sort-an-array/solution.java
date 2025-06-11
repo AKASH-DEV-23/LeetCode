493fa828ac91c156e3b7c41b0f59dd91c8dcbdb5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-11 06:57:26 +0000

    [Commit] Runtime - 32 ms (35.29%), Memory - 56.5 MB (99.95%)

--- DSA/0948-sort-an-array/solution.java
+++ DSA/0948-sort-an-array/solution.java
@@ -1,7 +1,40 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+       return mergeSort(nums,0,nums.length-1);
+    }
+    private int[] mergeSort(int[] nums, int st, int end){
+        if(st<end){
+            int mid=st+(end-st)/2;
+            mergeSort(nums,st,mid);
+            mergeSort(nums,mid+1,end);
+            merge(nums,st,mid,end);
+        }
         return nums;
-        
+    }
+    private void merge(int[] nums,int st, int mid, int end){
+        int l= mid-st+1;
+        int r=end-mid;
+        int[] left=new int[l];
+        int[] right=new int[r];
+        int k=st;
+        for(int i=0;i<l;i++){
+            left[i]=nums[k++];
+        }
+        for(int i=0;i<r;i++){
+            right[i]=nums[k++];
+        }
+        int i=0;
+        int j=0;
+        k=st;
+        while(i<l && j<r){
+            if(left[i]<=right[j])   nums[k++]=left[i++];
+            else    nums[k++]=right[j++];
+        }
+        while(i<l){
+            nums[k++]=left[i++];
+        }
+        while(j<r){
+            nums[k++]=right[j++];
+        }
     }
 }
