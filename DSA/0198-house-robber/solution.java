50ac12278e362e214c626f784748465f280403f1
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-11 03:21:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.8 MB (99.95%)

--- DSA/0198-house-robber/solution.java
+++ DSA/0198-house-robber/solution.java
@@ -1,15 +1,16 @@
 class Solution {
     int[] dp;
     public int rob(int[] nums) {
-        dp=new int[101];
+        int n=nums.length;
+        dp=new int[n];
         Arrays.fill(dp,-1);
-        return solve(nums,0);
+        return solve(nums,0, n);
     }
-    private int solve(int[] nums, int idx){
-        if(idx>=nums.length)    return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int steal=nums[idx]+solve(nums,idx+2);
-        int skip=solve(nums,idx+1);
-        return dp[idx]=Math.max(steal,skip);
+    private int solve(int[] nums, int i, int n){
+        if(i>=n)    return 0;
+        if(dp[i]!=-1)   return dp[i];
+        int steal=nums[i]+solve(nums,i+2,n);
+        int skip=solve(nums,i+1,n);
+        return dp[i]=Math.max(steal,skip);
     }
 }
