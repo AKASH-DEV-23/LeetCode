2533de594f72bfb32c563d997b89829cba9abca8
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-11 07:19:25 +0000

    [Commit] Runtime - 1 ms (44.51%), Memory - 45.2 MB (99.96%)

--- DSA/0733-flood-fill/solution.java
+++ DSA/0733-flood-fill/solution.java
@@ -1,32 +1,31 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if(image[sr][sc]==color)    return image;
+        BFS(image,sr,sc,color);
+        return image;
+    }
+    private void BFS(int[][] grid, int sr, int sc, int color){
         Queue<int[]> q=new LinkedList<>();
-        int mark=image[sr][sc];
         q.offer(new int[]{sr,sc});
-        image[sr][sc]=color;
-        int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        int curr=grid[sr][sc];
+        grid[sr][sc]=color;
+        int[][] directions={{0,-1},{0,1},{-1,0},{1,0}};
         while(!q.isEmpty()){
-            int k=q.size();
-            for(int i=0;i<k;i++){
+            int n=q.size();
+            for(int i=0;i<n;i++){
                 int[] cordi=q.poll();
                 int x=cordi[0];
                 int y=cordi[1];
-                for(int[] dir:directions){
+                for(int [] dir:directions){
                     int newX=x+dir[0];
                     int newY=y+dir[1];
-                    if(isSafe(image,newX,newY) && image[newX][newY]==mark){
+                    if(newX>=0 && newX<grid.length && newY>=0 && newY<grid[0].length && 
+                        grid[newX][newY]==curr){
                         q.offer(new int[]{newX,newY});
-                        image[newX][newY]=color;
+                        grid[newX][newY]=color;
                     }
                 }
             }
         }
-        return image;
-    }
-    private boolean isSafe(int[][] image, int i, int j){
-        int m=image.length;
-        int n=image[0].length;
-        return i>=0 && i<m && j>=0 && j<n;
     }
 }
