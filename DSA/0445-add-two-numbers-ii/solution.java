2fde39113c314a3eecee98cba39dec856147e9eb
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-12 02:59:34 +0000

    [Commit] Runtime - 1 ms (99.78%), Memory - 44.7 MB (100.00%)

--- DSA/0445-add-two-numbers-ii/solution.java
+++ DSA/0445-add-two-numbers-ii/solution.java
@@ -10,14 +10,14 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       l1= reverse(l1);
-       l2= reverse(l2);
-        ListNode dummy=new ListNode(0);
-        ListNode temp=dummy;
+        l1=reverse(l1);
+        l2=reverse(l2);
         int carry=0;
-        int sum=0;
-       while(l1 !=null || l2!=null || carry!=0){
-            if(l1 !=null){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(l1!=null || l2!=null || carry!=0){
+            int sum=0;
+            if(l1!=null){
                 sum+=l1.val;
                 l1=l1.next;
             }
@@ -25,26 +25,19 @@ class Solution {
                 sum+=l2.val;
                 l2=l2.next;
             }
+            sum+=carry;
+            temp.next=new ListNode(sum%10);
             carry=sum/10;
-           int num=sum%10;
-            temp.next=new ListNode(num);
             temp=temp.next;
-            sum=carry;
-       }
-      return reverse(dummy.next);
-       
-       
-    }
-
-    private static ListNode reverse(ListNode head){
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode nextNode = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextNode;
         }
-        return prev;
+        return reverse(dummy.next);
+    }
+    private ListNode reverse(ListNode head){
+        if(head==null || head.next==null)   return head;
+        ListNode newHead=reverse(head.next);
+        ListNode temp=head.next;
+        temp.next=head;
+        head.next=null;
+        return newHead;
     }
 }
