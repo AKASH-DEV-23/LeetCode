291126f4e4ac803d33f29377f5950d215095a15a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-13 22:53:05 +0530

    Time: 172 ms (5.01%) | Memory: 61.5 MB (5.03%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[][] dp=new int[301][5001];
+    public int change(int amount, int[] coins) {
+        for(int[] arr:dp)   Arrays.fill(arr,-1);
+        return solve(coins,0,amount);
+    }
+    private int solve(int[] coins, int i, int amount){
+        if(i>=coins.length){
+            if(amount==0)   return 1;
+            return 0;
+        }
+        if(dp[i][amount]!=-1)   return dp[i][amount];
+        if(amount<coins[i]){
+            return solve(coins,i+1,amount);
+        }
+        int take=solve(coins,i,amount-coins[i]);
+        int skip=solve(coins,i+1,amount);
+        return dp[i][amount]= take+skip;
+    }
+}
