ef4fc0449f09d12928fd62fe9c47fb719f61ad11
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-13 04:53:51 +0000

    [Commit] Runtime - 295 ms (9.37%), Memory - 45.7 MB (99.94%)

--- DSA/0055-jump-game/solution.java
+++ DSA/0055-jump-game/solution.java
@@ -1,11 +1,16 @@
 class Solution {
+    Boolean[] dp=new Boolean[10001];
     public boolean canJump(int[] nums) {
-        int maxi=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(i>maxi)  return false;
-            maxi=Math.max(maxi,i+nums[i]);
+        if(nums.length==1)  return true;
+        return solve(nums,0);
+    }
+    private boolean solve(int[] nums, int idx){
+        if(idx>=nums.length-1)  return true;
+        if(dp[idx]!=null)   return dp[idx]; 
+        int cnt=nums[idx];
+        for(int i=1;i<=cnt;i++){
+            if(solve(nums,i+idx))   return true;
         }
-        return true;
+        return dp[idx]=false;
     }
 }
