1216692a3c4c53d72449699167c408c8dd5dd72f
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-13 06:23:18 +0000

    Sync LeetCode submission Runtime - 59 ms (5.41%), Memory - 56.3 MB (6.17%)

--- DSA/1250-longest-common-subsequence/solution.java
+++ DSA/1250-longest-common-subsequence/solution.java
@@ -1,14 +1,18 @@
 class Solution {
+    int[][]dp=new int[1001][1001];
     public int longestCommonSubsequence(String text1, String text2) {
-        int m=text1.length();
-        int n=text2.length();
-        int[][] dp=new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1))    dp[i][j]=1+dp[i-1][j-1];
-                else    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-            }
+        for(int[] arr:dp)   Arrays.fill(arr,-1);
+        return solve(text1,text2,0,0);
+    }
+    private int solve(String s1, String s2, int i, int j){
+        if(i>=s1.length() || j>=s2.length())    return 0;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            return 1+solve(s1,s2,i+1,j+1);
+        }else{
+            int path1=solve(s1,s2,i+1,j);
+            int path2=solve(s1,s2,i,j+1);
+            return dp[i][j]=Math.max(path1,path2);
         }
-        return dp[m][n];
     }
 }
