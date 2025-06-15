db947b1db6389ac66b56e0b3956278d195834bd4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-15 17:46:37 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 40.5 MB (99.12%)

--- DSA/0062-unique-paths/solution.java
+++ DSA/0062-unique-paths/solution.java
@@ -9,7 +9,7 @@ class Solution {
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                grid[i][j]=grid[i-1][j]+grid[i][j-1];
+                grid[i][j]=grid[i][j-1]+grid[i-1][j];
             }
         }
         return grid[m-1][n-1];
