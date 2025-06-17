074abea14ad8881f885bc129eb07b5a86450752c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-17 04:18:19 +0000

    [Commit] Runtime - 1 ms (99.85%), Memory - 42.2 MB (99.98%)

--- DSA/1036-rotting-oranges/solution.java
+++ DSA/1036-rotting-oranges/solution.java
@@ -1,33 +1,20 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int fresh=0;
-        for(int[] mat:grid){
-            for(int num:mat){
-                if(num==1)  fresh++;
-            }
-        }
-        if(fresh==0)    return 0;
-        int ans=BFS(grid);
-        for(int[] mat:grid){
-            for(int num:mat){
-                if(num==1)  return -1;
-            }
-        }
-        return ans-1;
-    }
-    private int BFS(int[][] grid){
-        int cnt=0;
-        Queue<int[]> q=new LinkedList<>();
         int m=grid.length;
         int n=grid[0].length;
+        Queue<int[]> q=new LinkedList<>();
+        boolean flag=true;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                if(grid[i][j]==1)   flag=false;
                 if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
                 }
             }
         }
+        if(flag)    return 0;
         int[][] directions=new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        int cnt=0;
         while(!q.isEmpty()){
             int k=q.size();
             for(int i=0;i<k;i++){
@@ -37,7 +24,7 @@ class Solution {
                 for(int[] dir:directions){
                     int newX=x+dir[0];
                     int newY=y+dir[1];
-                    if(isSafe(grid,newX,newY) && grid[newX][newY]==1){
+                    if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]==1){
                         q.offer(new int[]{newX,newY});
                         grid[newX][newY]=2;
                     }
@@ -45,11 +32,12 @@ class Solution {
             }
             cnt++;
         }
-        return cnt;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1)   return -1;
+            }
+        }
+        return cnt-1;
     }
-    private boolean isSafe(int[][] grid, int i, int j){
-        int m=grid.length;
-        int n=grid[0].length;
-        return i>=0 && i<m && j>=0 && j<n;
-    }
+    
 }
