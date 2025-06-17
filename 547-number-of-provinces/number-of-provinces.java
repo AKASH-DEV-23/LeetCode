ddd5d274117c2c014514900618faeeea8410f3f6
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-17 08:36:28 +0530

    Time: 4 ms (19.15%) | Memory: 47.4 MB (74.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,48 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
         int n=isConnected.length;
-        DSU dsu=new DSU(n);
+        for(int i=1;i<=n;i++)   map.put(i,new ArrayList<>());
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1)
-                    dsu.union(i,j);
+                if( i!=j && isConnected[i][j]==1){
+                    map.get(i+1).add(j+1);
+                }
             }
         }
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<n;i++){
-            set.add(dsu.find(i));
+        int component=0;
+        boolean[] visited=new boolean[n+1];
+        for(int i=1;i<=n;i++){
+            if(!visited[i]){
+                DFS(map,i,visited);
+                component++;
+            }
         }
-        return set.size();
+        return component;
     }
-}
-class DSU{
-    int[] parent;
-    int[] size;
-
-    public DSU(int n){
-        parent=new int[n];
-        size=new int[n];
-
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-
-    public int find(int x){
-        if(x==parent[x])    return x;
-        return parent[x]=find(parent[x]);
-    }
-
-    public void union(int x, int y){
-        int xParent=find(x);
-        int yParent=find(y);
-        if(xParent==yParent)    return;
-        if(size[xParent]>size[yParent])     parent[yParent]=xParent;
-        else if(size[yParent]>size[xParent])    parent[xParent]=yParent;
-        else{
-            parent[yParent]=xParent;
-            size[xParent]++;
+    private void DFS(Map<Integer,List<Integer>> map, int u, boolean[] visited){
+        visited[u]=true;
+        for(int v:map.get(u)){
+            if(!visited[v]){
+                DFS(map,v,visited);
+            }
         }
     }
 }
