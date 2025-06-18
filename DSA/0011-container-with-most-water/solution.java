34e09ad4412570bb43669a9b5553ae5022fa94b2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-18 07:19:26 +0000

    [Commit] Runtime - 5 ms (78.05%), Memory - 57.8 MB (99.86%)

--- DSA/0011-container-with-most-water/solution.java
+++ DSA/0011-container-with-most-water/solution.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int maxArea(int[] height) {
+    public int maxArea(int[] heigth) {
         int left=0;
-        int right=height.length-1;
-        int maxArea = 0;
+        int right=heigth.length-1;
+        int maxArea=0;
         while(left<right){
-            int area = Math.min(height[left],height[right]) * (right-left);
-            maxArea = Math.max(maxArea,area);
-            if(height[left] < height[right]){
-                left++;
-            } else {
-                right--;
-            }
+            int area=Math.min(heigth[left],heigth[right])*(right-left);
+            maxArea=Math.max(area,maxArea);
+            if(heigth[left]<heigth[right])    left++;
+            else    right--;
         }
         return maxArea;
     }
