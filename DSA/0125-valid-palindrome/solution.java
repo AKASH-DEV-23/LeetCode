d1d98b6ad5d15db7b97b53cda263f88f342a7a8d
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-18 06:10:09 +0000

    [Commit] Runtime - 7 ms (39.34%), Memory - 45 MB (44.03%)

--- DSA/0125-valid-palindrome/solution.java
+++ DSA/0125-valid-palindrome/solution.java
@@ -1,23 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.toLowerCase();
-        // System.out.println(str);
         StringBuilder sb=new StringBuilder();
-        for(char ch:str.toCharArray()){
-            if(ch>='a' && ch<='z' || ch>='0' && ch<='9'){
-                sb.append(ch);
-            }
+        for(char ch:s.toCharArray()){
+            if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9')    sb.append(ch);
         }
-        int i=0;
-        int j=sb.length()-1;
-        while(i<=j){
-            if(sb.charAt(i) != sb.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        // System.out.println(sb);
-        return true;
+        System.out.println(sb);
+        String s2=sb.toString().toLowerCase();
+        String s1=sb.reverse().toString().toLowerCase();
+        return s1.equals(s2);
     }
 }
