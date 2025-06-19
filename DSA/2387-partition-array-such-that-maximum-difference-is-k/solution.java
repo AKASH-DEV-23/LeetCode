5ce6379a7953b8447cd49a3dfff14c7b39037683
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-19 03:42:36 +0000

    [commit] Runtime - 33 ms (96.08%), Memory - 56.9 MB (100.00%)

---
+++ DSA/2387-partition-array-such-that-maximum-difference-is-k/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int cnt=1;
+        int mini=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]-mini>k){
+                cnt++;
+                mini=nums[i];
+            }
+        }
+        return cnt;
+    }
+}
