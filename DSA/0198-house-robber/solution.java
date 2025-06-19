f3054d9cfd58568ef1c957ae5d47db99da0567c7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-19 03:52:19 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.3 MB (99.94%)

--- DSA/0198-house-robber/solution.java
+++ DSA/0198-house-robber/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     int[] dp;
     public int rob(int[] nums) {
-        int n=nums.length;
-        dp=new int[n];
+        dp=new int[101];
         Arrays.fill(dp,-1);
-        return solve(nums,0, n);
+        return solve(nums,0);
     }
-    private int solve(int[] nums, int i, int n){
-        if(i>=n)    return 0;
-        if(dp[i]!=-1)   return dp[i];
-        int steal=nums[i]+solve(nums,i+2,n);
-        int skip=solve(nums,i+1,n);
-        return dp[i]=Math.max(steal,skip);
+    private int solve(int[] nums, int idx){
+        if(idx>=nums.length)    return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int steal=nums[idx] + solve(nums,idx+2);
+        int skip=solve(nums,idx+1);
+        return dp[idx]=Math.max(steal,skip);
     }
 }
