f14123aa1fa07043766c3f76eb69f7dfb512f160
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-19 07:04:16 +0000

    [Commit] Runtime - 2 ms (40.44%), Memory - 44.2 MB (99.94%)

--- DSA/0046-permutations/solution.java
+++ DSA/0046-permutations/solution.java
@@ -2,27 +2,21 @@ class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> permute(int[] nums) {
         ans=new ArrayList<>();
-        helper(nums,0);
+        List<Integer> list=new ArrayList<>();
+        solve(nums,list);
         return ans;
     }
-    private void helper(int[] arr, int idx){
-        if(idx==arr.length){
-            List<Integer> list=new ArrayList<>();
-            for(int num:arr){
-                list.add(num);
-            }
-            ans.add(list);
+    private void solve(int[] nums, List<Integer> list){
+        if(list.size()==nums.length){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=idx;i<arr.length;i++){
-            swap(arr,i,idx);
-            helper(arr,idx+1);
-            swap(arr,i,idx);
+        for(int i=0;i<nums.length;i++){
+            if(!list.contains(nums[i])){
+                list.add(nums[i]);
+                solve(nums,list);
+                list.remove(list.size()-1);
+            }
         }
-    }
-    private void swap(int[] arr, int i, int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
     }
 }
