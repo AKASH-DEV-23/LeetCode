297e91e28beaca5564222d32f3775a54b5423124
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-23 06:41:41 +0000

    [Commit] Runtime - 406 ms (30.71%), Memory - 0.0B (100.00%)

--- DSA/0175-combine-two-tables/solution.sql
+++ DSA/0175-combine-two-tables/solution.sql
@@ -1,3 +1,4 @@
-SELECT P.firstName, P.lastName, A.city, A.state
-FROM Person P LEFT JOIN Address A
-on P.personId = A.personId
+# Write your MySQL query statement below
+select p.firstName,p.lastName,a.city,a.state
+from Person p left join Address a
+on p.personId=a.personId;
