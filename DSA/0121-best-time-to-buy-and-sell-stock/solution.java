c0fbfae53ded7677eaf6645c28377b8b3fa692af
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-06-27 03:20:54 +0000

    Sync LeetCode submission Runtime - 1 ms (99.82%), Memory - 61.4 MB (99.62%)

--- DSA/0121-best-time-to-buy-and-sell-stock/solution.java
+++ DSA/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,15 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxi=0;
-        int n=prices.length;
         int buy=prices[0];
-        for(int i=1;i<n;i++){
-            if(prices[i]<buy){
-                buy=prices[i];
-            }else{
-                maxi=Math.max(maxi,prices[i]-buy);
-            }
+        int maxProfit=0;
+        for(int num:prices){
+            if(num<buy)  buy=num;
+            else    maxProfit=Math.max(maxProfit,num-buy);
         }
-        return maxi;
+        return maxProfit;
     }
 }
