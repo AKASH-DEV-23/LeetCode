61ef34093ca19bd831ed708a1e6485a1f640780f
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-01 04:43:10 +0000

    [Commit] Runtime - 7 ms (78.49%), Memory - 44.7 MB (99.46%)

--- DSA/1482-how-many-numbers-are-smaller-than-the-current-number/solution.java
+++ DSA/1482-how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -1,23 +1,14 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] count = new int[101];
-        int[] res = new int[nums.length];
-        
-        for (int i =0; i < nums.length; i++) {
-            count[nums[i]]++;
+        int size=nums.length;
+        int[] ans=new int[size];
+        for(int i=0;i<size;i++){
+            int cnt=0;
+            for(int j=0;j<size;j++){
+                if(nums[j]<nums[i]) cnt++;
+            }
+            ans[i]=cnt;
         }
-        
-        for (int i = 1 ; i <= 100; i++) {
-            count[i] += count[i-1];    
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                res[i] = 0;
-            else 
-                res[i] = count[nums[i] - 1];
-        }
-        
-        return res;        
+        return ans;
     }
 }
