5b95972b252287e77c70ec46eff5a69d81e4bb6c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-03 06:26:48 +0000

    [Commit] Runtime - 3 ms (90.23%), Memory - 44.3 MB (98.55%)

--- DSA/0496-next-greater-element-i/solution.java
+++ DSA/0496-next-greater-element-i/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        for(int i=n1-1;i>=0;i--){
-            int max=-1;
-            for(int j=n2-1;j>=0;j--){
-                if(nums2[j]==nums1[i]){
-                    break;
-                }else if(nums2[j]>nums1[i]){
-                    max=nums2[j];
-                }
-            }
-            nums1[i]=max;
+        int len2=nums2.length;
+        int[] ans2=new int[len2];
+        len2-=1;
+        Stack<Integer> st=new Stack<>();
+        while(len2>=0){
+            while(!st.isEmpty() && st.peek()<nums2[len2])   st.pop();
+            if(st.isEmpty())    ans2[len2]=-1;
+            else    ans2[len2]=st.peek();
+            st.push(nums2[len2]);
+            len2--;
+        }
+        Map<Integer,Integer> map=new HashMap<>();
+        len2=nums2.length;
+        for(int i=0;i<len2;i++){
+            map.put(nums2[i],ans2[i]);
+        }
+        int i=0;
+        for(int num:nums1){
+            nums1[i++]=map.get(num);
         }
         return nums1;
     }
