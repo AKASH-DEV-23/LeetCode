5b8ef455cc991a1eb935b6b88c56967d2839a3af
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-08 03:01:30 +0000

    Sync LeetCode submission Runtime - 10 ms (2.55%), Memory - 44.9 MB (6.20%)

--- DSA/0678-valid-parenthesis-string/solution.java
+++ DSA/0678-valid-parenthesis-string/solution.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int min=0;
-        int max=0;
-        for(char ch : s.toCharArray()){
-            if(ch=='('){
-                min+=1;
-                max+=1;
-            }else if(ch==')'){
-                min-=1;
-                max-=1;
-            }else{
-                min-=1;
-                max+=1;
-            }
-            if(min<0)   min=0;
-            if(max<0)   return false;
+        return findString(s,0,0);
+    }
+    Map<String,Boolean> memo=new HashMap<>();
+    private boolean findString(String s, int idx, int open){
+        String str=idx+","+open;
+        if(memo.containsKey(str))    return memo.get(str);
+        boolean result;
+        if(idx==s.length())   return open==0;
+        if(open<0)  return false;
+        if(s.charAt(idx)=='(') result = findString(s,idx+1,open+1);
+        else if(s.charAt(idx)==')') result =  findString(s,idx+1,open-1);
+        else{
+            result = findString(s,idx+1,open+1) ||
+                    findString(s,idx+1,open-1) ||
+                    findString(s,idx+1,open);
         }
-        return min==0;
+        memo.put(str,result);
+        return result;
     }
 }
