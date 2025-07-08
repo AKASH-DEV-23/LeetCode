51b26c4db047929b3dacc0d0250106a9d62af66c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-08 05:12:19 +0000

    [Commit] Runtime - 11 ms (67.71%), Memory - 44.3 MB (99.96%)

--- DSA/0794-swim-in-rising-water/solution.java
+++ DSA/0794-swim-in-rising-water/solution.java
@@ -1,35 +1,35 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int n=grid.length;
-        boolean[][] visited=new boolean[n][n];
-        int maxHeight=0;
+        return Dijkstra(grid);
+    }
+    private int Dijkstra(int[][] grid){
+        int row=grid.length;
         Comparator<int[]> com=new Comparator<>(){
             public int compare(int[] a, int[] b){
                 if(a[0]<b[0])   return -1;
                 else if(a[0]>b[0])  return 1;
-                else    return 0;
+                else return 0;
             }
         };
-        PriorityQueue<int[]> pq=new PriorityQueue<>(com);
-        int[][] directions=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        pq.offer(new int[]{grid[0][0],0,0});
-        visited[0][0]=true;
-        while(!pq.isEmpty()){
-            int[] curr=pq.poll();
-            maxHeight=Math.max(maxHeight,curr[0]);
-            int x=curr[1];
-            int y=curr[2];
-            if(x==n-1 && y==n-1)  return maxHeight;
+        PriorityQueue<int[]> minPq=new PriorityQueue<>(com);
+        int[][] directions={{0,-1},{0,1},{-1,0},{1,0}};
+        minPq.offer(new int[]{grid[0][0],0,0});
+        int maxElevation=grid[0][0];
+        grid[0][0]=-1;
+        while(!minPq.isEmpty()){
+            int[] edge=minPq.poll();
+            maxElevation=Math.max(maxElevation,edge[0]);
+            if(edge[1]==row-1 && edge[2]==row-1)    return maxElevation;
             for(int[] dir:directions){
-                int newX=x+dir[0];
-                int newY=y+dir[1];
-                if(newX>=0 && newX<n && newY>=0 && newY<n && !visited[newX][newY]){
-                  pq.offer(new int[]{grid[newX][newY],newX,newY});
-                  visited[newX][newY]=true;
+                int newX=edge[1]+dir[0];
+                int newY=edge[2]+dir[1];
+                if(newX>=0 && newX<row && newY>=0 && newY<row && 
+                grid[newX][newY]!=-1){
+                    minPq.offer(new int[]{grid[newX][newY],newX,newY});
+                    grid[newX][newY]=-1;
                 }
             }
-
         }
-        return -1;
+        return maxElevation;
     }
 }
