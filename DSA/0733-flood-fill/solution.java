fbbb6ddb92bce471376cfe02b15f923bd2b3f649
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-08 04:05:34 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 44.8 MB (99.98%)

--- DSA/0733-flood-fill/solution.java
+++ DSA/0733-flood-fill/solution.java
@@ -1,31 +1,19 @@
 class Solution {
+    int preColor=0;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if(image[sr][sc]==color)    return image;
-        BFS(image,sr,sc,color);
+        preColor=image[sr][sc];
+        DFS(image,sr,sc,color);
         return image;
     }
-    private void BFS(int[][] grid, int sr, int sc, int color){
-        Queue<int[]> q=new LinkedList<>();
-        q.offer(new int[]{sr,sc});
-        int curr=grid[sr][sc];
-        grid[sr][sc]=color;
-        int[][] directions={{0,-1},{0,1},{-1,0},{1,0}};
-        while(!q.isEmpty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                int[] cordi=q.poll();
-                int x=cordi[0];
-                int y=cordi[1];
-                for(int [] dir:directions){
-                    int newX=x+dir[0];
-                    int newY=y+dir[1];
-                    if(newX>=0 && newX<grid.length && newY>=0 && newY<grid[0].length && 
-                        grid[newX][newY]==curr){
-                        q.offer(new int[]{newX,newY});
-                        grid[newX][newY]=color;
-                    }
-                }
-            }
-        }
+    private void DFS(int[][] image, int row, int col, int color){
+        if(row<0 || row>=image.length || col<0 || col>=image[0].length || 
+        image[row][col]!=preColor) return;
+        image[row][col]=color;
+        DFS(image,row+1,col,color);
+        DFS(image,row-1,col,color);
+        DFS(image,row,col+1,color);
+        DFS(image,row,col-1,color);
     }
 }
+
