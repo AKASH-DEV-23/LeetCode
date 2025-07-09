0d6b665f5a74f3ebab67d09c2fbb360d69c229d2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-09 03:23:17 +0000

    [Commit] Runtime - 3 ms (88.92%), Memory - 45.7 MB (99.86%)

--- DSA/0135-candy/solution.java
+++ DSA/0135-candy/solution.java
@@ -1,22 +1,18 @@
 class Solution {
     public int candy(int[] ratings) {
-     int n = ratings.length;
-     int[] candy = new int[n];
-     Arrays.fill(candy,1);
-     for(int i = 1; i < n; i++){
-        if(ratings[i] > ratings[i-1]){
-            candy[i] = candy[i-1]+1;
+        int len=ratings.length;
+        int[] candy=new int[len];
+        Arrays.fill(candy,1);
+        for(int i=1;i<len;i++){
+            if(ratings[i]>ratings[i-1])
+                candy[i]=candy[i]>candy[i-1]?candy[i]:candy[i-1]+1;
         }
-     }   
-     for(int i = n -2; i>=0; i--){
-        if(ratings[i] > ratings[i+1]){
-            candy[i] = Math.max(candy[i], candy[i+1]+1);
+        for(int i=len-2;i>-1;i--){
+            if(ratings[i]>ratings[i+1])
+                candy[i]=candy[i]>candy[i+1]?candy[i]:candy[i+1]+1;
         }
-     }
-     int sum = 0;
-     for(int c : candy){
-        sum += c;
-     }
-     return sum;
+        int totalCandy=0;
+        for(int can:candy)  totalCandy+=can;
+        return totalCandy;
     }
 }
