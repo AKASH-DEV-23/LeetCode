515b1f2b1ede3417aa63fda0af8994ee424e1ac4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-11 08:55:30 +0530

    Time: 1937 ms (8.84%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 SELECT 'Low Salary' AS category, COUNT(account_id) AS accounts_count FROM Accounts WHERE income < 20000
-UNION 
+UNION ALL
 SELECT 'Average Salary' as category, COUNT(account_id) AS accounts_count FROM Accounts WHERE income BETWEEN 20000 AND 50000
-UNION 
+UNION ALL
 SELECT 'High Salary' as category, COUNT(account_id) AS accounts_count FROM Accounts WHERE income > 50000
+ORDER BY accounts_count;
