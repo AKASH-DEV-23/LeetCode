cd2025fcf4a908cd3cd375e6e708caa8090b6c57
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-11 04:34:36 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 45.6 MB (99.88%)

--- DSA/0783-search-in-a-binary-search-tree/solution.java
+++ DSA/0783-search-in-a-binary-search-tree/solution.java
@@ -16,12 +16,11 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null)  return root;
-        if(root.val==val){
-            return root;
-        }
+        if(root.val==val)   return root;
         if(val<root.val){
-           return searchBST(root.left,val);
-        }else
+            return searchBST(root.left,val);
+        }else{
             return searchBST(root.right,val);
+        }
     }
 }
