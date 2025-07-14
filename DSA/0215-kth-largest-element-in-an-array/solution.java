e446fdf8d2e34ebd5cd01861daaae3cd729c9a11
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-14 17:48:48 +0000

    [Commit] Runtime - 62 ms (57.64%), Memory - 61.9 MB (99.95%)

--- DSA/0215-kth-largest-element-in-an-array/solution.java
+++ DSA/0215-kth-largest-element-in-an-array/solution.java
@@ -1,12 +1,9 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int num:nums){
             pq.offer(num);
-        }
-        while(k>1){
-            pq.poll();
-            k--;
+            if(pq.size()>k) pq.poll();
         }
         return pq.poll();
     }
