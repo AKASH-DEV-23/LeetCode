a50841012c1e9f3bb5964f7adf20a4ac51355e93
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-14 02:54:33 +0000

    [Commit] Runtime - 5 ms (12.03%), Memory - 47.1 MB (86.53%)

--- DSA/0547-number-of-provinces/solution.java
+++ DSA/0547-number-of-provinces/solution.java
@@ -1,96 +1,31 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        HashMap<Integer,List<Integer>> map=new HashMap<>();
-        int n=isConnected.length;
-        for(int i=1;i<=n;i++)   map.put(i,new ArrayList<>());
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        int V=isConnected.length;
+        Map<Integer,List<Integer>> adj=new HashMap<>();
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
                 if( i!=j && isConnected[i][j]==1){
-                    map.get(i+1).add(j+1);
+                    if(!adj.containsKey(i)) adj.put(i,new ArrayList<>());
+                    adj.get(i).add(j);
                 }
             }
         }
-        int component=0;
-        boolean[] visited=new boolean[n+1];
-        for(int i=1;i<=n;i++){
+        boolean[] visited=new boolean[V];
+        int cnt=0;
+        for(int i=0;i<V;i++){
             if(!visited[i]){
-                // DFS(map,i,visited);
-                BFS(map,i,visited);
-                component++;
+                DFS(adj,visited,i);
+                cnt++;
             }
-        }
-        // return component;
-        return disJoint(isConnected);
+        } 
+        return cnt;
     }
-    private void DFS(Map<Integer,List<Integer>> map, int u, boolean[] visited){
+    private void DFS(Map<Integer,List<Integer>> adj, boolean[] visited, int u){
         visited[u]=true;
-        for(int v:map.get(u)){
+        for(int v:adj.getOrDefault(u,new ArrayList<>())){
             if(!visited[v]){
-                DFS(map,v,visited);
+                DFS(adj,visited,v);
             }
-        }
-    }
-    private void BFS(Map<Integer,List<Integer>> map, int u, boolean[] visited){
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(u);
-        visited[u]=true;
-        while(!q.isEmpty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                int v=q.poll();
-                for(int neighbour:map.get(v)){
-                    if(!visited[neighbour]){
-                        q.offer(neighbour);
-                        visited[neighbour]=true;
-                    }
-                }
-            }
-        }
-    }
-    private int disJoint(int[][] isConnected){
-        int n=isConnected.length;
-        DSU dsu=new DSU(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    dsu.union(i+1,j+1);
-                }
-            }
-        }
-        Set<Integer> set=new HashSet<>();
-        for(int i=1;i<=n;i++){
-            int c=dsu.find(i);
-            set.add(c);
-        }
-        return set.size();
-    }
-}
-class DSU{
-    int[] parent;
-    int[] rank;
-
-    public DSU(int size){
-        parent=new int[size+1];
-        rank=new int[size+1];
-        for(int i=1;i<=size;i++){
-            parent[i]=i;
-            rank[i]=1;
-        }
-    }
-    public int find(int x){
-        if(parent[x]==x)    return x;
-        return parent[x] = find(parent[x]);
-    }
-    public void union(int x, int y){
-        int xParent=find(x);
-        int yParent=find(y);
-        if(xParent==yParent)    return;
-        if(rank[xParent]>rank[yParent])     parent[yParent]=xParent;
-        else if(rank[xParent]<rank[yParent])    parent[xParent]=yParent;
-        else{
-            parent[yParent]=xParent;
-            rank[xParent]++;
         }
     }
 }
