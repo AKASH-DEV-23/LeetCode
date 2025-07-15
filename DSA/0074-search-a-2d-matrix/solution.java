891376e948aea406e18f6a6a098d2e88c38ca584
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-15 04:15:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (98.28%)

--- DSA/0074-search-a-2d-matrix/solution.java
+++ DSA/0074-search-a-2d-matrix/solution.java
@@ -1,13 +1,24 @@
 class Solution {
-   public static boolean searchMatrix(int[][] matrix, int target) {
-        for (int[] num : matrix) {
-            for (int n : num) {
-                if (target == n) {
-                    return true;
-                }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int st=0;
+        int end=matrix.length-1;
+        int mid=0;
+        while(st<=end){
+            mid=st+(end-st)/2;
+            if(target<matrix[mid][0])   end=mid-1;
+            else if(target>matrix[mid][matrix[0].length-1]) st=mid+1;
+            else{
+                break;
             }
         }
+        st=0;
+        end=matrix[0].length-1;
+        while(st<=end){
+            int m=st+(end-st)/2;
+            if(target==matrix[mid][m])  return true;
+            if(target<matrix[mid][m])   end=m-1;
+            else if(target>matrix[mid][m])    st=m+1;
+        }
         return false;
-
     }
 }
