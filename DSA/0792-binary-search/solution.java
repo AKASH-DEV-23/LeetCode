bf1e82ce2d15dec03197e4e1628f1243c497b169
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-15 02:42:22 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 45.7 MB (98.44%)

--- DSA/0792-binary-search/solution.java
+++ DSA/0792-binary-search/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return solve(nums,0,nums.length-1,target);
-    }
-    private int solve(int[] nums, int low, int high, int target){
-        if(low>high)    return -1;
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target)   return mid;
-        else if(nums[mid]<target)  return solve(nums,mid+1,high,target);
-        else  return solve(nums,low,mid-1,target);
+        int st=0;
+        int end=nums.length-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target)   return mid;
+            if(target<nums[mid])    end=mid-1;
+            else if(target>nums[mid])   st=mid+1;
+        }
+        return -1;
     }
 }
