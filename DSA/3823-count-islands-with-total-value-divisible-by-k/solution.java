f90f773eeeea2e0794a540f0ac6a9b21ab44ced3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-19 15:32:51 +0000

    [commit] - Count Islands With Total Value Divisible by K - Runtime - 16 ms (90.69%), Memory - 72 MB (100.00%)

--- DSA/3823-count-islands-with-total-value-divisible-by-k/solution.java
+++ DSA/3823-count-islands-with-total-value-divisible-by-k/solution.java
@@ -1,26 +1,28 @@
 class Solution {
+    static int sum;
     public int countIslands(int[][] grid, int k) {
-        int m=grid.length;
-        int n=grid[0].length;
         int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]!=0){
-                    int[] arr=new int[1];
-                    DFS(grid,i,j,arr);
-                    if(arr[0]%k==0) cnt++;
+                    sum=0;
+                    int[] ans=new int[1];
+                    DFS(grid,i,j,ans);
+                    if(ans[0]%k==0)  cnt++;
+                    // System.out.print(ans[0]+" ");
                 }
             }
         }
         return cnt;
     }
-    private void DFS(int[][] grid, int i, int j, int[] arr){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)  return;
-        arr[0]=arr[0]+grid[i][j];
-        grid[i][j]=0;
-        DFS(grid,i+1,j,arr);
-        DFS(grid,i-1,j,arr);
-        DFS(grid,i,j+1,arr);
-        DFS(grid,i,j-1,arr);
+    private void DFS(int[][] grid, int i, int j, int[] sum){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0)  return;
+        // int temp=grid[i][j];
+        sum[0]=sum[0]+grid[i][j];
+            grid[i][j]=0;
+        DFS(grid,i+1,j,sum);
+        DFS(grid,i-1,j,sum);
+        DFS(grid,i,j+1,sum);
+        DFS(grid,i,j-1,sum);
     }
 }
