771e88d3e7de434b50d23e0e43dfaf5bfcb05ec8
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-19 15:22:36 +0000

    [commit] - Split Array by Prime Indices - Runtime - 4 ms (99.42%), Memory - 62.5 MB (100.00%)

--- DSA/3936-split-array-by-prime-indices/solution.java
+++ DSA/3936-split-array-by-prime-indices/solution.java
@@ -1,18 +1,22 @@
 class Solution {
     public long splitArray(int[] nums) {
+        boolean[] visited=new boolean[nums.length+1];
+        visited[0]=visited[1]=true;
+        for(int i=2;i*i<nums.length;i++){
+            if(visited[i]==false){
+                for(int j=i*i;j<nums.length;j+=i){
+                    visited[j]=true;
+                }
+            }
+        }
         long sum1=0;
         long sum2=0;
         for(int i=0;i<nums.length;i++){
-            if(checkPrime(i))   sum1+=nums[i];
-            else    sum2+=nums[i];
+            if(visited[i]==true){
+                sum1+=nums[i];
+                }else  sum2+=nums[i];
         }
+        // System.out.println(sum1 +" "+sum2);
         return Math.abs(sum1-sum2);
-    }
-    private boolean checkPrime(int idx){
-        if(idx<=1)  return false;
-        for(int i=2;i<=Math.sqrt(idx);i++){
-            if(idx%i==0)    return false;
-        }
-        return true;
     }
 }
