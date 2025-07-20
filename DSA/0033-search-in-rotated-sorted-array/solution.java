ddc699d6e94af3f08c3bc68fb36efbb51b595500
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-20 12:51:12 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.78%)

--- DSA/0033-search-in-rotated-sorted-array/solution.java
+++ DSA/0033-search-in-rotated-sorted-array/solution.java
@@ -1,6 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++)  if(target==nums[i]) return i;
+        int pivot=findPivot(nums);
+        int ans1= binarySearch(nums,0,pivot-1,target); 
+        int ans2 = binarySearch(nums,pivot,nums.length-1,target);
+        if(ans1 ==-1 && ans2==-1)   return -1;
+        if(ans1 ==-1)   return ans2;
+        return ans1;    
+    }
+    private int findPivot(int[] nums){
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            }else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+    private int binarySearch(int[] nums, int l, int r, int target){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)   return mid;
+            if(target<nums[mid])    r=mid-1;
+            else l=mid+1;
+        }
         return -1;
     }
 }
