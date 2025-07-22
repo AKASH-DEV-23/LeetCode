4a61bc1aa1746a74133ce2fc0c584373f5ed82e5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-22 13:36:11 +0000

    [Commit] Runtime - 1 ms (5.49%), Memory - 41.8 MB (99.97%)

--- DSA/0066-plus-one/solution.java
+++ DSA/0066-plus-one/solution.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-	if (digits[i] < 9) {
-		digits[i]++;
-		return digits;
-	}
-	digits[i] = 0;
-}
-
-digits = new int[digits.length + 1];
-digits[0] = 1;
-return digits;
+        StringBuilder sb=new StringBuilder();
+        int carry=1;
+        for(int i=digits.length-1;i>=0;i--){
+            int sum=digits[i];
+            sum+=carry;
+            carry=sum/10;
+            sb.append(sum%10);
+        }
+        if(carry>0){
+            sb.append(carry);
+        }
+        sb.reverse();
+        int[] ans=new int[sb.length()];
+        for(int i=0;i<sb.length();i++){
+            ans[i]=sb.charAt(i)-'0';
+        }
+        return ans;
     }
 }
