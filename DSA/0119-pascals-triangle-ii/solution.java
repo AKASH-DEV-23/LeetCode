81b282e7f0aa030828876f3663b7396e517e8af2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-07-29 19:01:16 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41.1 MB (99.96%)

--- DSA/0119-pascals-triangle-ii/solution.java
+++ DSA/0119-pascals-triangle-ii/solution.java
@@ -1,14 +1,17 @@
 class Solution {
-    public List<Integer> getRow(int row) {
-        List<Integer> ansRow = new ArrayList<>();
-        ansRow.add(1);
-        long res=1;
-        for(int i=1;i<row+1;i++){
-            res=res*(row+1-i);
-            res=res/i;
-            ansRow.add((int)res);
+    public List<Integer> getRow(int rowIndex) {
+        int[] res={1};
+        for(int i=0;i<rowIndex;i++){
+            int[] temp=new int[res.length+1];
+            for(int j=0;j<res.length;j++){
+                temp[j]+=res[j];
+                temp[j+1]+=res[j];
+            }
+            res=temp;
         }
-        return ansRow;
 
+        List<Integer> ans=new ArrayList<>();
+        for(int num:res)    ans.add(num);
+        return ans;
     }
 }
