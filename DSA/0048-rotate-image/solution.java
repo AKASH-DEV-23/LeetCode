1063ee0a2a4462bdb54f6fca825b2efcc98dde4c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-05 04:55:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.54%)

--- DSA/0048-rotate-image/solution.java
+++ DSA/0048-rotate-image/solution.java
@@ -1,32 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int row=matrix.length;
-        int col=matrix[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=i+1;j<col;j++){
-                swap(matrix,i,j);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
         for(int[] mat:matrix){
             reverse(mat);
         }
     }
-
-    private void swap(int[][] matrix, int i, int j){
-        int temp=matrix[i][j];
-        matrix[i][j]=matrix[j][i];
-        matrix[j][i]=temp;
-    }
-
     private void reverse(int[] mat){
-        int i=0; 
+        int i=0;
         int j=mat.length-1;
-        while(i<=j){
+        while(i<j){
             int temp=mat[i];
-            mat[i]=mat[j];
-            mat[j]=temp;
-            i++;
-            j--;
+            mat[i++]=mat[j];
+            mat[j--]=temp;
         }
     }
 }
