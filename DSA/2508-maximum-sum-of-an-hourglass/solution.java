5d13b8d32091c6b8ff908bc00d2975de38d828be
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-05 07:12:32 +0000

    [commit] Runtime - 4 ms (93.35%), Memory - 45.4 MB (98.19%)

---
+++ DSA/2508-maximum-sum-of-an-hourglass/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<m-2;i++){
+            for(int j=0;j<n-2;j++){
+                int sum=findSum(grid,i,j);
+                ans=Math.max(sum,ans);
+            }
+        }
+        return ans;
+    }
+    private int findSum(int[][] grid, int i, int j){
+        int sum=0;
+        for(int idx=0;idx<3;idx++){
+            sum+=grid[i][j];
+            sum+=grid[i+2][j];
+            if(idx==1)  sum+=grid[i+1][j];
+            j++;
+        }
+        return sum;
+    }
+}
