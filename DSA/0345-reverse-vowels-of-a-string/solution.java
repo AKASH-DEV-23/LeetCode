1a44a984db942d2c748e043b063e6895728f72bc
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-06 03:30:54 +0000

    [Commit] Runtime - 8 ms (13.64%), Memory - 45 MB (99.90%)

--- DSA/0345-reverse-vowels-of-a-string/solution.java
+++ DSA/0345-reverse-vowels-of-a-string/solution.java
@@ -1,24 +1,19 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] arr=s.toCharArray();
-        int j=arr.length-1;
-        StringBuilder str = new StringBuilder();
-        for(int i=0;i<=j;i++){
-            char ch = arr[i];
-            if(ch-'a'==0 || ch-'e'==0 || ch-'i'==0 || ch-'o'==0 || ch-'u'==0 || ch-'A'==0 
-            || ch-'E'==0 || ch-'I'==0 || ch-'O'==0 || ch-'U'==0){
-                str.append(ch);
+        StringBuilder sb=new StringBuilder();
+        Set<Character> visi=new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+        for(char ch:s.toCharArray()){
+            if(visi.contains(ch))   sb.append(ch);
+        }
+        StringBuilder ans=new StringBuilder(s);
+        // System.out.println(sb);
+        sb.reverse();
+        int idx=0;
+        for(int i=0;i<ans.length();i++){
+            if(visi.contains(ans.charAt(i))){
+                ans.setCharAt(i,sb.charAt(idx++));
             }
         }
-        j=0;
-        for(int i=arr.length-1;i>=0;i--){
-            char ch = arr[i];
-            if(ch-'a'==0 || ch-'e'==0 || ch-'i'==0 || ch-'o'==0 || ch-'u'==0 || ch-'A'==0 
-            || ch-'E'==0 || ch-'I'==0 || ch-'O'==0 || ch-'U'==0){
-                arr[i]=str.charAt(j++);
-            }
-        }
-        // System.out.println(Arrays.toString(arr));
-        return new String(arr);
+        return ans.toString();
     }
 }
