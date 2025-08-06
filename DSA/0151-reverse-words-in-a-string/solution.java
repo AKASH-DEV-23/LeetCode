cd87e8f733af67e948c21d3347632fe8b0b3e0b4
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-06 19:31:24 +0000

    [Commit] Runtime - 7 ms (67.06%), Memory - 43.8 MB (96.27%)

--- DSA/0151-reverse-words-in-a-string/solution.java
+++ DSA/0151-reverse-words-in-a-string/solution.java
@@ -1,12 +1,20 @@
 class Solution {
-    public static String reverseWords(String s) {
-        String[] words = s.split(" +");
-        StringBuilder ans = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--) {
-            ans.append(words[i]);
-            ans.append(" ");
+    public String reverseWords(String s) {
+        s=s.trim();
+        String[] arr=s.split(" +");
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            String temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        StringBuilder ans=new StringBuilder();
+        for(String temp:arr){
+            ans.append(temp+" ");
         }
         return ans.toString().trim();
     }
 }
-
