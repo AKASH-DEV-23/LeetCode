7ead4540df37ab66579b7d123109a2595abac26e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-08 11:30:31 +0530

    Time: 1 ms (88.07%) | Memory: 41.7 MB (69.46%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,25 +1,10 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         int cnt=0;
-        for(int i=0;i<s.length();i++){
-            StringBuilder sb=new StringBuilder();
-            int idx=0;
-            for(int j=i;j<s.length();j++){
-                sb.append(s.charAt(j));
-                idx++;
-                if(idx==3)  break;
-            }
-            System.out.println(sb);
-            if(idx==3 && solve(sb.toString()))    cnt++;
+        for(int i=2;i<s.length();i++){
+            if(s.charAt(i)!=s.charAt(i-1) && s.charAt(i)!=s.charAt(i-2) 
+            && s.charAt(i-1)!=s.charAt(i-2))    cnt++;
         }
         return cnt;
-    }
-    private boolean solve(String s){
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<s.length();j++){
-                if(s.charAt(i)==s.charAt(j))    return false;
-            }
-        }
-        return true;
     }
 }
