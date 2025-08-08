fd75bfc5ec3a35d69fdf5903bb52c285f44f4b15
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-08 11:25:50 +0530

    Time: 19 ms (5.38%) | Memory: 43.4 MB (20.89%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,24 +1,25 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int j = 0;
-        int cnt = 0;
-        char[] nums = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if (i - j + 1 == 3) {
-                if (map.size() == 3)
-                    cnt++;
-                int val = map.get(nums[j]);
-                val--;
-                if (val == 0)
-                    map.remove(nums[j]);
-                else
-                    map.put(nums[j], val);
-                j++;
-                System.out.println(map);
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+            StringBuilder sb=new StringBuilder();
+            int idx=0;
+            for(int j=i;j<s.length();j++){
+                sb.append(s.charAt(j));
+                idx++;
+                if(idx==3)  break;
             }
+            System.out.println(sb);
+            if(idx==3 && solve(sb.toString()))    cnt++;
         }
         return cnt;
+    }
+    private boolean solve(String s){
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j))    return false;
+            }
+        }
+        return true;
     }
 }
