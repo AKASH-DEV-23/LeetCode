ff5a0b828ca7ee6ccde4b037f560b16e8b7c3ec9
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-08 06:00:22 +0000

    [Commit] Runtime - 1 ms (95.15%), Memory - 41.7 MB (100.00%)

--- DSA/1987-substrings-of-size-three-with-distinct-characters/solution.java
+++ DSA/1987-substrings-of-size-three-with-distinct-characters/solution.java
@@ -1,23 +1,9 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int j = 0;
-        int cnt = 0;
-        char[] nums = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if (i - j + 1 == 3) {
-                if (map.size() == 3)
-                    cnt++;
-                int val = map.get(nums[j]);
-                val--;
-                if (val == 0)
-                    map.remove(nums[j]);
-                else
-                    map.put(nums[j], val);
-                j++;
-                System.out.println(map);
-            }
+        int cnt=0;
+        for(int i=2;i<s.length();i++){
+            if(s.charAt(i)!=s.charAt(i-1) && s.charAt(i)!=s.charAt(i-2) 
+            && s.charAt(i-1)!=s.charAt(i-2))    cnt++;
         }
         return cnt;
     }
