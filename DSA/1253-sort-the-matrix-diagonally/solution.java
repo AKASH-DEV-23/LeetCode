9d4f6f41404723baf25cd2210f947064b40a4540
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-15 06:08:26 +0000

    [Commit] Runtime - 9 ms (61.64%), Memory - 44.9 MB (100.00%)

--- DSA/1253-sort-the-matrix-diagonally/solution.java
+++ DSA/1253-sort-the-matrix-diagonally/solution.java
@@ -1,20 +1,56 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        Map<Integer,PriorityQueue<Integer>> map=new HashMap<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int key=i-j;
-                if(!map.containsKey(key))   map.put(key,new PriorityQueue<>());
-                map.get(key).offer(mat[i][j]);
+        List<List<Integer>> clst=new ArrayList<>();
+        int m=mat.length-1;
+        int n=mat[0].length-1;
+        for(int idx=0;idx<=n;idx++){
+            int i=0;
+            int j=idx;
+            List<Integer> temp=new ArrayList<>();
+            while(i<=m && j<=n){
+                temp.add(mat[i][j]);
+                i++;
+                j++;
             }
+            clst.add(new ArrayList<>(temp));
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int key=i-j;
-                mat[i][j]=map.get(key).poll();
+        List<List<Integer>> rlst=new ArrayList<>();
+        for(int idx=0;idx<=m;idx++){
+            int i=idx;
+            int j=0;
+            List<Integer> temp=new ArrayList<>();
+            while(i<=m && j<=n){
+                temp.add(mat[i][j]);
+                i++;
+                j++;
             }
+            rlst.add(new ArrayList<>(temp));
+        }
+        for(int idx=0;idx<=n;idx++){
+            List<Integer> temp=clst.get(idx);
+                Collections.sort(temp);
+                int i=0;
+                int j=idx;
+                int itr=0;
+                while(i<=m && j<=n && itr<temp.size()){
+                    mat[i][j]=temp.get(itr);
+                    i++;
+                    j++;
+                    itr++;
+                }
+        }
+             for(int idx=0;idx<=m;idx++){
+                List<Integer> temp=rlst.get(idx);
+                int i=idx;
+                int j=0;
+                int itr=0;
+                Collections.sort(temp);
+                while(i<=m && j<=n && itr<temp.size()){
+                     mat[i][j]=temp.get(itr);
+                     i++;
+                     j++;
+                     itr++;
+                }
         }
         return mat;
     }
