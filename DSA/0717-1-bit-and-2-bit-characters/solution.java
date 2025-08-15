116fd85d0d3a558af39185b82349ee5496fefda7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-15 05:17:21 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 42.6 MB (99.96%)

---
+++ DSA/0717-1-bit-and-2-bit-characters/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i=0;
+        for(i=0; i<bits.length-1;++i){
+            if(bits[i]==1)  ++i;
+        }
+        if(i==bits.length-1)    return true;
+        return false;
+    }
+}
