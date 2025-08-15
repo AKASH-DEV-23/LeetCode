eba0c9f9071e92f8b9a5d0bbb71128fdbdc1fbf0
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-15 05:09:31 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 41 MB (99.96%)

--- DSA/0342-power-of-four/solution.java
+++ DSA/0342-power-of-four/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<=0)  return false;
-        return ((n&(n-1))==0 && (n-1)%3==0);
+        if(n==1)    return true;
+        if(n<=0 || n<=3 || n%4!=0)    return false;
+        return isPowerOfFour(n/4);
     }
 }
