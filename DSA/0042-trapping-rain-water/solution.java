0a77018a114bc39e263b677e96b3d8e7d4d3ed36
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-18 04:25:14 +0000

    [Commit] Runtime - 1 ms (58.84%), Memory - 46.1 MB (99.87%)

--- DSA/0042-trapping-rain-water/solution.java
+++ DSA/0042-trapping-rain-water/solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int[] left = new int[n];
-        left[0] = height[0];
-        for(int i = 1; i < n; i++){
-            left[i] = Math.max(left[i-1],height[i]);
+        int size=height.length;
+        int[] left=new int[size];
+        int[] right=new int[size];
+        left[0]=height[0];
+        for(int i=1;i<size;i++){
+            left[i]=Math.max(left[i-1],height[i]);
         }
-        int[] right = new int[n];
-        right[n-1] = height[n-1];
-        for(int i = n-2; i>=0;i--){
-            right[i] = Math.max(right[i+1],height[i]);
+        right[size-1]=height[size-1];
+        for(int i=size-2;i>=0;i--){
+            right[i]=Math.max(right[i+1],height[i]);
         }
-        int water = 0;
-        for(int i =0; i< n;i++){
-            water += Math.min(left[i],right[i])-height[i];
+        int ans=0;
+        for(int i=0;i<size;i++){
+            ans+=Math.min(left[i],right[i])-height[i];
         }
-        return water;
+        return ans;
     }
 }
