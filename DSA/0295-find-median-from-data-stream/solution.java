cd525e7cb1895cac3a6a53537ea24752c9ae710c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-18 09:27:07 +0000

    [commit] Runtime - 134 ms (61.76%), Memory - 63.9 MB (98.04%)

--- DSA/0295-find-median-from-data-stream/solution.java
+++ DSA/0295-find-median-from-data-stream/solution.java
@@ -1,27 +1,29 @@
 class MedianFinder {
-    PriorityQueue<Integer> minpq = new PriorityQueue<>();
-    PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
-
+    PriorityQueue<Integer> minpq=new PriorityQueue();
+    PriorityQueue<Integer> maxpq=new PriorityQueue(Collections.reverseOrder());
     public MedianFinder() {
-
+        
     }
-
+    
     public void addNum(int num) {
-        if (minpq.size() == maxpq.size()) {
-            maxpq.offer(num);
-            minpq.offer(maxpq.poll());
-        } else {
+        if(minpq.size()==maxpq.size()){
             minpq.offer(num);
             maxpq.offer(minpq.poll());
+        }else{
+            maxpq.offer(num);
+            minpq.offer(maxpq.poll());
         }
     }
-
+    
     public double findMedian() {
-        if (minpq.size() == maxpq.size()) {
-            return (minpq.peek() + maxpq.peek()) / 2.0;
-        } else {
-            return minpq.peek();
-        }
+        if(minpq.size()==maxpq.size())    return (minpq.peek()+maxpq.peek())/2.0;
+        else    return maxpq.peek();
     }
 }
 
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
