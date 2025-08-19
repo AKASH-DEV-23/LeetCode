bb3408834aaf4d1f21a5642c1c1dbd5ec30a218c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-19 02:47:17 +0000

    [commit] Runtime - 3 ms (96.58%), Memory - 61.4 MB (99.38%)

---
+++ DSA/2432-number-of-zero-filled-subarrays/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans=0;
+        long cnt=0;
+        for(int num:nums){
+            if(num==0)  cnt++;
+            else{
+                ans+=cnt*(cnt+1)/2;
+                cnt=0;
+            }
+        }
+        ans+=cnt*(cnt+1)/2;
+        return ans;
+    }
+}
