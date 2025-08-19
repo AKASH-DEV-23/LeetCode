c9df1423067dc3334ba8ffbdef53f6da80b27a42
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-19 17:18:36 +0000

    [Commit] Runtime - 2 ms (95.43%), Memory - 44.5 MB (99.93%)

--- DSA/0039-combination-sum/solution.java
+++ DSA/0039-combination-sum/solution.java
@@ -1,26 +1,19 @@
-
-        class Solution {
-  public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    List<List<Integer>> ans = new ArrayList<>();
-    Arrays.sort(candidates);
-    dfs(0, candidates, target, new ArrayList<>(), ans);
-    return ans;
-  }
-
-  private void dfs(int s, int[] candidates, int target, List<Integer> path,
-                   List<List<Integer>> ans) {
-    if (target < 0)
-      return;
-    if (target == 0) {
-      ans.add(new ArrayList<>(path));
-      return;
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        solve(0,candidates,0,target,new ArrayList<>(),ans);
+        return ans;
     }
-
-    for (int i = s; i < candidates.length; ++i) {
-      path.add(candidates[i]);
-      dfs(i, candidates, target - candidates[i], path, ans);
-      path.remove(path.size() - 1);
+    private void solve(int idx,int[] nums, int sum, int target, List<Integer> temp, List<List<Integer>> ans){
+        if(sum>target)  return;
+        if(sum==target){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=idx;i<nums.length;i++){
+            temp.add(nums[i]);
+            solve(i,nums,sum+nums[i],target,temp,ans);
+            temp.remove(temp.size()-1);
+        }
     }
-  }
 }
-        
