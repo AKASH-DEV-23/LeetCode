a5267b1f5dfb0794bd838956ae3fb92acf9e548e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-08-30 12:28:28 +0000

    [commit] Runtime - 13 ms (11.31%), Memory - 45 MB (96.86%)

--- DSA/0036-valid-sudoku/solution.java
+++ DSA/0036-valid-sudoku/solution.java
@@ -1,17 +1,13 @@
 class Solution {
-    public static boolean isValidSudoku(char[][] board) {
-        Set<String> set = new HashSet<>();
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    continue;
-                }
-                char c = board[row][col];
-                if (!set.add(c + "in row" + row) ||
-                        !set.add(c + "in col" + col) ||
-                        !set.add(c + "in box" + row / 3 + "_" + col / 3)) {
-                    return false;
-                }
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> vis=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')    continue;
+                char ch=board[i][j];
+                if(!vis.add(ch+"found on i"+i) || 
+                !vis.add(ch+"found on j"+j) || 
+                !vis.add(ch+"found on"+ i/3+"-"+j/3))  return false;
             }
         }
         return true;
