11ce82f81e4d761f531d2d890061690ca331e8e9
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-09-07 00:28:40 +0530

    Time: 2 ms (4.04%) | Memory: 45.6 MB (90.89%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,12 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return solve(nums,0,nums.length-1,target);
-    }
-    private int solve(int[] nums, int low, int high, int target){
-        if(low>high)    return -1;
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target)   return mid;
-        else if(nums[mid]<target)  return solve(nums,mid+1,high,target);
-        else  return solve(nums,low,mid-1,target);
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=(right-left)+left/2;
+            if(nums[mid]==target)   return mid;
+            if(target<nums[mid])    right=mid-1;
+            else    left=mid+1;
+        }
+        return -1;
     }
 }
