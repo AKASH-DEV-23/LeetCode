6b819f2e8c05a52f71f5723ecbc26438fe1fbca7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-09-06 18:55:07 +0000

    [commit] Runtime - 1 ms (99.72%), Memory - 46 MB (99.15%)

--- DSA/0004-median-of-two-sorted-arrays/solution.java
+++ DSA/0004-median-of-two-sorted-arrays/solution.java
@@ -1,35 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Get the sizes of both input arrays.
-        int n = nums1.length;
-        int m = nums2.length;
-
-        // Merge the arrays into a single sorted array.
-        int[] merged = new int[n + m];
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            merged[k++] = nums1[i];
+        int m=nums1.length;
+        int n=nums2.length;
+        int len=m+n;
+        int[] temp=new int[len];
+        int i=0;  
+        int j=0;
+        int idx=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j])   temp[idx++]=nums1[i++];
+            else    temp[idx++]=nums2[j++];
         }
-        for (int i = 0; i < m; i++) {
-            merged[k++] = nums2[i];
+        while(i<m)  temp[idx++]=nums1[i++];
+        while(j<n)  temp[idx++]=nums2[j++];
+        if(len%2==0){
+            return (double)(temp[len/2]+temp[(len/2)-1])/2;
         }
-
-        // Sort the merged array.
-        Arrays.sort(merged);
-
-        // Calculate the total number of elements in the merged array.
-        int total = merged.length;
-
-        if (total % 2 == 1) {
-            // If the total number of elements is odd, return the middle element as the median.
-            return (double) merged[total / 2];
-        } else {
-            // If the total number of elements is even, calculate the average of the two middle elements as the median.
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return ((double) middle1 + (double) middle2) / 2.0;
-        }
+        return temp[len/2];
     }
 }
