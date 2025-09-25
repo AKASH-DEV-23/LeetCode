5f899b5e835e087bb687bb9c5dc3c6cc0a26f60e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-09-25 07:00:21 +0000

    Sync LeetCode submission Runtime - 8 ms (20.95%), Memory - 45.4 MB (74.68%)

--- DSA/0120-triangle/solution.java
+++ DSA/0120-triangle/solution.java
@@ -1,30 +1,19 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> list) {
-        int n=list.size();
-        int[][] grid=new int[n][];
-        for(int i=0;i<n;i++){
-            grid[i]=new int[i+1];
-        }
-        grid[0][0]=list.get(0).get(0);
-        for(int row=1;row<n;row++){
-            for(int col=0;col<grid[row].length;col++){
-                grid[row][col]=list.get(row).get(col)+findMin(grid,row,col);
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        for(int i=n-2; i>=0; i--){
+            for(int j=0;j<=i;j++){
+                int currEle=triangle.get(i).get(j);
+                int minValue=Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
+                triangle.get(i).set(j, currEle+minValue);
             }
         }
-        int min=Integer.MAX_VALUE;
-        for(int col=0;col<n;col++){
-            min=Math.min(min,grid[n-1][col]);
-        }
-        // System.out.println(Arrays.deepToString(grid));
-        return min;
-    }
-    private int findMin(int[][] grid, int row, int col){
-        int min=Integer.MAX_VALUE;
-        if(col<grid[row-1].length)
-            min=Math.min(min,grid[row-1][col]);
-        if(col-1>=0){
-            min=Math.min(min,grid[row-1][col-1]);
-        }
-        return min==Integer.MAX_VALUE ? 0 : min;
+        return triangle.get(0).get(0);
     }
 }
+
+
+    //             2
+    //         3       4
+    //     6       5       7       <--  st from this pick curr ele + min(nextRow,nextRow+1);
+    // 4       1       8       3
