d760a5d75d8c6589c3847ce96b127e4b4437572e
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-09-26 10:37:23 +0530

    Time: 25 ms (35.42%) | Memory: 43.5 MB (38.21%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=n-1;i>=0;i--){
+            int low=0;
+            int high=i-1;
+            while(low<high){
+                if(nums[low]+nums[high]>nums[i]){
+                    count+=(high-low);
+                    high--;
+                }else{
+                    low++;
+                }
+            }
+        }
+        return count;
+    }
+}
