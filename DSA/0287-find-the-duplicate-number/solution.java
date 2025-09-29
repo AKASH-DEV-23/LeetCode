fc14f186fd5d5dfc16555e913e8d936b14fc2216
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-09-29 13:30:14 +0000

    [commit] Runtime - 5 ms (60.78%), Memory - 58.9 MB (97.26%)

--- DSA/0287-find-the-duplicate-number/solution.java
+++ DSA/0287-find-the-duplicate-number/solution.java
@@ -1,10 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums){
-            if(!set.add(num))
-                return num;
+        int slow=nums[0];
+        int fast=nums[0];
+        
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+
+        slow=nums[0];
+
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return 0;
+        return slow;
     }
 }
