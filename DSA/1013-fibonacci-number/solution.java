9b5b154b801f2601dbaf97e7c65d20ad98bb2cbc
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-10-02 16:46:18 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 40.4 MB (98.41%)

--- DSA/1013-fibonacci-number/solution.java
+++ DSA/1013-fibonacci-number/solution.java
@@ -1,8 +1,9 @@
 class Solution {
     public int fib(int n) {
-        int[] dp=new int[31];
+        if(n<=1)    return n;
+        int[] dp=new int[n+1];
         dp[1]=1;
-        for(int i=2;i<=30;i++){
+        for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
