a306458c2e67e3c570958453f1d02d186d2359f3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-10-02 22:14:49 +0530

    Time: 8 ms (45.33%) | Memory: 40.1 MB (92.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,6 @@
 class Solution {
     public int fib(int n) {
-        int[] dp=new int[31];
-        dp[1]=1;
-        for(int i=2;i<=30;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        if(n<=1)    return n;
+        return fib(n-1)+fib(n-2);
     }
 }
