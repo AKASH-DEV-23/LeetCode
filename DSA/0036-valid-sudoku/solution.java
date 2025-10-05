08e3b0b165d901a9861019cd902211b1cdc65a02
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-10-05 12:48:13 +0000

    [Commit] Runtime - 15 ms (6.96%), Memory - 45 MB (99.93%)

--- DSA/0036-valid-sudoku/solution.java
+++ DSA/0036-valid-sudoku/solution.java
@@ -3,11 +3,11 @@ class Solution {
         Set<String> vis=new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.')    continue;
                 char ch=board[i][j];
-                if(!vis.add(ch+"found on i"+i) || 
-                !vis.add(ch+"found on j"+j) || 
-                !vis.add(ch+"found on"+ i/3+"-"+j/3))  return false;
+                if(ch=='.') continue;
+                if(!vis.add(ch+"in row"+i)) return false;
+                if(!vis.add(ch+"in col"+j)) return false; 
+                if(!vis.add(ch+"in box"+i/3+"-"+j/3)) return false;
             }
         }
         return true;
