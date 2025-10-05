cd8708c1aca104633346eba0b054d4113eefafea
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-10-05 18:28:49 +0000

    [Commit] Runtime - 1 ms (99.79%), Memory - 57 MB (99.90%)

--- DSA/0053-maximum-subarray/solution.java
+++ DSA/0053-maximum-subarray/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum=Integer.MIN_VALUE;
         int sum=0;
+        int maxi=nums[0];
         for(int num:nums){
-            if(sum<0)   sum=0;
             sum+=num;
-            maxSum=Math.max(maxSum,sum);
+            maxi=Math.max(maxi,sum);
+            if(sum<0)   sum=0;
         }
-        return maxSum;
+        return maxi;
     }
 }
