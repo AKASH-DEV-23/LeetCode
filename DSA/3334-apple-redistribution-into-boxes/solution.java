1a7e38e4c1d4d2b63e71e3d29127bd1ecf273f49
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-12-24 03:47:38 +0000

    [commit] Runtime - 0 ms (100.00%), Memory - 45.1 MB (5.95%)

---
+++ DSA/3334-apple-redistribution-into-boxes/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
+    }
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int totalApples=0;
+        for(int app:apple){
+            totalApples+=app;
+        }
+        Arrays.sort(capacity);
+        for(int i=capacity.length-1;i>=0;i--){
+            totalApples-=capacity[i];
+            if(totalApples<1)   return capacity.length-i;
+        }
+        return -1;
+    }
+}
