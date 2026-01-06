8ab8045de97cd8f85adf6feac29c2df9f9412f42
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-01-06 06:31:14 +0000

    [Commit] Runtime - 8 ms (93.82%), Memory - 49.4 MB (26.34%)

---
+++ DSA/1116-maximum-level-sum-of-a-binary-tree/solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+       if(root==null) return 0;
+        Queue<TreeNode> qobj = new LinkedList<>();
+        qobj.offer(root);
+        int lvl=1;
+        int currLvl=1;
+        int maxSum=Integer.MIN_VALUE;
+        while(!qobj.isEmpty()){
+            int size=qobj.size();
+            int sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode curr = qobj.poll();
+                sum+=curr.val;
+                if(curr.left!=null){
+                    qobj.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    qobj.offer(curr.right);
+                }
+            }
+            if(maxSum<sum){
+                maxSum=sum;
+                lvl=currLvl;
+            }
+            currLvl++;
+        }
+        return lvl;
+
+        
+    }
+}
