204d4ff3140f3cb411f6ccd778f3a70b18a31e85
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-01-08 14:37:28 +0000

    [Commit] Runtime - 11 ms (38.62%), Memory - 48.6 MB (10.32%)

---
+++ DSA/1569-max-dot-product-of-two-subsequences/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    Integer[][] dp;
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        dp=new Integer[nums1.length][nums2.length];
+        return solve(nums1,nums2,0,0);
+    }
+    private int solve(int[] nums1, int[] nums2, int i, int j){
+        if(i==nums1.length || j==nums2.length)  return Integer.MIN_VALUE;
+        if(dp[i][j]!=null)  return dp[i][j];
+        int takeI=solve(nums1,nums2,i,j+1);
+        int takeJ=solve(nums1,nums2,i+1,j);
+        int takeBoth=(nums1[i]*nums2[j])+Math.max(0,solve(nums1,nums2,i+1,j+1));
+        return dp[i][j]=Math.max(takeBoth,Math.max(takeI,takeJ));
+    }
+}
