ff131716cde7b06e1c60461cdb5432af309afc2a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-01-09 12:39:37 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 44.4 MB (49.64%)

--- DSA/0104-maximum-depth-of-binary-tree/solution.java
+++ DSA/0104-maximum-depth-of-binary-tree/solution.java
@@ -15,19 +15,11 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        return depth(root);
+    }
+    
+    private int depth(TreeNode root){
         if(root==null)  return 0;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        int depth=0;
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode curr=q.poll();
-                if(curr.left!=null) q.offer(curr.left);
-                if(curr.right!=null)    q.offer(curr.right);
-            }
-            depth++;
-        }
-        return depth;
+        return 1+Math.max(depth(root.left),depth(root.right));
     }
 }
