a81cf70ba502402c6d36355a5dcccf0d20d0c2fc
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-01-09 12:59:16 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 43.4 MB (94.39%)

--- DSA/0896-smallest-subtree-with-all-the-deepest-nodes/solution.java
+++ DSA/0896-smallest-subtree-with-all-the-deepest-nodes/solution.java
@@ -16,12 +16,13 @@
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
         if(root==null)  return root;
-        int leftHeight=getHeight(root.left);
-        int rightHeight=getHeight(root.right);
-        if(leftHeight==rightHeight) return root;
-        else if(leftHeight>rightHeight) return subtreeWithAllDeepest(root.left);
+        int leftH=getHeight(root.left);
+        int rightH=getHeight(root.right);
+        if(leftH==rightH)    return root;
+        if(leftH>rightH)    return subtreeWithAllDeepest(root.left);
         else    return subtreeWithAllDeepest(root.right);
     }
+
     private int getHeight(TreeNode root){
         if(root==null)  return 0;
         return 1+Math.max(getHeight(root.left),getHeight(root.right));
