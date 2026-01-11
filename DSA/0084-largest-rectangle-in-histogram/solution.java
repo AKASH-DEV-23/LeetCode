23e36fca099671d4767d09559b8e796a894b2ae3
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-01-11 11:26:15 +0000

    [Commit] Runtime - 108 ms (22.50%), Memory - 76.3 MB (85.15%)

--- DSA/0084-largest-rectangle-in-histogram/solution.java
+++ DSA/0084-largest-rectangle-in-histogram/solution.java
@@ -1,32 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int maxi=0;
         int size=heights.length;
         int[] left=new int[size];
-        int[] right=new int[size];
-        Stack<Integer> st=new Stack<>();
+        Stack<Integer> st=new Stack();
         for(int i=0;i<size;i++){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            if(st.isEmpty())    left[i]=-1;
-            else    left[i]=st.peek();
+            left[i]=st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
+
         st.clear();
-        int len=size;
-        for(int i=len-1;i>=0;i--){
+
+        int[] right=new int[size];
+        for(int i=size-1;i>=0;i--){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            if(st.isEmpty())    right[i]=size;
-            else    right[i]=st.peek();
+            right[i]=st.isEmpty() ? size : st.peek();
             st.push(i);
         }
+
+        int ans=Integer.MIN_VALUE;
         for(int i=0;i<size;i++){
             int width=right[i]-left[i]-1;
-            maxi=Math.max(maxi,heights[i]*width);
+            ans=Math.max(ans,heights[i]*width);
+
         }
-        return maxi;
+        return ans;
+
     }
 }
