017a80a0ac0c4fe2fa467011b50bc46c0c055dca
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-03-22 02:42:07 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 45 MB (100.00%)

---
+++ DSA/4256-construct-uniform-parity-array-i/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int even=0;
+        // int odd=0;
+        // int size=nums1.length;
+        // for(int num:nums1){
+        //     if(num%2==0)  even++;
+        //     else  odd++;
+        // }
+
+        return true;
+    }
+}
