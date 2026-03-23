fdee2fcb8dbe31be830990d759aa37aae0275739
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-03-23 05:32:59 +0000

    [Commit] Runtime - 4 ms (64.08%), Memory - 50.4 MB (32.52%)

--- DSA/0064-minimum-path-sum/solution.java
+++ DSA/0064-minimum-path-sum/solution.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int minPathSum(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[][] grid=new int[m][n];
-        grid[0][0]=matrix[0][0];
-        for(int j=1;j<n;j++){
-            grid[0][j]=matrix[0][j]+grid[0][j-1];
+    public int minPathSum(int[][] grid) {
+        int row=grid.length;
+        int col=grid[0].length;
+        for(int j=1;j<col;j++){
+            grid[0][j]=grid[0][j-1]+grid[0][j];
         }
-        for(int i=1;i<m;i++){
-            grid[i][0]=matrix[i][0]+grid[i-1][0];
+        for(int i=1;i<row;i++){
+            grid[i][0]=grid[i-1][0]+grid[i][0];
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                grid[i][j]=matrix[i][j]+Math.min(grid[i-1][j],grid[i][j-1]);
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        // System.out.println(Arrays.deepToString(grid));
-        return grid[m-1][n-1];
+
+        return grid[row-1][col-1];
     }
 }
