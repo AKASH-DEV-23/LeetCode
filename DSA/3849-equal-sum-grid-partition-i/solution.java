5d578c56b55d4a41bdc6857f337e30fc358b0ab1
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-03-25 07:18:02 +0000

    [Commit] Runtime - 4 ms (97.20%), Memory - 160.5 MB (81.12%)

--- DSA/3849-equal-sum-grid-partition-i/solution.java
+++ DSA/3849-equal-sum-grid-partition-i/solution.java
@@ -2,44 +2,38 @@ class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int row=grid.length;
         int col=grid[0].length;
-        long[] hori=new long[row];
-        long[] verti=new long[col];
-        int h=0;
-        int v=0;
-        long hsum=0;
-        long vsum=0;
-        for(int i=0;i<row;i++){
+        long[] rowSum=new long[row];
+        long[] colSum=new long[col];
+        int idx=0;
+        long totalSum=0;
+        for(int[] temp:grid){
             long sum=0;
-            for(int j=0;j<col;j++){
+            for(int curr:temp){
+                sum+=curr;
+            }
+            totalSum+=sum;
+            rowSum[idx++]=sum;
+        }
+        if(totalSum%2!=0)   return false;
+        idx=0;
+        for(int j=0;j<col;j++){
+            long sum=0;
+            for(int i=0;i<row;i++){
                 sum+=grid[i][j];
             }
-            hori[h++]=sum;
-            hsum+=sum;
+            colSum[idx++]=sum;
         }
-        for(int i=0;i<col;i++){
-            long sum=0;
-            for(int j=0;j<row;j++){
-                sum+=grid[j][i];
-            }
-            verti[v++]=sum;
-            vsum+=sum;
+        long sumDiff=totalSum/2;
+        for(long currSum:rowSum){
+            sumDiff-=currSum;
+            if(sumDiff==0)  return true;
+            if(sumDiff<0)   break;
         }
-        if(hsum%2!=0 && vsum%2!=0)    return false;
-        long thsum=hsum/2;
-        long tvsum=vsum/2;
-        long currsum=0;
-        if(hsum%2==0){
-            for(int i=0;i<hori.length-1;i++){
-                currsum+=hori[i];
-                if(currsum==thsum)    return true;
-            }
-        }
-        currsum=0;
-        if(vsum%2==0){
-            for(int i=0;i<verti.length-1;i++){
-                currsum+=verti[i];
-                if(currsum==tvsum)    return true;
-            }
+        sumDiff=totalSum/2;
+        for(long currSum:colSum){
+            sumDiff-=currSum;
+            if(sumDiff==0)  return true;
+            if(sumDiff<0)   break;
         }
         return false;
     }
