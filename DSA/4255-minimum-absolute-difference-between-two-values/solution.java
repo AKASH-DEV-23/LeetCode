923e45d6e8d6df501ff31947a50ac600df61ae03
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-03-28 14:35:24 +0000

    [Commit] Runtime - 3 ms (0.00%), Memory - 46.7 MB (0.00%)

---
+++ DSA/4255-minimum-absolute-difference-between-two-values/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAbsoluteDifference(int[] nums) {
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==1 && nums[j]==2){
+                    ans=Math.min(ans,Math.abs(j-i));
+                }else if(nums[i]==2 && nums[j]==1){
+                    ans=Math.min(ans,Math.abs(j-i));
+                }
+            }
+        }
+
+        return ans==Integer.MAX_VALUE ? -1 : ans;
+    }
+}
