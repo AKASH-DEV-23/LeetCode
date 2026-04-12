d2164cb459a653a0834a8f492b15dde0efc9a67b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-04-12 07:40:41 +0000

    [Commit] Runtime - 1 ms (16.79%), Memory - 48.1 MB (49.94%)

--- DSA/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ DSA/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int st=0;
-        int end=nums.length-1;
-        int mid=0;
-        boolean flag=true;
-        while(st<=end){
-            mid=(st+end)/2;
-            if(nums[mid]==target){
-                flag=false;
-                break;
-            }
-            else if(target<nums[mid]){
-                end=mid-1;
-            }else   st=mid+1;
+        int[] arr=new int[nums.length+1];
+        int idx=0;
+        for(int num:nums)   arr[idx++]=num;
+        arr[idx]=Integer.MAX_VALUE;
+        int lb=lowerBound(arr,target);
+        int ub=upperBound(arr,target);
+        if(nums.length==0 || arr[lb]!=target)    return new int[]{-1,-1};
+        ub-=1;
+        return new int[]{lb,ub};
+    }
+
+    private int lowerBound(int[] nums, int target){
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<target)    low=mid+1;
+            else    high=mid;
         }
-        if(flag)    return new int[]{-1,-1};
-        int i=mid;
-        while(i>=0 && nums[i]==target)  i--;
-        while(mid<nums.length && nums[mid]==target) mid++;
-        return new int[]{i+1,mid-1};
+        return low;
+    }
+
+    private int upperBound(int[] nums, int target){
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<=target)   low=mid+1;
+            else   high=mid;
+        }
+        return low;
     }
 }
