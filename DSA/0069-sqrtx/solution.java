3e40c376d3f973a99e021da8f707c8793cc27771
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-04-14 06:49:54 +0000

    [Commit] Runtime - 1 ms (97.92%), Memory - 42.8 MB (8.34%)

--- DSA/0069-sqrtx/solution.java
+++ DSA/0069-sqrtx/solution.java
@@ -1,5 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if(x<=1)    return x;
+        int low=1;
+        int high=x;
+        while(low<high){
+            int mid=(low+high) >>> 1;
+            if((long)mid*mid>x){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low-1;
     }
 }
