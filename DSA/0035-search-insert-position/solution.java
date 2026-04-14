66901b0b10291e387a969212e2aa662cb803d2e1
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-04-14 06:57:15 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 44.8 MB (47.43%)

--- DSA/0035-search-insert-position/solution.java
+++ DSA/0035-search-insert-position/solution.java
@@ -3,9 +3,12 @@ class Solution {
         int low=0;
         int high=nums.length;
         while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]<target)    low=mid+1;
-            else    high=mid;
+            int mid=(low+high)  >>> 1;
+            if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
         }
         return low;
     }
