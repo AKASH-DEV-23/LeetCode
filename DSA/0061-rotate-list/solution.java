67bbdbd5367bd4b7db67aae232a342d86dd68349
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-05-05 01:44:41 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 44.4 MB (22.02%)

--- DSA/0061-rotate-list/solution.java
+++ DSA/0061-rotate-list/solution.java
@@ -1,34 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || k == 0) {
-            return head;
+        if(k==0 || head==null || head.next==null) return head;
+        int len=0;
+        ListNode temp=head;
+        ListNode tail=head;
+        while(temp!=null){
+            if(temp.next==null){
+                tail=temp;
+            }
+            len++;
+            temp=temp.next;
         }
-        ListNode temp = head;
-        int length = 0;
-        while (temp.next != null) {
-            length++;
-            temp = temp.next;
+        k=k%len;
+        k=len-k;
+        ListNode current=head;
+        for(int i=1;i<k;i++){
+            current=current.next;
         }
-        length++;
-        temp.next = head;
-        int rotate = length - k % length;
-        temp = head;
-        while (rotate-- > 1) {
-            temp = temp.next;
-        }
-        head = temp.next;
-        temp.next = null;
-        return head;
+        
+        tail.next=head;
+        ListNode and =current.next;
+        current.next=null;
+        return and;
     }
 }
