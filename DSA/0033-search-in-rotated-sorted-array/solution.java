00e7087de2cde3ad804a407c4cb2370b4b1a6f73
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-05-22 16:48:29 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 43.9 MB (45.99%)

--- DSA/0033-search-in-rotated-sorted-array/solution.java
+++ DSA/0033-search-in-rotated-sorted-array/solution.java
@@ -1,31 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot=findPivot(nums);
-        int ans1= binarySearch(nums,0,pivot-1,target); 
-        int ans2 = binarySearch(nums,pivot,nums.length-1,target);
-        if(ans1 ==-1 && ans2==-1)   return -1;
-        if(ans1 ==-1)   return ans2;
-        return ans1;    
-    }
-    private int findPivot(int[] nums){
-        int l=0;
-        int r=nums.length-1;
-        while(l<r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums[r]){
-                l=mid+1;
-            }else{
-                r=mid;
-            }
-        }
-        return l;
-    }
-    private int binarySearch(int[] nums, int l, int r, int target){
-        while(l<=r){
-            int mid=l+(r-l)/2;
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
             if(nums[mid]==target)   return mid;
-            if(target<nums[mid])    r=mid-1;
-            else l=mid+1;
+
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
         }
         return -1;
     }
