5d76f0497ee8b36ef6fc08d42657e55f58972e3c
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-12 13:02:05 +0000

    [Commit] Runtime - 4 ms (8.93%), Memory - 46.7 MB (96.58%)

--- DSA/0057-insert-interval/solution.java
+++ DSA/0057-insert-interval/solution.java
@@ -1,39 +1,23 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> myList=new ArrayList<>();
-        boolean flag=true;
-        for(int[] edge:intervals){
-            myList.add(edge);
-        }
-        myList.add(new int[]{newInterval[0],newInterval[1]});
-        Comparator<int[]> com=new Comparator<>(){
-            public int compare(int[] a, int[] b){
-                if(a[0]<b[0])   return -1;
-                else if(a[0]>b[0])  return 1;
-                else return 0;
-            }
-        };
-        Collections.sort(myList,com);
-        // myList.forEach(arr -> System.out.print(Arrays.toString(arr)));
-        int[] temp=new int[]{myList.get(0)[0],0};
-        List<int[]> ans=new ArrayList<>();
-        int lastTime=myList.get(0)[1];
-        for(int i=1;i<myList.size();i++){
-            int st=myList.get(i)[0];
-            int end=myList.get(i)[1];
-            if(st>lastTime){
-                temp[1]=lastTime;
-                ans.add(temp);
-                temp=new int[]{st,0};
-                lastTime=end;
+        List<int[]> mergeList=new ArrayList<>(Arrays.asList(intervals));
+        mergeList.add(newInterval);
+        Collections.sort(mergeList,(a,b)-> a[0]==b[0] ? Integer.compare(a[1],b[1]) : Integer.compare(a[0],b[0]));
+
+        int[] prev=mergeList.get(0);
+        List<int[]> result=new ArrayList<>();
+
+        for(int i=1;i<mergeList.size();i++){
+            int[] current=mergeList.get(i);
+            if(prev[1]>=current[0]){
+                prev[1]=Math.max(prev[1],current[1]);
             }else{
-                lastTime=Math.max(lastTime,end);
+                result.add(prev);
+                prev=current;
             }
         }
-        temp[1]=lastTime;
-        ans.add(temp);
-        ans.forEach(arr -> System.out.print(Arrays.toString(arr)));
-        int[][] finalAns=ans.toArray(new int[0][0]);
-        return finalAns;
+
+        result.add(prev);
+        return result.toArray(new int[0][]);
     }
 }
