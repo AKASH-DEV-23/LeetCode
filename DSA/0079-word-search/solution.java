fc6202573866ef0f9ded15869cafdc5a43f7caad
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-13 11:41:15 +0000

    [Commit] Runtime - 156 ms (31.42%), Memory - 42.8 MB (75.10%)

--- DSA/0079-word-search/solution.java
+++ DSA/0079-word-search/solution.java
@@ -1,26 +1,33 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==word.charAt(0) && solve(board,word,i,j,0))    return true;
+        int row=board.length;
+        int col=board[0].length;
 
+        boolean[][] visited=new boolean[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j]==word.charAt(0) && checkWordInFourDirection(board,i,j,word,1,visited))   return true;
             }
         }
+
         return false;
     }
-    private boolean solve(char[][] board, String word, int i, int j, int idx){
-        if(idx>=word.length())    return true;
-        if(i<0 || i>=board.length || j<0 || j>=board[i].length || 
-        board[i][j]!=word.charAt(idx) || board[i][j]=='*')    return false;
-        char temp=board[i][j];
-        board[i][j]='*';
-        if(solve(board,word,i+1,j,idx+1))  return true;
-        if(solve(board,word,i-1,j,idx+1))  return true;
-        if(solve(board,word,i,j+1,idx+1))  return true;
-        if(solve(board,word,i,j-1,idx+1))   return true;
-        board[i][j]=temp;
+
+    private final int[] dirX={-1,1,0,0};
+    private final int[] dirY={0,0,-1,1};
+
+    private boolean checkWordInFourDirection(char[][] board, int i, int j, String word, int index, boolean[][] visited){
+        if(index==word.length())    return true;
+        visited[i][j]=true;
+
+        for(int cor=0;cor<4;cor++){
+            int currX=i+dirX[cor];
+            int currY=j+dirY[cor];
+            if(currX<0 || currX==board.length || currY<0 || currY==board[0].length)  continue;
+            if(visited[currX][currY] || board[currX][currY]!=word.charAt(index))  continue;
+            if(checkWordInFourDirection(board,currX,currY,word,index+1,visited)) return true;
+        }
+        visited[i][j]=false;
         return false;
     }
 }
