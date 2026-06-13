590c3c52ab5ad445f14ea10706fc753224d7820a
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-13 12:58:57 +0000

    [Commit] Runtime - 13 ms (96.17%), Memory - 43.6 MB (70.26%)

--- DSA/0005-longest-palindromic-substring/solution.java
+++ DSA/0005-longest-palindromic-substring/solution.java
@@ -1,36 +1,34 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
+        int left=0;
+        int right=0;
 
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-        s = "#" + s.replaceAll("", "#") + "#";
-        int[] dp = new int[s.length()];
-        int center = 0;
-        int right = 0;
+        int len=s.length();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i < right) {
-                dp[i] = Math.min(right - i, dp[2 * center - i]);
-            }
+        for(int i=0;i<len;i++){
 
-            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s.charAt(i - dp[i] - 1) == s.charAt(i + dp[i] + 1)) {
-                dp[i]++;
-            }
+            int len1=expandAroundCenter(s,i,i);
 
-            if (i + dp[i] > right) {
-                center = i;
-                right = i + dp[i];
-            }
+            int len2=expandAroundCenter(s,i,i+1);
 
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxStr = s.substring(i - dp[i], i + dp[i] + 1).replaceAll("#", "");
+            int maxLen=Math.max(len1,len2);
+
+            if(maxLen>(right-left)){
+                left=i-(maxLen-1)/2;
+                right=i+maxLen/2;
             }
         }
 
-        return maxStr;
+        return s.substring(left,right+1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right){
+
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return right-left-1;
     }
 }
