75f81a3a16f5bfdd3db7340f29be150dfc1482e6
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-18 16:30:41 +0000

    [Commit] Runtime - 2 ms (91.87%), Memory - 47.9 MB (37.62%)

--- DSA/0283-move-zeroes/solution.java
+++ DSA/0283-move-zeroes/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                swap(nums,i,j);
-                i++;
-            }
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                swap(nums,left,right);
+                left++;
+            }    
         }
-
     }
-    private void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+
+    private void swap(int[] nums, int left, int right){
+        int temp=nums[left];
+        nums[left]=nums[right];
+        nums[right]=temp;
     }
 }
