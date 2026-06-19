a8882214b83c90fb0858888a322dc87a7314bc3f
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-19 03:26:25 +0000

    [Commit] Runtime - 0 ms (100.00%), Memory - 46.4 MB (85.78%)

--- DSA/0142-linked-list-cycle-ii/solution.java
+++ DSA/0142-linked-list-cycle-ii/solution.java
@@ -12,21 +12,25 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         if(head==null || head.next==null)   return null;
+        
         ListNode slow=head;
         ListNode fast=head;
+
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast){
-                break;
-            }
+            if(slow==fast)  break;
         }
+
         if(slow!=fast)  return null;
-        slow=head;
-        while(slow!=fast){
+
+        ListNode startNode=head;
+
+        while(slow!=startNode){
             slow=slow.next;
-            fast=fast.next;
+            startNode=startNode.next;
         }
+
         return slow;
     }
 }
