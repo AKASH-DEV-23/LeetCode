1f03d99fe7a1ea4e5d9e5d6c61287788bb063477
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-06-20 14:38:35 +0000

    [Commit] Runtime - 4 ms (17.06%), Memory - 46.5 MB (68.08%)

---
+++ DSA/4342-create-grid-with-exactly-one-path/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] createGrid(int m, int n) {
+        String[] answer=new String[m];
+
+        for(int i=0;i<m;i++){
+            StringBuilder sb=new StringBuilder();
+            for(int j=0;j<n;j++){
+                if(i==0 || j==n-1){
+                    sb.append(".");
+                }else{
+                    sb.append("#");
+                }
+            }
+            answer[i]=sb.toString();
+        }
+
+        return answer;
+    }
+}
