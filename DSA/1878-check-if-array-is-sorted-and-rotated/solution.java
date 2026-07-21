ee8af7289f73d140aec352ed96341ef1ad491b32
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-07-21 17:10:19 +0000

    [commit] - Check if Array Is Sorted and Rotated - Runtime - 0 ms (100.00%), Memory - 43 MB (61.83%)

--- DSA/1878-check-if-array-is-sorted-and-rotated/solution.java
+++ DSA/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean check(int[] nums) {
-        int len=nums.length;
-        int cnt=0;
-        for(int i=1;i<=nums.length;i++){
-            if(nums[(i-1)%len]>nums[i%len]) cnt++;
+        if(nums==null || nums.length==0 || nums.length==2)  return true;
+        int slop=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length])   slop++;
         }
-        return cnt>1?false:true;
+        return slop<2;
     }
 }
