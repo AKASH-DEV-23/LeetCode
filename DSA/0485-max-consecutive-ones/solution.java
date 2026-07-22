147edd99c249016527f30f702d24de124fb3f5f2
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-07-22 17:20:00 +0000

    [commit] - Max Consecutive Ones - Runtime - 3 ms (60.24%), Memory - 52.6 MB (26.25%)

--- DSA/0485-max-consecutive-ones/solution.java
+++ DSA/0485-max-consecutive-ones/solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int max=0;
-        int cnt=0;
+        if(nums==null || nums.length==0)    return -1;
+        int ans=0;
+        int cntZero=0;
         for(int num:nums){
-            if(num==1){
-                cnt++;
-                if(cnt>max) max=cnt;
-            }else{
-                cnt=0;
-            }
+            if(num==1)  cntZero++;
+            ans=Math.max(ans,cntZero);
+            if(num==0)  cntZero=0;
         }
-        return max;
+        return ans;
     }
 }
