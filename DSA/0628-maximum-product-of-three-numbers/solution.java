a0eb92b850c7c3d21f80b62732617012a0839927
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-07-26 05:47:45 +0000

    [commit] - Maximum Product of Three Numbers - Runtime - 17 ms (10.26%), Memory - 48.4 MB (11.67%)

--- DSA/0628-maximum-product-of-three-numbers/solution.java
+++ DSA/0628-maximum-product-of-three-numbers/solution.java
@@ -1,11 +1,9 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int last3=1;
-        Arrays.sort(nums);
         int n=nums.length;
-        last3=nums[n-1]*nums[n-2]*nums[n-3];
-        int first2=1;
-        first2=nums[0]*nums[1]*nums[n-1];
-        return Math.max(first2,last3);
+        Arrays.sort(nums);
+        int last3=nums[n-1]*nums[n-2]*nums[n-3];
+        int first2=nums[0]*nums[1]*nums[n-1];
+        return Math.max(last3,first2);
     }
 }
