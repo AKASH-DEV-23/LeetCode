c382ff878070cafb5b86f7bca98d50682528c1ee
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-07-27 08:36:04 +0000

    [commit] - Maximum Product of Two Elements in an Array - Runtime - 6 ms (15.94%), Memory - 45 MB (24.41%)

--- DSA/1574-maximum-product-of-two-elements-in-an-array/solution.java
+++ DSA/1574-maximum-product-of-two-elements-in-an-array/solution.java
@@ -1,17 +1,7 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max=-1;
-        int smax=-1;
-        for(int num:nums){
-            if(max<num){
-                smax=max;
-                max=num;
-            }
-            else if(smax<num){
-                smax=num;
-            }
-        }
-
-        return (max-1)*(smax-1);
+        Arrays.sort(nums);
+        int n=nums.length;
+        return (nums[n-1]-1)*(nums[n-2]-1);
     }
 }
