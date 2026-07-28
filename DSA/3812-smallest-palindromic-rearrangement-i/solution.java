010a437c1024590a2cb341d6d7b4b830e846a04b
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-07-28 03:31:10 +0000

    [commit] - Smallest Palindromic Rearrangement I - Runtime - 31 ms (63.05%), Memory - 48.2 MB (57.43%)

--- DSA/3812-smallest-palindromic-rearrangement-i/solution.java
+++ DSA/3812-smallest-palindromic-rearrangement-i/solution.java
@@ -1,24 +1,31 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int[] count=new int[26];
-        for(char ch:s.toCharArray()){
-            count[ch-'a']++;
-        }
-        StringBuilder sb1=new StringBuilder();
-        StringBuilder middle=new StringBuilder();
+        StringBuilder sb=new StringBuilder();
+        int[] freq=new int[26];
+        for(char ch:s.toCharArray())    freq[ch-'a']++;
+        int odd=-1;
         for(int i=0;i<26;i++){
-            if(count[i]%2==1){
-                char mid=(char)('a'+i);
-                middle.append(mid);
-            }
-            int cnt=count[i]/2;
-            char ch=(char)('a'+i);
-            while(cnt>0){
-                sb1.append(ch);
+            int cnt=freq[i];
+            if(cnt%2==0){
+                cnt>>=1;
+                while(cnt>0){
+                    sb.append((char)('a'+i));
                     cnt--;
+                }
+            }else{
+                cnt>>=1;
+                while(cnt>0){
+                    sb.append((char)('a'+i));
+                    cnt--;
+                }
+                odd=i;
             }
         }
-        
-        return sb1.toString() + middle.toString() + sb1.reverse().toString();
+        StringBuilder rev=new StringBuilder();
+        rev.append(sb);
+        if(odd!=-1)
+            rev.append((char)('a'+odd));
+        rev.append(sb.reverse());
+        return rev.toString();
     }
 }
