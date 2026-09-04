ce1f2917bb3239bdce04880faea4cb37ae0631f5
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-09-04 16:01:26 +0000

    [commit] - Smallest Stable Index I - Runtime - 1 ms (99.79%), Memory - 46.6 MB (15.79%)

--- DSA/4284-smallest-stable-index-i/solution.java
+++ DSA/4284-smallest-stable-index-i/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int size=nums.length;
-        int[] max=new int[size];
-        int[] min=new int[size];
-        max[0]=nums[0];
-        min[size-1]=nums[size-1];
-        for(int i=1;i<size;i++){
-            max[i]=Math.max(nums[i],max[i-1]);
+        int n=nums.length;
+        int[] suffix=new int[n];
+        suffix[n-1]=nums[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=Math.min(suffix[i+1],nums[i]);
         }
-        for(int i=size-2;i>=0;i--){
-            min[i]=Math.min(nums[i],min[i+1]);
+
+        int max=nums[0];
+        for(int i=0;i<n;i++){
+            max=Math.max(nums[i],max);
+            if(max-suffix[i]<=k)    return i;
         }
-        for(int i=0;i<size;i++){
-            if(max[i]-min[i]<=k) return i;
-        }
+
         return -1;
     }
 }
