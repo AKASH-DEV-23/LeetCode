8615297fb3ae76742cf860ea8d5777cfe7d7e7d8
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2026-09-08 15:33:03 +0000

    [commit] - Count Commas in Range - Runtime - 1 ms (83.58%), Memory - 42.8 MB (22.43%)

--- DSA/4245-count-commas-in-range/solution.java
+++ DSA/4245-count-commas-in-range/solution.java
@@ -1,20 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000)  return 0;
-        int ans=0;
-        for(int i=1000;i<=n;i++){
-            ans+=check(i);
-        }
-        return ans;
-    }
-
-    private int check(int num){
-        StringBuilder sb=new StringBuilder(num+"");
-        int len=sb.length();
-        int div=len/3;
-        if(len%3!=0){
-            return div;
-        }
-        return div-1;
+        return Math.max(n-999,0);
     }
 }
